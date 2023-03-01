89288a5b7cf0a07a25a5ce0e2cb18c27dc59a6e1
Irene Diez <idiez@redhat.com>
2023-03-01 18:39:18 +0100

    distro/fedora: use lvm+luks+ext4 for iot-simplified-installer
    This changes the partitioning system for Fedora iot-simplified-installer
    images so that they default to:
    GPT with
    - /boot/efi (vfat) 501 MiB
    - /boot (ext4) 1 GiB
    - LUKS (null encrypted)
       - LVM (rootvg)
         - / (ext4, rootlv) 2.5 GiB
    
    Signed-off-by: Irene Diez <idiez@redhat.com>

--- pkg/distro/fedora/distro.go
+++ pkg/distro/fedora/distro.go
@@ -175,7 +175,7 @@ var (
 		buildPipelines:      []string{"build"},
 		payloadPipelines:    []string{"ostree-deployment", "image", "xz", "coi-tree", "efiboot-tree", "bootiso-tree", "bootiso"},
 		exports:             []string{"bootiso"},
-		basePartitionTables: iotBasePartitionTables,
+		basePartitionTables: iotSimplifiedInstallerPartitionTables,
 	}
 
 	iotRawImgType = imageType{
