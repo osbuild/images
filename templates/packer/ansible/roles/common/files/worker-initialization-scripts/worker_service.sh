111a40301131b10a458d5aa2e53a601bc7c31854
Achilleas Koutsou <achilleas@koutsou.net>
2023-07-10 16:23:52 +0200

    templates: remove all deploy templates
    Deploy templates aren't applicable to the project.

--- templates/packer/ansible/roles/common/files/worker-initialization-scripts/worker_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source /tmp/cloud_init_vars
-
-echo "Starting worker service."
-
-# Prepare osbuild-composer's remote worker services and sockets.
-systemctl enable --now "osbuild-remote-worker@${COMPOSER_HOST}:${COMPOSER_PORT}"
--- templates/packer/config.pkr.hcl
+++
@@ -1,8 +0,0 @@
-packer {
-  required_plugins {
-    amazon = {
-      version = ">= 1.2.3"
-      source = "github.com/hashicorp/amazon"
-    }
-  }
-}
