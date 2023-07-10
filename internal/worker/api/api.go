61fe72832ebd209a94f7ca06171ec49004533a96
Achilleas Koutsou <achilleas@koutsou.net>
2023-07-10 16:09:34 +0200

    split: prune internal packages
    Removing internal packages that are used by osbuild-composer and are not
    relevant for the images.
    Also removing command line programs from cmd/ that are not related to
    images but implement osbuild-composer functionality.
    
    Unused vendored packages removed by 'go mod tidy'.

--- internal/worker/api/api.go
+++
@@ -1,6 +0,0 @@
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -package=api -generate types,server,spec -o api.gen.go openapi.yml
-
-package api
-
-// default basepath, can be overwritten
-var BasePath = "/api/worker/v1"
