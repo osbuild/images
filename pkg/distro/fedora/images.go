3a5a5d1912607071761215b5815d186ee0ce19b2
Simon de Vlieger <cmdr@supakeen.com>
2023-07-12 08:22:59 +0200

    image/live: renamed to `disk`
    Renames the `image/live` to `image/disk`. Als renames all image types in
    all distros to be called `diskImage` instead of `liveImage`.
    
    This much better describes what the old `liveImage` did (build various
    disk images).

--- pkg/distro/fedora/images.go
+++ pkg/distro/fedora/images.go
@@ -200,7 +200,7 @@ func osCustomizations(
 
 // IMAGES
 
-func liveImage(workload workload.Workload,
+func diskImage(workload workload.Workload,
 	t *imageType,
 	customizations *blueprint.Customizations,
 	options distro.ImageOptions,
@@ -208,7 +208,7 @@ func liveImage(workload workload.Workload,
 	containers []container.SourceSpec,
 	rng *rand.Rand) (image.ImageKind, error) {
 
-	img := image.NewLiveImage()
+	img := image.NewDiskImage()
 	img.Platform = t.platform
 	img.OSCustomizations = osCustomizations(t, packageSets[osPkgsKey], containers, customizations)
 	img.Environment = t.environment
