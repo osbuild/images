ff26acf36685cb27202003d9bd90761cb3a84a22
Sayan Paul <paul.sayan@gmail.com>
2023-08-16 06:52:23 -0400

    osbuild/systemd-unit: add user service dropin
    The stage is modified to add dropin file for user service this is
    achieved by adding a unit-type which can be set to system or
    global.Seperate struct is created for the unit section parameters
    accepted by systemd.
    Relevant testing added.
    
    Signed-off-by: Sayan Paul <paul.sayan@gmail.com>

--- pkg/osbuild/systemd_unit_stage_test.go
+++ pkg/osbuild/systemd_unit_stage_test.go
@@ -14,3 +14,23 @@ func TestNewSystemdUnitStage(t *testing.T) {
 	actualStage := NewSystemdUnitStage(&SystemdUnitStageOptions{})
 	assert.Equal(t, expectedStage, actualStage)
 }
+
+func TestNewSystemdGlobalUnitStage(t *testing.T) {
+	var options = SystemdUnitStageOptions{
+		Unit:   "test.timer",
+		Dropin: "10-greenboot.conf",
+		Config: SystemdServiceUnitDropin{
+			Unit: &SystemdUnitSection{
+				FileExists: "/usr/lib/test",
+			},
+		},
+		UnitType: Global,
+	}
+
+	expectedStage := &Stage{
+		Type:    "org.osbuild.systemd.unit",
+		Options: &options,
+	}
+	actualStage := NewSystemdUnitStage(&options)
+	assert.Equal(t, expectedStage, actualStage)
+}
