2d50ba71c26d67b77831605273d39dbafe6372f2
Peter Robinson <pbrobinson@gmail.com>
2023-09-08 17:20:23 +0100

    minimal-raw/rhel: Add offset to partitions
    A number of edge devices, in most cases pre-prod that may be on
    the edge of proper certification, need to write the firmware to
    the beginning of the storage, while this is far from ideal the
    default 2048 blocks (1Mb) is generally not enough and it means
    when the firmware is written out we will zap the EFI ESP
    partition so let's start this with an offset of 8Mb for
    these images to ensure we have enough space for those (hopefully
    rare) use cases.
    
    Signed-off-by: Peter Robinson <pbrobinson@gmail.com>

--- pkg/distro/rhel9/edge.go
+++ pkg/distro/rhel9/edge.go
@@ -191,7 +191,7 @@ var (
 		buildPipelines:      []string{"build"},
 		payloadPipelines:    []string{"os", "image", "xz"},
 		exports:             []string{"xz"},
-		basePartitionTables: defaultBasePartitionTables,
+		basePartitionTables: minimalrawPartitionTables,
 	}
 
 	// Shared Services
@@ -213,6 +213,102 @@ var (
 		},
 	}
 	// Partition tables
+	minimalrawPartitionTables = distro.BasePartitionTableMap{
+		platform.ARCH_X86_64.String(): disk.PartitionTable{
+			UUID:        "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
+			Type:        "gpt",
+			StartOffset: 8 * common.MebiByte,
+			Partitions: []disk.Partition{
+				{
+					Size: 200 * common.MebiByte,
+					Type: disk.EFISystemPartitionGUID,
+					UUID: disk.EFISystemPartitionUUID,
+					Payload: &disk.Filesystem{
+						Type:         "vfat",
+						UUID:         disk.EFIFilesystemUUID,
+						Mountpoint:   "/boot/efi",
+						Label:        "EFI-SYSTEM",
+						FSTabOptions: "defaults,uid=0,gid=0,umask=077,shortname=winnt",
+						FSTabFreq:    0,
+						FSTabPassNo:  2,
+					},
+				},
+				{
+					Size: 500 * common.MebiByte,
+					Type: disk.XBootLDRPartitionGUID,
+					UUID: disk.FilesystemDataUUID,
+					Payload: &disk.Filesystem{
+						Type:         "xfs",
+						Mountpoint:   "/boot",
+						Label:        "boot",
+						FSTabOptions: "defaults",
+						FSTabFreq:    0,
+						FSTabPassNo:  0,
+					},
+				},
+				{
+					Size: 2 * common.GibiByte,
+					Type: disk.FilesystemDataGUID,
+					UUID: disk.RootPartitionUUID,
+					Payload: &disk.Filesystem{
+						Type:         "xfs",
+						Label:        "root",
+						Mountpoint:   "/",
+						FSTabOptions: "defaults",
+						FSTabFreq:    0,
+						FSTabPassNo:  0,
+					},
+				},
+			},
+		},
+		platform.ARCH_AARCH64.String(): disk.PartitionTable{
+			UUID:        "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
+			Type:        "gpt",
+			StartOffset: 8 * common.MebiByte,
+			Partitions: []disk.Partition{
+				{
+					Size: 200 * common.MebiByte,
+					Type: disk.EFISystemPartitionGUID,
+					UUID: disk.EFISystemPartitionUUID,
+					Payload: &disk.Filesystem{
+						Type:         "vfat",
+						UUID:         disk.EFIFilesystemUUID,
+						Mountpoint:   "/boot/efi",
+						Label:        "EFI-SYSTEM",
+						FSTabOptions: "defaults,uid=0,gid=0,umask=077,shortname=winnt",
+						FSTabFreq:    0,
+						FSTabPassNo:  2,
+					},
+				},
+				{
+					Size: 500 * common.MebiByte,
+					Type: disk.XBootLDRPartitionGUID,
+					UUID: disk.FilesystemDataUUID,
+					Payload: &disk.Filesystem{
+						Type:         "xfs",
+						Mountpoint:   "/boot",
+						Label:        "boot",
+						FSTabOptions: "defaults",
+						FSTabFreq:    0,
+						FSTabPassNo:  0,
+					},
+				},
+				{
+					Size: 2 * common.GibiByte,
+					Type: disk.FilesystemDataGUID,
+					UUID: disk.RootPartitionUUID,
+					Payload: &disk.Filesystem{
+						Type:         "xfs",
+						Label:        "root",
+						Mountpoint:   "/",
+						FSTabOptions: "defaults",
+						FSTabFreq:    0,
+						FSTabPassNo:  0,
+					},
+				},
+			},
+		},
+	}
 	edgeBasePartitionTables = distro.BasePartitionTableMap{
 		platform.ARCH_X86_64.String(): disk.PartitionTable{
 			UUID: "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
