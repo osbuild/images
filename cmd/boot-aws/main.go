116f3d90cf61ec53fb5e1f944727828cd193f06d
Achilleas Koutsou <achilleas@koutsou.net>
2023-10-02 11:39:41 +0200

    boot-aws: use cobra for command like setup
    Use cobra to create subcommands and global flags.
    Supports global flags on the root command that are required for all
    subcommands.
    Adds two subcommands: setup and teardown

--- cmd/boot-aws/main.go
+++ cmd/boot-aws/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -12,9 +11,20 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	"github.com/osbuild/images/internal/cloud/awscloud"
 )
+
+// exitCheck can be deferred from the top of command functions to exit with an
+// error code after any other defers are run in the same scope.
+func exitCheck(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
 
 // createUserData creates cloud-init's user-data that contains user redhat with
 // the specified public key
@@ -42,217 +52,28 @@ type resources struct {
 	InstanceID    *string `json:"instance,omitempty"`
 }
 
-func tearDown(aws *awscloud.AWS, res *resources) {
-	fmt.Println("Tearing down")
-	if res.InstanceID != nil {
-		fmt.Printf("terminating instance %s\n", *res.InstanceID)
-		if _, err := aws.TerminateInstanceEC2(res.InstanceID); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to terminate instance: %v\n", err)
-		}
-	}
-
-	if res.SecurityGroup != nil {
-		fmt.Printf("deleting security group %s\n", *res.SecurityGroup)
-		if _, err := aws.DeleteSecurityGroupEC2(res.SecurityGroup); err != nil {
-			fmt.Fprintf(os.Stderr, "cannot delete the security group: %v\n", err)
-		}
-	}
-
-	if res.AMI != nil {
-		fmt.Printf("deleting EC2 image %s and snapshot %s\n", *res.AMI, *res.Snapshot)
-		if err := aws.DeleteEC2Image(res.AMI, res.Snapshot); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to deregister image: %v\n", err)
-		}
-	}
-
-}
-
-func uploadAndBoot() int {
-	var accessKeyID string
-	var secretAccessKey string
-	var sessionToken string
-	var region string
-	var bucketName string
-	var keyName string
-	var filename string
-	var imageName string
-	var shareWith string
-	var arch string
-	var bootMode string
-	var username string
-	var sshKey string
-
-	flag.StringVar(&accessKeyID, "access-key-id", "", "access key ID")
-	flag.StringVar(&secretAccessKey, "secret-access-key", "", "secret access key")
-	flag.StringVar(&sessionToken, "session-token", "", "session token")
-	flag.StringVar(&region, "region", "", "target region")
-	flag.StringVar(&bucketName, "bucket", "", "target S3 bucket name")
-	flag.StringVar(&keyName, "key", "", "target S3 key name")
-	flag.StringVar(&filename, "image", "", "image file to upload")
-	flag.StringVar(&imageName, "name", "", "AMI name")
-	flag.StringVar(&shareWith, "account-id", "", "account id to share image with")
-	flag.StringVar(&arch, "arch", "", "arch (x86_64 or aarch64)")
-	flag.StringVar(&bootMode, "boot-mode", "", "boot mode (legacy-bios, uefi, uefi-preferred)")
-	flag.StringVar(&username, "username", "", "name of the user to create on the system")
-	flag.StringVar(&sshKey, "ssh-key", "", "path to user's public ssh key")
-
-	var resourcesFile string
-	flag.StringVar(&resourcesFile, "tear-down", "", "path to resources file to use for tear-down")
-	flag.Parse()
-
-	a, err := awscloud.New(region, accessKeyID, secretAccessKey, sessionToken)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "awscloud.New() failed: %s\n", err.Error())
-		return 1
-	}
-
-	res := &resources{}
-
-	if len(resourcesFile) > 0 {
-		resfile, err := os.Open(resourcesFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to open resources file: %s\n", err.Error())
-			return 1
-		}
-		resdata, err := io.ReadAll(resfile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to read resources file: %s\n", err.Error())
-			return 1
-		}
-		if err := json.Unmarshal(resdata, res); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to unmarshal resources data: %s\n", err.Error())
-			return 1
-		}
-		defer tearDown(a, res)
-		return 0
-	}
-
-	defer func() {
-		resdata, err := json.MarshalIndent(res, "", "  ")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to marshal resources data: %s\n", err.Error())
-			return
-		}
-		fmt.Println(string(resdata))
-	}()
-
-	userData, err := createUserData(username, sshKey)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "createUserData(): %s\n", err.Error())
-		return 1
-	}
-
-	uploadOutput, err := a.Upload(filename, bucketName, keyName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Upload() failed: %s\n", err.Error())
-		return 1
-	}
-
-	fmt.Printf("file uploaded to %s\n", aws.StringValue(&uploadOutput.Location))
-
-	var share []string
-	if shareWith != "" {
-		share = append(share, shareWith)
-	}
-
-	var bootModePtr *string
-	if bootMode != "" {
-		bootModePtr = &bootMode
-	}
-
-	ami, snapshot, err := a.Register(imageName, bucketName, keyName, share, arch, bootModePtr)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Register(): %s\n", err.Error())
-		return 1
-	}
-
-	res.AMI = ami
-	res.Snapshot = snapshot
-
-	fmt.Printf("AMI registered: %s\n", aws.StringValue(ami))
-
-	securityGroup, err := a.CreateSecurityGroupEC2("image-tests", "image-tests-security-group")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "CreateSecurityGroup(): %s\n", err.Error())
-		return 1
-	}
-
-	res.SecurityGroup = securityGroup.GroupId
-
-	_, err = a.AuthorizeSecurityGroupIngressEC2(securityGroup.GroupId, "0.0.0.0/0", 22, 22, "tcp")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "AuthorizeSecurityGroupIngressEC2(): %s\n", err.Error())
-		return 1
-	}
-
-	runResult, err := a.RunInstanceEC2(ami, securityGroup.GroupId, userData, "t3.micro")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "RunInstanceEC2(): %s\n", err.Error())
-		return 1
-	}
-	instanceID := runResult.Instances[0].InstanceId
-	res.InstanceID = instanceID
-
-	ip, err := a.GetInstanceAddress(instanceID)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "GetInstanceAddress(): %s\n", err.Error())
-		return 1
-	}
-	fmt.Printf("Instance %s is running and has IP address %s\n", *instanceID, ip)
-
-	hostsfile := "/tmp/test_hosts"
-	if err := keyscan(ip, hostsfile); err != nil {
-		fmt.Fprintf(os.Stderr, "keyscan(): %s\n", err.Error())
-		fmt.Printf("Press RETURN to clean up")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
-		return 1
-	}
-
-	key := strings.TrimSuffix(sshKey, ".pub")
-	if err := sshCheck(ip, username, key, hostsfile); err != nil {
-		fmt.Fprintf(os.Stderr, "sshCheck(): %s\n", err.Error())
-		return 1
-	}
-
-	return 0
-}
-
 func run(c string, args ...string) ([]byte, []byte, error) {
 	fmt.Printf("> %s %s\n", c, strings.Join(args, " "))
 	cmd := exec.Command(c, args...)
 
-	stdout, err := cmd.StdoutPipe()
+	var cmdout, cmderr bytes.Buffer
+	cmd.Stdout = &cmdout
+	cmd.Stderr = &cmderr
+	err := cmd.Run()
 	if err != nil {
 		return nil, nil, err
 	}
 
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return nil, nil, err
+	stdout := cmdout.Bytes()
+	if len(stdout) > 0 {
+		fmt.Println(string(stdout))
 	}
 
-	if err := cmd.Start(); err != nil {
-		return nil, nil, err
+	stderr := cmderr.Bytes()
+	if len(stderr) > 0 {
+		fmt.Fprintf(os.Stderr, string(stderr)+"\n")
 	}
-
-	cmdout, err := io.ReadAll(stdout)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	cmderr, err := io.ReadAll(stderr)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	err = cmd.Wait()
-	if len(cmdout) > 0 {
-		fmt.Println(string(cmdout))
-	}
-	if len(cmderr) > 0 {
-		fmt.Fprintf(os.Stderr, string(cmderr)+"\n")
-	}
-	return cmdout, cmderr, err
+	return stdout, stderr, err
 }
 
 func sshCheck(ip, user, key, hostsfile string) error {
@@ -296,6 +117,295 @@ func keyscan(ip, filepath string) error {
 	return nil
 }
 
+func newClientFromArgs(flags *pflag.FlagSet) (*awscloud.AWS, error) {
+	region, err := flags.GetString("region")
+	if err != nil {
+		return nil, err
+	}
+	keyID, err := flags.GetString("access-key-id")
+	if err != nil {
+		return nil, err
+	}
+	secretKey, err := flags.GetString("secret-access-key")
+	if err != nil {
+		return nil, err
+	}
+	sessionToken, err := flags.GetString("session-token")
+	if err != nil {
+		return nil, err
+	}
+
+	return awscloud.New(region, keyID, secretKey, sessionToken)
+}
+
+func doSetup(a *awscloud.AWS, filename string, flags *pflag.FlagSet, res *resources) error {
+	username, err := flags.GetString("username")
+	if err != nil {
+		return err
+	}
+	sshKey, err := flags.GetString("ssh-key")
+	if err != nil {
+		return err
+	}
+
+	userData, err := createUserData(username, sshKey)
+	if err != nil {
+		return fmt.Errorf("createUserData(): %s\n", err.Error())
+	}
+
+	bucketName, err := flags.GetString("bucket")
+	if err != nil {
+		return err
+	}
+	keyName, err := flags.GetString("key")
+	if err != nil {
+		return err
+	}
+
+	uploadOutput, err := a.Upload(filename, bucketName, keyName)
+	if err != nil {
+		return fmt.Errorf("Upload() failed: %s\n", err.Error())
+	}
+
+	fmt.Printf("file uploaded to %s\n", aws.StringValue(&uploadOutput.Location))
+
+	var share []string
+	if shareWith, err := flags.GetString("account-id"); shareWith != "" {
+		share = append(share, shareWith)
+	} else if err != nil {
+		return err
+	}
+
+	var bootModePtr *string
+	if bootMode, err := flags.GetString("boot-mode"); bootMode != "" {
+		bootModePtr = &bootMode
+	} else if err != nil {
+		return err
+	}
+
+	imageName, err := flags.GetString("name")
+	if err != nil {
+		return err
+	}
+
+	arch, err := flags.GetString("arch")
+	if err != nil {
+		return err
+	}
+
+	ami, snapshot, err := a.Register(imageName, bucketName, keyName, share, arch, bootModePtr)
+	if err != nil {
+		return fmt.Errorf("Register(): %s\n", err.Error())
+	}
+
+	res.AMI = ami
+	res.Snapshot = snapshot
+
+	fmt.Printf("AMI registered: %s\n", aws.StringValue(ami))
+
+	securityGroup, err := a.CreateSecurityGroupEC2("image-tests", "image-tests-security-group")
+	if err != nil {
+		return fmt.Errorf("CreateSecurityGroup(): %s\n", err.Error())
+	}
+
+	res.SecurityGroup = securityGroup.GroupId
+
+	_, err = a.AuthorizeSecurityGroupIngressEC2(securityGroup.GroupId, "0.0.0.0/0", 22, 22, "tcp")
+	if err != nil {
+		return fmt.Errorf("AuthorizeSecurityGroupIngressEC2(): %s\n", err.Error())
+	}
+
+	runResult, err := a.RunInstanceEC2(ami, securityGroup.GroupId, userData, "t3.micro")
+	if err != nil {
+		return fmt.Errorf("RunInstanceEC2(): %s\n", err.Error())
+	}
+	instanceID := runResult.Instances[0].InstanceId
+	res.InstanceID = instanceID
+
+	ip, err := a.GetInstanceAddress(instanceID)
+	if err != nil {
+		return fmt.Errorf("GetInstanceAddress(): %s\n", err.Error())
+	}
+	fmt.Printf("Instance %s is running and has IP address %s\n", *instanceID, ip)
+
+	hostsfile := "/tmp/test_hosts"
+	if err := keyscan(ip, hostsfile); err != nil {
+		return fmt.Errorf("keyscan(): %s\n", err.Error())
+	}
+
+	key := strings.TrimSuffix(sshKey, ".pub")
+	if err := sshCheck(ip, username, key, hostsfile); err != nil {
+		return fmt.Errorf("sshCheck(): %s\n", err.Error())
+	}
+
+	return nil
+}
+
+func setup(cmd *cobra.Command, args []string) {
+	var err error
+	defer func() { exitCheck(err) }()
+
+	filename := args[0]
+	flags := cmd.Flags()
+
+	a, err := newClientFromArgs(flags)
+	if err != nil {
+		return
+	}
+
+	// collect resources into res and write them out when the function returns
+	resourcesFile, err := flags.GetString("resourcefile")
+	if err != nil {
+		return
+	}
+	res := &resources{}
+	defer func() {
+		resdata, err := json.MarshalIndent(res, "", "  ")
+		if err != nil {
+			err = fmt.Errorf("failed to marshal resources data: %s\n", err.Error())
+			return
+		}
+		resfile, err := os.Create(resourcesFile)
+		if err != nil {
+			err = fmt.Errorf("failed to create resources file: %s\n", err.Error())
+			return
+		}
+		_, err = resfile.Write(resdata)
+		if err != nil {
+			err = fmt.Errorf("failed to write resources file: %s\n", err.Error())
+			return
+		}
+		fmt.Printf("IDs for any newly created resources are stored in %s. Use the teardown command to clean them up.\n", resourcesFile)
+		err = resfile.Close()
+	}()
+
+	err = doSetup(a, filename, flags, res)
+}
+
+func doTeardown(aws *awscloud.AWS, res *resources) error {
+	if res.InstanceID != nil {
+		fmt.Printf("terminating instance %s\n", *res.InstanceID)
+		if _, err := aws.TerminateInstanceEC2(res.InstanceID); err != nil {
+			return fmt.Errorf("failed to terminate instance: %v\n", err)
+		}
+	}
+
+	if res.SecurityGroup != nil {
+		fmt.Printf("deleting security group %s\n", *res.SecurityGroup)
+		if _, err := aws.DeleteSecurityGroupEC2(res.SecurityGroup); err != nil {
+			return fmt.Errorf("cannot delete the security group: %v\n", err)
+		}
+	}
+
+	if res.AMI != nil {
+		fmt.Printf("deleting EC2 image %s and snapshot %s\n", *res.AMI, *res.Snapshot)
+		if err := aws.DeleteEC2Image(res.AMI, res.Snapshot); err != nil {
+			return fmt.Errorf("failed to deregister image: %v\n", err)
+		}
+	}
+	return nil
+}
+
+func teardown(cmd *cobra.Command, args []string) {
+	var err error
+	defer func() { exitCheck(err) }()
+
+	flags := cmd.Flags()
+
+	a, err := newClientFromArgs(flags)
+	if err != nil {
+		return
+	}
+
+	resourcesFile, err := flags.GetString("resourcefile")
+	if err != nil {
+		return
+	}
+
+	res := &resources{}
+
+	resfile, err := os.Open(resourcesFile)
+	if err != nil {
+		err = fmt.Errorf("failed to open resources file: %s\n", err.Error())
+		return
+	}
+	resdata, err := io.ReadAll(resfile)
+	if err != nil {
+		err = fmt.Errorf("failed to read resources file: %s\n", err.Error())
+		return
+	}
+	if err := json.Unmarshal(resdata, res); err != nil {
+		err = fmt.Errorf("failed to unmarshal resources data: %s\n", err.Error())
+		return
+	}
+
+	err = doTeardown(a, res)
+}
+
+func setupCLI() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:                   "boot",
+		Long:                  "upload and boot an image to the appropriate cloud provider",
+		DisableFlagsInUseLine: true,
+	}
+
+	rootFlags := rootCmd.PersistentFlags()
+	rootFlags.String("access-key-id", "", "access key ID")
+	rootFlags.String("secret-access-key", "", "secret access key")
+	rootFlags.String("session-token", "", "session token")
+	rootFlags.String("region", "", "target region")
+	rootFlags.String("bucket", "", "target S3 bucket name")
+	rootFlags.String("key", "", "target S3 key name")
+	rootFlags.String("name", "", "AMI name")
+	rootFlags.String("account-id", "", "account id to share image with")
+	rootFlags.String("arch", "", "arch (x86_64 or aarch64)")
+	rootFlags.String("boot-mode", "", "boot mode (legacy-bios, uefi, uefi-preferred)")
+	rootFlags.String("username", "", "name of the user to create on the system")
+	rootFlags.String("ssh-key", "", "path to user's public ssh key")
+
+	exitCheck(rootCmd.MarkPersistentFlagRequired("access-key-id"))
+	exitCheck(rootCmd.MarkPersistentFlagRequired("secret-access-key"))
+	exitCheck(rootCmd.MarkPersistentFlagRequired("region"))
+	exitCheck(rootCmd.MarkPersistentFlagRequired("bucket"))
+
+	// TODO: make it optional and use UUID if not specified
+	exitCheck(rootCmd.MarkPersistentFlagRequired("key"))
+
+	// TODO: make it optional and use UUID if not specified
+	exitCheck(rootCmd.MarkPersistentFlagRequired("name"))
+
+	exitCheck(rootCmd.MarkPersistentFlagRequired("arch"))
+
+	// TODO: make it optional and use a default
+	exitCheck(rootCmd.MarkPersistentFlagRequired("username"))
+
+	// TODO: make ssh key optional for 'run' and if not specified generate a
+	// temporary key pair
+	exitCheck(rootCmd.MarkPersistentFlagRequired("ssh-key"))
+
+	setupCmd := &cobra.Command{
+		Use:                   "setup [--resourcefile <filename>] <filename>",
+		Short:                 "upload and boot an image and save the created resource IDs to a file for later teardown",
+		Args:                  cobra.ExactArgs(1),
+		Run:                   setup,
+		DisableFlagsInUseLine: true,
+	}
+	setupCmd.Flags().StringP("resourcefile", "r", "resources.json", "path to store the resource IDs")
+	rootCmd.AddCommand(setupCmd)
+
+	teardownCmd := &cobra.Command{
+		Use:   "teardown [--resourcefile <filename>]",
+		Short: "teardown (clean up) all the resources specified in a resources file created by a previous 'setup' call",
+		Args:  cobra.NoArgs,
+		Run:   teardown,
+	}
+	teardownCmd.Flags().StringP("resourcefile", "r", "resources.json", "path to store the resource IDs")
+	rootCmd.AddCommand(teardownCmd)
+
+	return rootCmd
+}
+
 func main() {
-	os.Exit(uploadAndBoot())
+	cmd := setupCLI()
+	cmd.Execute()
 }
