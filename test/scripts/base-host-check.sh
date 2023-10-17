6879f5714a6a97f8671339020dd2895853adcb00
Achilleas Koutsou <achilleas@koutsou.net>
2023-10-17 23:34:37 +0200

    test: run systemctl list-units for boot test
    When the system is in a degraded state or not fully operational, seeing
    the state of all units (and listing them) can be useful.

--- test/scripts/base-host-check.sh
+++ test/scripts/base-host-check.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-systemctl is-system-running --wait
+if ! sudo systemctl is-system-running --wait; then
+    # system is not fully operational
+    # (try to) list units so we can troubleshoot any failures
+    systemctl list-units
+
+    # exit with failure; we don't care about the exact exit code from the
+    # failed condition
+    exit 1
+fi
+
 rpm -qa
 cat /etc/os-release
 uname -a
