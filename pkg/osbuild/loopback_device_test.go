e9f5b310a23cc107c914ceee7d1a110ca0610b95
Michael Vogt <michael.vogt@gmail.com>
2024-01-30 10:50:05 +0100

    osbuild: add support for `partscan` option in org.osbuild.loopback
    This commit adds support for `partscan` in `org.osbuild.loopback`.
    
    The coresponding osbuild option was added in
    https://github.com/osbuild/osbuild/pull/1501

---
+++ pkg/osbuild/loopback_device_test.go
@@ -0,0 +1,46 @@
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/osbuild/images/internal/common"
+	"github.com/osbuild/images/pkg/osbuild"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoopbackDeviceOptionsSerializesAll(t *testing.T) {
+	dev := osbuild.LoopbackDeviceOptions{
+		Filename:   "foo.disk",
+		Start:      12345,
+		Size:       54321,
+		SectorSize: common.ToPtr(uint64(4096)),
+		Lock:       true,
+		Partscan:   true,
+	}
+	json, err := json.MarshalIndent(dev, "", "  ")
+	require.Nil(t, err)
+	assert.Equal(t, string(json), `
+{
+  "filename": "foo.disk",
+  "start": 12345,
+  "size": 54321,
+  "sector-size": 4096,
+  "lock": true,
+  "partscan": true
+}`[1:])
+}
+
+func TestLoopbackDeviceOptionsSerializesOmitEmptyHonored(t *testing.T) {
+	dev := osbuild.LoopbackDeviceOptions{
+		Filename: "foo.disk",
+	}
+	json, err := json.MarshalIndent(dev, "", "  ")
+	require.Nil(t, err)
+	assert.Equal(t, string(json), `
+{
+  "filename": "foo.disk"
+}`[1:])
+}
