dfc653c6c6ce15158ccf4fa01a98d17bfa4b15f5
Michael Vogt <michael.vogt@gmail.com>
2024-02-04 17:39:36 +0100

    osbuild: remove the unused NewZiplStageOptions()
    The function is not used in the code and it is also not needed
    as it will just be equivalent to `new(ZiplStageOptions)` or
    `&ZiplStageOption{}`. If we can always reintroduce it if we
    need it with a more meaningful init of the options.

--- pkg/osbuild/zipl_stage_test.go
+++ pkg/osbuild/zipl_stage_test.go
@@ -6,14 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewZiplStageOptions(t *testing.T) {
-	expectedOptions := &ZiplStageOptions{
-		Timeout: 0,
-	}
-	actualOptions := NewZiplStageOptions()
-	assert.Equal(t, expectedOptions, actualOptions)
-}
-
 func TestNewZiplStage(t *testing.T) {
 	expectedStage := &Stage{
 		Type:    "org.osbuild.zipl",
