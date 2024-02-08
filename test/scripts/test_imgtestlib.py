41624568c089e4bd23106f21a98f595f930ad360
Achilleas Koutsou <achilleas@koutsou.net>
2024-02-08 14:58:21 +0100

    test: add path generator unit tests
    Test the path generators with a mocked get_osbuild_nevra() function so
    that they can be run without osbuild installed and on non-rpm based
    systems.

--- test/scripts/test_imgtestlib.py
+++ test/scripts/test_imgtestlib.py
@@ -1,17 +1,17 @@
 import os
 
-from imgtestlib import rng_seed_env, runcmd
+import imgtestlib as testlib
 
 
 def test_runcmd():
-    stdout, stderr = runcmd(["/bin/echo", "hello"])
+    stdout, stderr = testlib.runcmd(["/bin/echo", "hello"])
     assert stdout == b"hello\n"
     assert stderr == b""
 
 
 def test_runcmd_env():
     os.environ["RUNCMD_GLOBAL_TEST_VAR"] = "global test value"
-    stdout, stderr = runcmd(["env"], extra_env={"RUNCMD_TEST_VAR": "the test value"})
+    stdout, stderr = testlib.runcmd(["env"], extra_env={"RUNCMD_TEST_VAR": "the test value"})
     assert b"RUNCMD_TEST_VAR=the test value\n" in stdout, "extra env var not set"
     assert b"RUNCMD_GLOBAL_TEST_VAR=global test value\n" in stdout, "global env vars not preserved"
     assert stderr == b""
@@ -19,5 +19,16 @@ def test_runcmd_env():
 
 def test_read_seed():
     # check that it's read without error - no need to test the value itself
-    seed_env = rng_seed_env()
+    seed_env = testlib.rng_seed_env()
     assert "OSBUILD_TESTING_RNG_SEED" in seed_env
+
+
+def test_path_generators():
+    testlib.get_osbuild_nevra = lambda: "osbuild-104-1.fc39.noarch"
+
+    assert testlib.gen_build_info_dir_path("inforoot", testlib.get_osbuild_nevra(), "abc123") == \
+        "inforoot/osbuild-104-1.fc39.noarch/abc123/"
+    assert testlib.gen_build_info_path("inforoot", testlib.get_osbuild_nevra(), "abc123") == \
+        "inforoot/osbuild-104-1.fc39.noarch/abc123/info.json"
+    assert testlib.gen_build_info_s3("fedora-39", "aarch64", "abc123") == \
+        testlib.S3_BUCKET + "/images/builds/fedora-39/aarch64/osbuild-104-1.fc39.noarch/abc123/"
