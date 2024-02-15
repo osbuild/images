2ba0b641b2f5fda84bca1585d1564e631fefe4b5
Sayan Paul <paul.sayan@gmail.com>
2024-02-15 21:06:20 +0530

    stage/systemd-unit-create:add systemd-unit-create stage
    Create systemd unit using the system-unit-create stage
    This new stage adds the ability to create a systemd service unit file.
    It provides customization to choose the unit-type and unit-path, defaults to '/usr/lib/systemd/system/'
    Filename is validated using the same rules as specified by systemd.unit(5) and they must contain the
    '.service' suffix (other types of unit files are not supported). config accepts sections: `Unit`,
    `Service`, `Install` and each section accepts list of options that is in accordance to systemd.service
    documentation. Relavent testing is also added.
    
    Signed-off-by: Sayan Paul <paul.sayan@gmail.com>

---
+++ pkg/osbuild/systemd_unit_create_stage_test.go
@@ -0,0 +1,70 @@
+package osbuild
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func createSystemdUnit() SystemdServiceUnit {
+
+	var unit = Unit{
+		Description:              "Create directory and files",
+		ConditionPathExists:      []string{"!/etc/myfile"},
+		ConditionPathIsDirectory: []string{"!/etc/mydir"},
+		Requires:                 []string{"dbus.service", "libvirtd.service"},
+		Wants:                    []string{"local-fs.target"},
+	}
+	var service = Service{
+		Type:            Oneshot,
+		RemainAfterExit: true,
+		ExecStartPre:    []string{"echo creating_files"},
+		ExecStopPost:    []string{"echo done_creating_files"},
+		ExecStart:       []string{"mkdir -p /etc/mydir", "touch /etc/myfiles"},
+	}
+
+	var install = Install{
+		RequiredBy: []string{"multi-user.target", "boot-complete.target"},
+		WantedBy:   []string{"sshd.service"},
+	}
+
+	var systemdUnit = SystemdServiceUnit{
+		Unit:    &unit,
+		Service: &service,
+		Install: &install,
+	}
+
+	return systemdUnit
+}
+
+func TestNewSystemdUnitCreateStage(t *testing.T) {
+	systemdServiceConfig := createSystemdUnit()
+	var options = SystemdUnitCreateStageOptions{
+		Filename: "create-dir-files",
+		Config:   systemdServiceConfig,
+	}
+	expectedStage := &Stage{
+		Type:    "org.osbuild.systemd.unit.create",
+		Options: &options,
+	}
+
+	actualStage := NewSystemdUnitCreateStageOptions(&options)
+	assert.Equal(t, expectedStage, actualStage)
+}
+
+func TestNewSystemdUnitCreateStageInEtc(t *testing.T) {
+	systemdServiceConfig := createSystemdUnit()
+	var options = SystemdUnitCreateStageOptions{
+		Filename: "create-dir-files",
+		Config:   systemdServiceConfig,
+		UnitPath: Etc,
+		UnitType: Global,
+	}
+	expectedStage := &Stage{
+		Type:    "org.osbuild.systemd.unit.create",
+		Options: &options,
+	}
+
+	actualStage := NewSystemdUnitCreateStageOptions(&options)
+	assert.Equal(t, expectedStage, actualStage)
+}
