16cffce40127641f4f18b1ac63e83f7383ded00f
Michael Vogt <michael.vogt@gmail.com>
2024-02-16 16:11:25 +0100

    osbuild: generate bootupd mounts to span a single disk via partscan=True
    The `bootupd` and `bootc install to-filesystem` stages require a
    continous disk so that grub can install the bootloader onto the
    disk. This means that the new `partscan` feature from [0] must be
    used when generating the disk instead of the current approach that
    generates a bunch of loopback devices for each partition.
    
    Note that this version of the code does not implement support
    for "container" partition tables like LUKS.
    
    [0] https://github.com/osbuild/osbuild/pull/1501

--- pkg/osbuild/bootupd_stage_test.go
+++ pkg/osbuild/bootupd_stage_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/osbuild/images/internal/common"
+	"github.com/osbuild/images/pkg/disk"
 	"github.com/osbuild/images/pkg/osbuild"
 )
 
@@ -142,4 +144,103 @@ func TestBootupdStageJsonHappy(t *testing.T) {
     }
   ]
 }`)
+}
+
+func TestGenBootupdDevicesMountsMissingRoot(t *testing.T) {
+	filename := "fake-disk.img"
+	pt := &disk.PartitionTable{}
+	_, _, err := osbuild.GenBootupdDevicesMounts(filename, pt)
+	assert.EqualError(t, err, "required mounts for bootupd stage [/ /boot /boot/efi] missing")
+}
+
+var fakePt = &disk.PartitionTable{
+	UUID: "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
+	Type: "gpt",
+	Partitions: []disk.Partition{
+		{
+			Size:     1 * common.MebiByte,
+			Bootable: true,
+			Type:     disk.BIOSBootPartitionGUID,
+			UUID:     disk.BIOSBootPartitionUUID,
+		},
+		{
+			Size: 501 * common.MebiByte,
+			Type: disk.EFISystemPartitionGUID,
+			UUID: disk.EFISystemPartitionUUID,
+			Payload: &disk.Filesystem{
+				Type:         "vfat",
+				UUID:         disk.EFIFilesystemUUID,
+				Mountpoint:   "/boot/efi",
+				Label:        "EFI-SYSTEM",
+				FSTabOptions: "umask=0077,shortname=winnt",
+				FSTabFreq:    0,
+				FSTabPassNo:  2,
+			},
+		},
+		{
+			Size: 1 * common.GibiByte,
+			Type: disk.FilesystemDataGUID,
+			UUID: disk.FilesystemDataUUID,
+			Payload: &disk.Filesystem{
+				Type:         "ext4",
+				Mountpoint:   "/boot",
+				Label:        "boot",
+				FSTabOptions: "defaults",
+				FSTabFreq:    1,
+				FSTabPassNo:  2,
+			},
+		},
+		{
+			Size: 2 * common.GibiByte,
+			Type: disk.FilesystemDataGUID,
+			UUID: disk.RootPartitionUUID,
+			Payload: &disk.Filesystem{
+				Type:         "ext4",
+				Label:        "root",
+				Mountpoint:   "/",
+				FSTabOptions: "defaults",
+				FSTabFreq:    1,
+				FSTabPassNo:  1,
+			},
+		},
+	},
+}
+
+func TestGenBootupdDevicesMountsHappy(t *testing.T) {
+	filename := "fake-disk.img"
+
+	devices, mounts, err := osbuild.GenBootupdDevicesMounts(filename, fakePt)
+	require.Nil(t, err)
+	assert.Equal(t, devices, map[string]osbuild.Device{
+		"disk": {
+			Type: "org.osbuild.loopback",
+			Options: &osbuild.LoopbackDeviceOptions{
+				Filename: "fake-disk.img",
+				Partscan: true,
+			},
+		},
+	})
+	assert.Equal(t, mounts, []osbuild.Mount{
+		{
+			Name:      "part4",
+			Type:      "org.osbuild.ext4",
+			Source:    "disk",
+			Target:    "/",
+			Partition: common.ToPtr(4),
+		},
+		{
+			Name:      "part3",
+			Type:      "org.osbuild.ext4",
+			Source:    "disk",
+			Target:    "/boot",
+			Partition: common.ToPtr(3),
+		},
+		{
+			Name:      "part2",
+			Type:      "org.osbuild.fat",
+			Source:    "disk",
+			Target:    "/boot/efi",
+			Partition: common.ToPtr(2),
+		},
+	})
 }
