fc0e163175a1788b55cc296685d618b8911b2941
Gianluca Zuccarelli <gzuccare@redhat.com>
2024-02-20 12:55:57 +0000

    osbuild: add local containers input
    We need to differentiate between containers coming from local container
    storage and remote containers, since we have different sources for each.

--- pkg/osbuild/containers_input.go
+++ pkg/osbuild/containers_input.go
@@ -16,6 +16,10 @@ type ContainersInput struct {
 func NewContainersInputForSources(containers []container.Spec) ContainersInput {
 	refs := make(map[string]ContainersInputSourceRef, len(containers))
 	for _, c := range containers {
+		if c.LocalStorage {
+			// skip containers that are coming from local containers storage
+			continue
+		}
 		ref := ContainersInputSourceRef{
 			Name: c.LocalName,
 		}
@@ -26,6 +30,28 @@ func NewContainersInputForSources(containers []container.Spec) ContainersInput {
 		References: refs,
 		inputCommon: inputCommon{
 			Type:   "org.osbuild.containers",
+			Origin: InputOriginSource,
+		},
+	}
+}
+
+func NewLocalContainersInputForSources(containers []container.Spec) ContainersInput {
+	refs := make(map[string]ContainersInputSourceRef, len(containers))
+	for _, c := range containers {
+		if !c.LocalStorage {
+			// skip containers that are not in the local containers storage
+			continue
+		}
+		ref := ContainersInputSourceRef{
+			Name: c.LocalName,
+		}
+		refs[c.ImageID] = ref
+	}
+
+	return ContainersInput{
+		References: refs,
+		inputCommon: inputCommon{
+			Type:   "org.osbuild.containers-storage",
 			Origin: InputOriginSource,
 		},
 	}
