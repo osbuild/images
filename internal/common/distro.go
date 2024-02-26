b51f9b1859563c5552786fb178acc61a51621256
Simon de Vlieger <cmdr@supakeen.com>
2024-02-26 13:04:34 +0100

    common: base version constraints on eachother
    Basing `VersionGreaterThanOrEqual` on `!VersionLessThan` saves us a few
    lines of code in error checking.

--- internal/common/distro.go
+++ internal/common/distro.go
@@ -28,14 +28,5 @@ func VersionLessThan(a, b string) bool {
 }
 
 func VersionGreaterThanOrEqual(a, b string) bool {
-	aV, err := version.NewVersion(a)
-	if err != nil {
-		panic(err)
-	}
-	bV, err := version.NewVersion(b)
-	if err != nil {
-		panic(err)
-	}
-
-	return aV.GreaterThanOrEqual(bV)
+	return !VersionLessThan(a, b)
 }
