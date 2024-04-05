3039ddb9a9522d965ec878ed03ae0ba81fffebe9
Michael Vogt <michael.vogt@gmail.com>
2024-04-05 16:13:35 +0200

    osbuild: add new `osbuild.NewOSTreeDeploymentMountDefault()`
    Add the "Default" and "Source" options to OSTreeMountDeployment.
    See
    - "default" https://github.com/osbuild/osbuild/pull/1553
    - "source"  https://github.com/osbuild/osbuild/pull/1535

---
+++ pkg/osbuild/ostree_deployment_mount_test.go
@@ -0,0 +1,46 @@
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/osbuild/images/pkg/osbuild"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOSTreeMountDeploymentDefaultSerialized(t *testing.T) {
+	mntStage := osbuild.NewOSTreeDeploymentMountDefault("some-name", osbuild.OSTreeMountSourceMount)
+	json, err := json.MarshalIndent(mntStage, "", "  ")
+	require.Nil(t, err)
+	assert.Equal(t, string(json), `
+{
+  "name": "some-name",
+  "type": "org.osbuild.ostree.deployment",
+  "options": {
+    "source": "mount",
+    "deployment": {
+      "default": true
+    }
+  }
+}`[1:])
+}
+
+func TestOSTreeMountDeploymentSerialized(t *testing.T) {
+	mntStage := osbuild.NewOSTreeDeploymentMount("some-name", "some-osname", "some-ref", 0)
+	json, err := json.MarshalIndent(mntStage, "", "  ")
+	require.Nil(t, err)
+	assert.Equal(t, string(json), `
+{
+  "name": "some-name",
+  "type": "org.osbuild.ostree.deployment",
+  "options": {
+    "deployment": {
+      "osname": "some-osname",
+      "ref": "some-ref",
+      "serial": 0
+    }
+  }
+}`[1:])
+}
