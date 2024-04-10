26899f834510cbd72a071e073455daf7a7cf9f7c
Ondřej Budai <ondrej@budai.cz>
2024-04-10 16:34:29 +0200

    distro: add ReadOSReleaseFromTree
    This new method supports reading os-release from an arbitrary tree,
    and it also correctly handles multiple os-release locations (see
    os-release(5)). It's public so we can reuse it in multiple places.

--- pkg/distro/host_test.go
+++ pkg/distro/host_test.go
@@ -1,9 +1,13 @@
 package distro
 
 import (
+	"os"
+	"path"
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestOSRelease(t *testing.T) {
@@ -51,4 +55,37 @@ VARIANT_ID=workstation`,
 			t.Fatalf("%d: readOSRelease returned unexpected result: %#v", i, osrelease)
 		}
 	}
+}
+
+func TestReadOSReleaseFromTree(t *testing.T) {
+	tree := t.TempDir()
+
+	// initialize dirs
+	require.NoError(t, os.MkdirAll(path.Join(tree, "usr/lib"), 0755))
+	require.NoError(t, os.MkdirAll(path.Join(tree, "etc"), 0755))
+
+	// firstly, let's write a simple /usr/lib/os-release
+	require.NoError(t,
+		os.WriteFile(path.Join(tree, "usr/lib/os-release"), []byte("ID=toucan\n"), 0600),
+	)
+
+	osRelease, err := ReadOSReleaseFromTree(tree)
+	require.NoError(t, err)
+	require.Equal(t, "toucan", osRelease["ID"])
+
+	// secondly, let's override it with /etc/os-release
+	require.NoError(t,
+		os.WriteFile(path.Join(tree, "etc/os-release"), []byte("ID=kingfisher\n"), 0600),
+	)
+
+	osRelease, err = ReadOSReleaseFromTree(tree)
+	require.NoError(t, err)
+	require.Equal(t, "kingfisher", osRelease["ID"])
+}
+
+func TestReadOSReleaseFromTreeUnhappy(t *testing.T) {
+	tree := t.TempDir()
+
+	_, err := ReadOSReleaseFromTree(tree)
+	require.ErrorContains(t, err, "failed to read os-release")
 }
