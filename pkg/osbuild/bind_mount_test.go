70dbb2217c3203f7db977017f0d2c44a9b0d8c03
Michael Vogt <michael.vogt@gmail.com>
2024-04-12 12:09:15 +0200

    osbuild: add new `org.osbuild.bind` mount module
    See https://github.com/osbuild/osbuild/pull/1711

---
+++ pkg/osbuild/bind_mount_test.go
@@ -0,0 +1,38 @@
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+func TestBindMountSerialized(t *testing.T) {
+	mntStage := osbuild.NewBindMount("some-name", "mount://", "tree://")
+	json, err := json.MarshalIndent(mntStage, "", "  ")
+	require.Nil(t, err)
+	assert.Equal(t, string(json), `
+{
+  "name": "some-name",
+  "type": "org.osbuild.bind",
+  "target": "tree://",
+  "options": {
+    "source": "mount://"
+  }
+}`[1:])
+}
+
+func TestBindMountErrorCheckingSrc(t *testing.T) {
+	assert.PanicsWithError(t, `bind mount source must start with "mount://", got "invalid-src"`, func() {
+		osbuild.NewBindMount("some-name", "invalid-src", "tree://")
+	})
+}
+
+func TestBindMountErrorCheckingDst(t *testing.T) {
+	assert.PanicsWithError(t, `bind mount target must start with "tree://", got "invalid-target"`, func() {
+		osbuild.NewBindMount("some-name", "mount://", "invalid-target")
+	})
+}
