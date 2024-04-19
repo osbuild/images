546a626a142637c31bf25324571f718c5e196e5d
Tomáš Hozza <thozza@redhat.com>
2024-04-19 16:40:30 +0200

    Distro/RHEL/AMI: set the ttyS0 as primary console (RHEL-32885)
    Current RHEL AMIs have two console parameters
    'console=ttyS0,115200n8 console=tty0', which set tty0 as the primary
    console. As a result, we have less information printed than the RHEL
    guest image, Amazon Linux and Ubuntu during the boot. To keep this
    setting consistent with RHEL guest image and make it more useful for
    boot debug, set the ttyS0 as the primary console.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- pkg/distro/rhel/rhel7/ami.go
+++ pkg/distro/rhel/rhel7/ami.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	ec2KernelOptions = "ro console=ttyS0,115200n8 console=tty0 net.ifnames=0 rd.blacklist=nouveau nvme_core.io_timeout=4294967295 crashkernel=auto LANG=en_US.UTF-8"
+	ec2KernelOptions = "ro console=tty0 console=ttyS0,115200n8 net.ifnames=0 rd.blacklist=nouveau nvme_core.io_timeout=4294967295 crashkernel=auto LANG=en_US.UTF-8"
 )
 
 func mkEc2ImgTypeX86_64() *rhel.ImageType {
