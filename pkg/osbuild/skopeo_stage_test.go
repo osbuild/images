4d0192ecb2f29c2ce7b39dc340f052a2fe29f213
Michael Vogt <michael.vogt@gmail.com>
2024-05-03 09:42:38 +0200

    osbuild(skopeo): add `RemoveSignatures` option
    This adds support for the new `remove-signatures` option in the
    osbuild skopeo stage.
    
    See:
    https://github.com/osbuild/osbuild/pull/1752
    https://github.com/osbuild/osbuild/pull/1763

---
+++ pkg/osbuild/skopeo_stage_test.go
@@ -0,0 +1,50 @@
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/internal/common"
+	"github.com/osbuild/images/pkg/container"
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+func TestSkopeoStageJSONSmoke(t *testing.T) {
+	inputs := osbuild.NewContainersInputForSources([]container.Spec{
+		{
+			ImageID:   "id-0",
+			Source:    "registry.example.org/reg/img",
+			LocalName: "local-name",
+		},
+	})
+
+	stage := osbuild.NewSkopeoStageWithOCI("/some/path", inputs, nil)
+	opts := stage.Options.(*osbuild.SkopeoStageOptions)
+	opts.RemoveSignatures = common.ToPtr(true)
+
+	stageJson, err := json.MarshalIndent(stage, "", "  ")
+	assert.NoError(t, err)
+	assert.Equal(t, string(stageJson), `{
+  "type": "org.osbuild.skopeo",
+  "inputs": {
+    "images": {
+      "type": "org.osbuild.containers",
+      "origin": "org.osbuild.source",
+      "references": {
+        "id-0": {
+          "name": "local-name"
+        }
+      }
+    }
+  },
+  "options": {
+    "destination": {
+      "type": "oci",
+      "path": "/some/path"
+    },
+    "remove-signatures": true
+  }
+}`)
+}
