9d35d03c88185aade4ab3175c5d84d1cd7f4dceb
Ondřej Budai <ondrej@budai.cz>
2024-05-10 11:17:08 +0200

    disk: propagate the btrfs UUID to its subvolumes
    Prior this commit, the GenUUID method of Btrfs didn't propagate the
    generated UUID to a subvolume. Thus, when GetFSSpec was called
    on the subvolume, an empty UUID was returned. Thus, invalid
    kernel arguments were generated (for root fs), and the system
    rendered to be unbootable.

--- pkg/disk/partition_table_test.go
+++ pkg/disk/partition_table_test.go
@@ -1,11 +1,14 @@
 package disk_test
 
 import (
+	"math/rand"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
+	"github.com/osbuild/images/internal/common"
 	"github.com/osbuild/images/internal/testdisk"
+	"github.com/osbuild/images/pkg/disk"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPartitionTable_GetMountpointSize(t *testing.T) {
@@ -23,4 +26,57 @@ func TestPartitionTable_GetMountpointSize(t *testing.T) {
 	_, err = pt.GetMountpointSize("/custom")
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "cannot find mountpoint /custom")
+}
+
+func TestPartitionTable_GenerateUUIDs(t *testing.T) {
+	pt := disk.PartitionTable{
+		Type: "gpt",
+		Partitions: []disk.Partition{
+			{
+				Size:     1 * common.MebiByte,
+				Bootable: true,
+				Type:     disk.BIOSBootPartitionGUID,
+				UUID:     disk.BIOSBootPartitionUUID,
+			},
+			{
+				Size: 2 * common.GibiByte,
+				Type: disk.FilesystemDataGUID,
+				Payload: &disk.Filesystem{
+					Type:         "xfs",
+					Label:        "root",
+					Mountpoint:   "/",
+					FSTabOptions: "defaults",
+					FSTabFreq:    0,
+					FSTabPassNo:  0,
+				},
+			},
+			{
+				Size: 10 * common.GibiByte,
+				Payload: &disk.Btrfs{
+					Subvolumes: []disk.BtrfsSubvolume{
+						{
+							Mountpoint: "/var",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Static seed for testing
+	/* #nosec G404 */
+	rnd := rand.New(rand.NewSource(0))
+
+	pt.GenerateUUIDs(rnd)
+
+	// Check that GenUUID doesn't change already defined UUIDs
+	assert.Equal(t, disk.BIOSBootPartitionUUID, pt.Partitions[0].UUID)
+
+	// Check that GenUUID generates fresh UUIDs if not defined prior the call
+	assert.Equal(t, "a178892e-e285-4ce1-9114-55780875d64e", pt.Partitions[1].UUID)
+	assert.Equal(t, "6e4ff95f-f662-45ee-a82a-bdf44a2d0b75", pt.Partitions[1].Payload.(*disk.Filesystem).UUID)
+
+	// Check that GenUUID generates the same UUID for BTRFS and its subvolumes
+	assert.Equal(t, "fb180daf-48a7-4ee0-b10d-394651850fd4", pt.Partitions[2].Payload.(*disk.Btrfs).UUID)
+	assert.Equal(t, "fb180daf-48a7-4ee0-b10d-394651850fd4", pt.Partitions[2].Payload.(*disk.Btrfs).Subvolumes[0].UUID)
 }
