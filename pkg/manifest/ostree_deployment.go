f7f152e3d9cf9015b5a93954b4fcdbad8105eefa
Achilleas Koutsou <achilleas@koutsou.net>
2024-05-13 17:43:48 +0200

    manifest: disable RemainAfterExit for mountpoint service
    With RemainAfterExit enabled, the service is not considered "stopped"
    when the execution ends, so the ExecStopPost is never executed.
    Disabling RemainAfterExit ensures that ExecStopPost is run always,
    even when the execution of the unit fails.

--- pkg/manifest/ostree_deployment.go
+++ pkg/manifest/ostree_deployment.go
@@ -518,7 +518,7 @@ func createMountpointService(serviceName string, mountpoints []string) *osbuild.
 	}
 	service := osbuild.Service{
 		Type:            osbuild.Oneshot,
-		RemainAfterExit: true,
+		RemainAfterExit: false,
 		// compatibility with composefs, will require transient rootfs to be enabled too.
 		ExecStartPre: []string{"/bin/sh -c \"if grep -Uq composefs /run/ostree-booted; then chattr -i /; fi\""},
 		ExecStopPost: []string{"/bin/sh -c \"if grep -Uq composefs /run/ostree-booted; then chattr +i /; fi\""},
