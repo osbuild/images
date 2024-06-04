df582f46ce780222373c7170af62553d40ae3e9a
Michael Vogt <michael.vogt@gmail.com>
2024-06-04 16:02:07 +0200

    New command: otk-make-partition-stages
    New Stages generator for creating the stages that can realise a
    filesystem based on a Partition Table.
    
    This external consumes the data from `otk-gen-partition-table` and
    generate osbuild stages from it.
    
    The expected usage is something like:
    ```yaml
    otk.define:
     modifications:
      filesystem:
        min_disk_size: 20 GiB
    
    otk.define.defaults:
     filesystem:
       otk.external.gen_partition_table:
         modifications: ${modifications.filesystem}
         properties: ...
         partitions: ...
    
    stages:
     ...
     otk.external.make_partition_stages:
       internal: ${filesystem.internal}
       # TODO: add filename here
    ```

---
+++ cmd/otk-make-partition-stages/main_test.go
@@ -0,0 +1,119 @@
+package main_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	makestages "github.com/osbuild/images/cmd/otk-make-partition-stages"
+	"github.com/osbuild/images/pkg/disk"
+)
+
+// this is not symetrical to the output, this is sad but also
+// okay because the input is really just a dump of the internal
+// disk.PartitionTable so encoding it in json here will not add
+// a benefit for the test
+var minimalInputBase = makestages.Input{
+	Internal: makestages.InputInternal{
+		PartitionTable: &disk.PartitionTable{
+			Size: 10738466816,
+			UUID: "0194fdc2-fa2f-4cc0-81d3-ff12045b73c8",
+			Type: "dos",
+			Partitions: []disk.Partition{
+				{
+					Start: 1048576,
+					Size:  10737418240,
+					Payload: &disk.Filesystem{
+						Type:       "ext4",
+						UUID:       "6e4ff95f-f662-45ee-a82a-bdf44a2d0b75",
+						Mountpoint: "/",
+					},
+				},
+			},
+		},
+	},
+}
+
+var minimalExpectedStages = `[
+  {
+    "type": "org.osbuild.truncate",
+    "options": {
+      "filename": "disk.img",
+      "size": "10738466816"
+    }
+  },
+  {
+    "type": "org.osbuild.sfdisk",
+    "options": {
+      "label": "dos",
+      "uuid": "0194fdc2-fa2f-4cc0-81d3-ff12045b73c8",
+      "partitions": [
+        {
+          "size": 20971520,
+          "start": 2048
+        }
+      ]
+    },
+    "devices": {
+      "device": {
+        "type": "org.osbuild.loopback",
+        "options": {
+          "filename": "disk.img",
+          "lock": true
+        }
+      }
+    }
+  },
+  {
+    "type": "org.osbuild.mkfs.ext4",
+    "options": {
+      "uuid": "6e4ff95f-f662-45ee-a82a-bdf44a2d0b75"
+    },
+    "devices": {
+      "device": {
+        "type": "org.osbuild.loopback",
+        "options": {
+          "filename": "disk.img",
+          "start": 2048,
+          "size": 20971520,
+          "lock": true
+        }
+      }
+    }
+  }
+]
+`
+
+func TestIntegration(t *testing.T) {
+	minimalInput := minimalInputBase
+	expectedStages := minimalExpectedStages
+
+	inpJSON, err := json.Marshal(&minimalInput)
+	assert.NoError(t, err)
+	fakeStdin := bytes.NewBuffer(inpJSON)
+	fakeStdout := bytes.NewBuffer(nil)
+
+	err = makestages.Run(fakeStdin, fakeStdout)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedStages, fakeStdout.String())
+}
+
+func TestModificationFname(t *testing.T) {
+	input := minimalInputBase
+	input.Modifications.Filename = "mydisk.img2"
+	expectedStages := strings.Replace(minimalExpectedStages, `"disk.img"`, `"mydisk.img2"`, -1)
+
+	inpJSON, err := json.Marshal(&input)
+	assert.NoError(t, err)
+	fakeStdin := bytes.NewBuffer(inpJSON)
+	fakeStdout := bytes.NewBuffer(nil)
+
+	err = makestages.Run(fakeStdin, fakeStdout)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedStages, fakeStdout.String())
+}
