8b4ef96d82a8c6227d422207e0029207218d971d
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-05 16:47:19 +0200

    New command: otk-make-partition-mounts-devices
    New Mounts and Devices generator for creating the mount and device
    options that can be attached to stages that need to operate on a
    partitioned image file.
    
    The external consumes the data from `otk-gen-partition-table` and
    generates osbuild devices and mounts from it.

---
+++ cmd/otk-make-partition-mounts-devices/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/osbuild/images/internal/otk"
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+type Input struct {
+	Filename string                `json:"filename"`
+	Internal otk.PartitionInternal `json:"internal"`
+}
+
+type Output struct {
+	RootMountName string                    `json:"root_mount_name"`
+	Mounts        []osbuild.Mount           `json:"mounts"`
+	Devices       map[string]osbuild.Device `json:"devices"`
+}
+
+func run(r io.Reader, w io.Writer) error {
+	var inp Input
+	if err := json.NewDecoder(r).Decode(&inp); err != nil {
+		return err
+	}
+
+	rootMntName, mounts, devices, err := osbuild.GenMountsDevicesFromPT(inp.Filename, inp.Internal.PartitionTable)
+	if err != nil {
+		return err
+	}
+
+	out := Output{
+		RootMountName: rootMntName,
+		Mounts:        mounts,
+		Devices:       devices,
+	}
+	outputJson, err := json.MarshalIndent(out, "", "  ")
+	if err != nil {
+		return fmt.Errorf("cannot marshal response: %w", err)
+	}
+	fmt.Fprintf(w, "%s\n", outputJson)
+	return nil
+}
+
+func main() {
+	if err := run(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err.Error())
+		os.Exit(1)
+	}
+}
