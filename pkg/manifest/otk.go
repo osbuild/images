4fcdcd7e91252058b9e0d7e75f9600ab9fee6e33
Achilleas Koutsou <achilleas@koutsou.net>
2024-07-02 18:56:50 +0200

    manifest: add OTKManifest type
    The OTKManifest is a second implementation of the manifest.Manifest
    interface that will be used to generate manifests from otk yaml files.
    The type only implements the Serialize() method.  None of the other
    methods are important (at least for now).  The content spec methods in
    particular are not needed since otk will handle content resolution
    (depsolving, container and ostree commit resolves) itself.

---
+++ pkg/manifest/otk.go
@@ -0,0 +1,56 @@
+package manifest
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+
+	"github.com/osbuild/images/pkg/container"
+	"github.com/osbuild/images/pkg/ostree"
+	"github.com/osbuild/images/pkg/rpmmd"
+)
+
+type OTKManifest struct {
+	// entrypoint yaml file for this otk manifest
+	entrypoint string
+}
+
+func NewOTK(path string) *OTKManifest {
+	return &OTKManifest{entrypoint: path}
+}
+
+func (m *OTKManifest) addPipeline(p Pipeline) {}
+
+func (m *OTKManifest) GetPackageSetChains() map[string][]rpmmd.PackageSet {
+	return nil
+}
+
+func (m *OTKManifest) GetContainerSourceSpecs() map[string][]container.SourceSpec {
+	return nil
+}
+
+func (m *OTKManifest) GetOSTreeSourceSpecs() map[string][]ostree.SourceSpec {
+	return nil
+}
+
+func (m *OTKManifest) Serialize(_ map[string][]rpmmd.PackageSpec, _ map[string][]container.Spec, _ map[string][]ostree.CommitSpec, _ map[string][]rpmmd.RepoConfig) (OSBuildManifest, error) {
+	cmd := exec.Command("otk", "compile", "--target=osbuild", m.entrypoint)
+	var stdoutBuffer bytes.Buffer
+	var stderrBuffer bytes.Buffer
+	cmd.Stdout = &stdoutBuffer
+	cmd.Stderr = &stderrBuffer
+	err := cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("compiling omnifest: %v %s", err, stderrBuffer.String())
+
+	}
+	return OSBuildManifest(stdoutBuffer.Bytes()), nil
+}
+
+func (m *OTKManifest) GetCheckpoints() []string {
+	return nil
+}
+
+func (m *OTKManifest) GetExports() []string {
+	return nil
+}
