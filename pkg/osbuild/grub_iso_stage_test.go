ad7d81ad333de3dad8feed39b7563ffc66d7a011
Achilleas Koutsou <achilleas@koutsou.net>
2024-07-04 11:37:22 +0200

    osbuild: test the grub2.iso stage validation

---
+++ pkg/osbuild/grub_iso_stage_test.go
@@ -0,0 +1,92 @@
+package osbuild_test
+
+import (
+	"testing"
+
+	"github.com/osbuild/images/pkg/osbuild"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGrubIsoStageValidation(t *testing.T) {
+	type testCase struct {
+		options       osbuild.GrubISOStageOptions
+		expectedError string
+	}
+
+	testCases := map[string]testCase{
+		"happy": {
+			options: osbuild.GrubISOStageOptions{
+				Product: osbuild.Product{
+					Name:    "whatever",
+					Version: "42",
+				},
+				Kernel: osbuild.ISOKernel{
+					Dir: "/path",
+				},
+				ISOLabel: "whatever-42-workstation",
+			},
+		},
+		"no product name": {
+			options: osbuild.GrubISOStageOptions{
+				Product: osbuild.Product{
+					Version: "42",
+				},
+				Kernel: osbuild.ISOKernel{
+					Dir: "/path",
+				},
+				ISOLabel: "whatever-42-workstation",
+			},
+			expectedError: "org.osbuild.grub2.iso: product.name option is required",
+		},
+		"no product version": {
+			options: osbuild.GrubISOStageOptions{
+				Product: osbuild.Product{
+					Name: "whatever",
+				},
+				Kernel: osbuild.ISOKernel{
+					Dir: "/path",
+				},
+				ISOLabel: "whatever-42-workstation",
+			},
+			expectedError: "org.osbuild.grub2.iso: product.version option is required",
+		},
+		"no kernel dir": {
+			options: osbuild.GrubISOStageOptions{
+				Product: osbuild.Product{
+					Name:    "whatever",
+					Version: "13",
+				},
+				Kernel:   osbuild.ISOKernel{},
+				ISOLabel: "whatever-42-workstation",
+			},
+			expectedError: "org.osbuild.grub2.iso: kernel.dir option is required",
+		},
+		"no isolabel": {
+			options: osbuild.GrubISOStageOptions{
+				Product: osbuild.Product{
+					Name:    "whatever",
+					Version: "13",
+				},
+				Kernel: osbuild.ISOKernel{
+					Dir: "/doesnt/matter",
+				},
+			},
+			expectedError: "org.osbuild.grub2.iso: isolabel option is required",
+		},
+	}
+
+	for name := range testCases {
+		tc := testCases[name]
+		t.Run(name, func(t *testing.T) {
+			if tc.expectedError == "" {
+				assert.NotPanics(t, func() { osbuild.NewGrubISOStage(&tc.options) })
+			} else {
+				assert.PanicsWithError(
+					t,
+					tc.expectedError,
+					func() { osbuild.NewGrubISOStage(&tc.options) },
+				)
+			}
+		})
+	}
+}
