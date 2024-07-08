761ad8a8aaea9bcba825eb4ec14f2ef9222de4d1
Michael Vogt <michael.vogt@gmail.com>
2024-07-08 09:48:03 +0200

    cmd: add `otk-make-fstab-stage` helper
    This helper is similar to the `otk-make-partition-stages` but for
    the `fstab` generation. It consumes the variables generated by
    `otk-gen-partition-table` and produces a fragment to generate
    the `org.osbuild.fstab` stage.
    
    The expected usage is something like:
    ```yaml
    otk.define:
     filesystem:
       otk.external.gen_partition_table:
         modifications: ${modifications.filesystem}
         properties: ...
         partitions: ...
    
    stages:
     ...
     otk.external.make_fstab_stage:
       internal: ${filesystem.internal}
    ```

---
+++ cmd/otk-make-fstab-stage/main_test.go
@@ -0,0 +1,60 @@
+package main_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	makefstab "github.com/osbuild/images/cmd/otk-make-fstab-stage"
+	"github.com/osbuild/images/internal/otkdisk"
+	"github.com/osbuild/images/internal/testdisk"
+)
+
+// this is not symetrical to the output, this is sad but also
+// okay because the input is really just a dump of the internal
+// disk.PartitionTable so encoding it in json here will not add
+// a benefit for the test
+var minimalInputBase = makefstab.Input{
+	Const: otkdisk.Const{
+		Internal: otkdisk.Internal{
+			PartitionTable: testdisk.MakeFakePartitionTable("/", "/var"),
+		},
+	},
+}
+
+var minimalExpectedStages = `{
+  "type": "org.osbuild.fstab",
+  "options": {
+    "filesystems": [
+      {
+        "uuid": "6264D520-3FB9-423F-8AB8-7A0A8E3D3562",
+        "vfs_type": "ext4",
+        "path": "/"
+      },
+      {
+        "uuid": "CB07C243-BC44-4717-853E-28852021225B",
+        "vfs_type": "ext4",
+        "path": "/var"
+      }
+    ]
+  }
+}
+`
+
+func TestIntegration(t *testing.T) {
+	minimalInput := minimalInputBase
+	minimalInput.Const.Filename = "disk.img"
+	expectedStages := minimalExpectedStages
+
+	inpJSON, err := json.Marshal(&minimalInput)
+	assert.NoError(t, err)
+	fakeStdin := bytes.NewBuffer(inpJSON)
+	fakeStdout := bytes.NewBuffer(nil)
+
+	err = makefstab.Run(fakeStdin, fakeStdout)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedStages, fakeStdout.String())
+}
