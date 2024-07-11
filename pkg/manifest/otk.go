ffaa1a60326ca371621afac4238b661542114450
Achilleas Koutsou <achilleas@koutsou.net>
2024-07-11 22:19:58 +0200

    manifest: create customizations file when serializing
    When serializing an OTKManifest, create a customizations file that
    includes 'otk.define's to represent the options of a blueprint, then
    'otk.include' the original entrypoint to generate a manifest with the
    user customizations included.

--- pkg/manifest/otk.go
+++ pkg/manifest/otk.go
@@ -8,6 +8,7 @@ import (
 	"github.com/osbuild/images/pkg/blueprint"
 	"github.com/osbuild/images/pkg/container"
 	"github.com/osbuild/images/pkg/ostree"
+	"github.com/osbuild/images/pkg/otk"
 	"github.com/osbuild/images/pkg/rpmmd"
 )
 
@@ -37,13 +38,18 @@ func (m *OTKManifest) GetOSTreeSourceSpecs() map[string][]ostree.SourceSpec {
 }
 
 func (m *OTKManifest) Serialize(_ map[string][]rpmmd.PackageSpec, _ map[string][]container.Spec, _ map[string][]ostree.CommitSpec, _ map[string][]rpmmd.RepoConfig) (OSBuildManifest, error) {
-	cmd := exec.Command("otk", "compile", "--target=osbuild", m.entrypoint)
+	customizations, err := otk.NewCustomizationsFile(m.blueprint, m.entrypoint)
+	if err != nil {
+		return nil, err
+	}
+	defer customizations.Cleanup()
+
+	cmd := exec.Command("otk", "compile", "--target=osbuild", customizations.Path())
 	var stdoutBuffer bytes.Buffer
 	var stderrBuffer bytes.Buffer
 	cmd.Stdout = &stdoutBuffer
 	cmd.Stderr = &stderrBuffer
-	err := cmd.Run()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		return nil, fmt.Errorf("compiling omnifest: %v %s", err, stderrBuffer.String())
 
 	}
