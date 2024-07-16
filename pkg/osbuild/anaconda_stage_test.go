717c852481e15f865c2ae5c1c8aa28c7e5ca8074
Achilleas Koutsou <achilleas@koutsou.net>
2024-07-16 18:54:03 +0200

    osbuild: add test for Anaconda stage options creation

---
+++ pkg/osbuild/anaconda_stage_test.go
@@ -0,0 +1,76 @@
+package osbuild_test
+
+import (
+	"testing"
+
+	"github.com/osbuild/images/pkg/osbuild"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAnacondaStageOptions(t *testing.T) {
+
+	type testCase struct {
+		additional []string
+		expected   []string
+	}
+
+	testCases := map[string]testCase{
+		"zero-add": {
+			additional: []string{},
+			expected: []string{
+				"org.fedoraproject.Anaconda.Modules.Payloads",
+				"org.fedoraproject.Anaconda.Modules.Network",
+				"org.fedoraproject.Anaconda.Modules.Storage",
+			},
+		},
+		"duplicate": {
+			additional: []string{
+				"org.fedoraproject.Anaconda.Modules.Payloads",
+				"org.fedoraproject.Anaconda.Modules.Network",
+				"org.fedoraproject.Anaconda.Modules.Storage",
+			},
+			expected: []string{
+				"org.fedoraproject.Anaconda.Modules.Payloads",
+				"org.fedoraproject.Anaconda.Modules.Network",
+				"org.fedoraproject.Anaconda.Modules.Storage",
+				"org.fedoraproject.Anaconda.Modules.Payloads",
+				"org.fedoraproject.Anaconda.Modules.Network",
+				"org.fedoraproject.Anaconda.Modules.Storage",
+			},
+		},
+		"add-users": {
+			additional: []string{
+				"org.fedoraproject.Anaconda.Modules.Users",
+			},
+			expected: []string{
+				"org.fedoraproject.Anaconda.Modules.Payloads",
+				"org.fedoraproject.Anaconda.Modules.Network",
+				"org.fedoraproject.Anaconda.Modules.Storage",
+				"org.fedoraproject.Anaconda.Modules.Users",
+			},
+		},
+		"add-nonsense": {
+			additional: []string{
+				"org.osbuild.not.anaconda.module",
+			},
+			expected: []string{
+				"org.fedoraproject.Anaconda.Modules.Payloads",
+				"org.fedoraproject.Anaconda.Modules.Network",
+				"org.fedoraproject.Anaconda.Modules.Storage",
+				"org.osbuild.not.anaconda.module",
+			},
+		},
+	}
+
+	for name := range testCases {
+		tc := testCases[name]
+		t.Run(name, func(t *testing.T) {
+			require := require.New(t)
+			options := osbuild.NewAnacondaStageOptions(tc.additional)
+
+			require.NotNil(options)
+			require.ElementsMatch(options.KickstartModules, tc.expected)
+		})
+	}
+
+}
