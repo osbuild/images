0addc3ad0717e5c5455bad4217f4428dd40e818b
Achilleas Koutsou <achilleas@koutsou.net>
2024-07-24 17:23:27 +0200

    disk: add relayout test with root at the start of the array
    Regardless of the position of the root partition in the array, its start
    position is always last.

--- pkg/disk/partition_table_internal_test.go
+++ pkg/disk/partition_table_internal_test.go
@@ -321,6 +321,48 @@ func TestRelayout(t *testing.T) {
 				},
 			},
 		},
+		"simple-gpt-root-first": {
+			pt: &PartitionTable{
+				Type: "gpt",
+				Size: 100 * MiB,
+				Partitions: []Partition{
+					{
+						Size: 10 * MiB,
+						Payload: &Filesystem{
+							Mountpoint: "/",
+						},
+					},
+					{
+						Size: 20 * MiB,
+					},
+					{
+						Size: 30 * MiB,
+					},
+				},
+			},
+			size: 100 * MiB,
+			expected: &PartitionTable{
+				Type: "gpt",
+				Size: 100 * MiB,
+				Partitions: []Partition{
+					{
+						Start: 51 * MiB,                                   // root gets moved to last position
+						Size:  49*MiB - (DefaultSectorSize + (128 * 128)), // Grows to fill the space, but gpt adds a footer the same size as the header (unaligned)
+						Payload: &Filesystem{
+							Mountpoint: "/",
+						},
+					},
+					{
+						Start: 1 * MiB, // header (1 sector + 128 B * 128 partitions) aligned up to the default grain (1 MiB)
+						Size:  20 * MiB,
+					},
+					{
+						Start: 21 * MiB, // header (1 sector + 128 B * 128 partitions) aligned up to the default grain (1 MiB)
+						Size:  30 * MiB,
+					},
+				},
+			},
+		},
 	}
 
 	for name := range testCases {
