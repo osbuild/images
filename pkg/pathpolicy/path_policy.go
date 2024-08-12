48a701c29fccc540e6850a3af8cae83271377770
Michael Vogt <michael.vogt@gmail.com>
2024-08-12 11:33:32 +0200

    pathpolicy: tweak error messages and add test
    Trivial tweak for the errors returned from PathPolicies.Check()
    so that they are a bit more user friendly.
    
    See also https://github.com/osbuild/bootc-image-builder/pull/601

--- pkg/pathpolicy/path_policy.go
+++ pkg/pathpolicy/path_policy.go
@@ -26,15 +26,14 @@ func NewPathPolicies(entries map[string]PathPolicy) *PathPolicies {
 
 // Check a given path against the PathPolicies
 func (pol *PathPolicies) Check(fsPath string) error {
-
 	// Quickly check we have a path and it is absolute
 	if fsPath == "" || fsPath[0] != '/' {
-		return fmt.Errorf("path must be absolute")
+		return fmt.Errorf("path %q must be absolute", fsPath)
 	}
 
 	// ensure that only clean paths are valid
 	if fsPath != path.Clean(fsPath) {
-		return fmt.Errorf("path must be canonical")
+		return fmt.Errorf("path %q must be canonical", fsPath)
 	}
 
 	node, left := pol.Lookup(fsPath)
@@ -46,7 +45,7 @@ func (pol *PathPolicies) Check(fsPath string) error {
 	// 1) path is explicitly not allowed or
 	// 2) a subpath was match but an explicit match is required
 	if policy.Deny || (policy.Exact && len(left) > 0) {
-		return fmt.Errorf("path '%s ' is not allowed", fsPath)
+		return fmt.Errorf("path %q is not allowed", fsPath)
 	}
 
 	// exact match or recursive path allowed
