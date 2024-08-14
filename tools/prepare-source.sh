82f7807b8670864424354669b8fa53a1a460b920
Achilleas Koutsou <achilleas@koutsou.net>
2024-08-14 15:45:55 +0200

    tools: drop 'go mod vendor' from prepare-source.sh

--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -12,7 +12,6 @@ $GO_BINARY download
 
 # Ensure that go.mod and go.sum are up to date.
 $GO_BINARY mod tidy
-$GO_BINARY mod vendor
 
 # Ensure the code is formatted correctly.
 $GO_BINARY fmt ./...
