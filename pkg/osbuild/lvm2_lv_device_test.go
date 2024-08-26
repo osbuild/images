85d159deea5c6f469aa073012e6f13caaccef440
Michael Vogt <michael.vogt@gmail.com>
2024-08-26 16:15:16 +0200

    osbuild: add new `vg_partum` to `LVM2LVDeviceOptions`
    Add support for the new `vg_partnum` parameter for the
    `org.osbuild.lvm2.lv` devices. This allows to mount the logical
    volume from a single underlying loop device which is required
    for `bootc install to-filesystem/bootupd`.
    
    See https://github.com/osbuild/osbuild/pull/1867

--- pkg/osbuild/lvm2_lv_device_test.go
+++ pkg/osbuild/lvm2_lv_device_test.go
@@ -1,1 +1,45 @@
-package osbuild
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/internal/common"
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+func TestLVM2LVDeviceMarshal(t *testing.T) {
+	opts := &osbuild.LVM2LVDeviceOptions{Volume: "some-volume"}
+	dev := osbuild.NewLVM2LVDevice("some-parent", opts)
+	b, err := json.MarshalIndent(dev, "", " ")
+	assert.NoError(t, err)
+	expectedJSON := `{
+ "type": "org.osbuild.lvm2.lv",
+ "parent": "some-parent",
+ "options": {
+  "volume": "some-volume"
+ }
+}`
+	assert.Equal(t, expectedJSON, string(b))
+}
+
+func TestLVM2LVDeviceMarshalWithVGPartition(t *testing.T) {
+	opts := &osbuild.LVM2LVDeviceOptions{
+		Volume:    "some-volume",
+		VGPartnum: common.ToPtr(4),
+	}
+	dev := osbuild.NewLVM2LVDevice("some-parent", opts)
+	b, err := json.MarshalIndent(dev, "", " ")
+	assert.NoError(t, err)
+	expectedJSON := `{
+ "type": "org.osbuild.lvm2.lv",
+ "parent": "some-parent",
+ "options": {
+  "volume": "some-volume",
+  "vg_partnum": 4
+ }
+}`
+	assert.Equal(t, expectedJSON, string(b))
+}
