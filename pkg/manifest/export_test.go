d90a1639d71c8ca8a4d917abc7cfff350e29f4c2
Michael Vogt <michael.vogt@gmail.com>
2024-09-17 16:58:46 +0200

    manifest: add `transform` to manifest.Tar`
    This commit adds the new osbuild.TarStageOptions option `transform`
    to the `manifest.Tar` object so that it can be used by the
    `image.BootcDiskImage` later.

--- pkg/manifest/export_test.go
+++ pkg/manifest/export_test.go
@@ -1,3 +1,11 @@
 package manifest
 
+import (
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
 var FindStage = findStage
+
+func (p *Tar) Serialize() osbuild.Pipeline {
+	return p.serialize()
+}
---
+++ pkg/manifest/tar_test.go
@@ -0,0 +1,35 @@
+package manifest_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/pkg/manifest"
+	"github.com/osbuild/images/pkg/osbuild"
+	"github.com/osbuild/images/pkg/runner"
+)
+
+func TestTarSerialize(t *testing.T) {
+	mani := manifest.New()
+	runner := &runner.Linux{}
+	build := manifest.NewBuild(&mani, runner, nil, nil)
+
+	// setup
+	rawImage := manifest.NewRawImage(build, nil)
+	tarPipeline := manifest.NewTar(build, rawImage, "tar-pipeline")
+	tarPipeline.SetFilename("filename.tar")
+	tarPipeline.Transform = "s/foo/bar"
+
+	// run
+	osbuildPipeline := tarPipeline.Serialize()
+
+	// assert
+	assert.Equal(t, "tar-pipeline", osbuildPipeline.Name)
+	assert.Equal(t, 1, len(osbuildPipeline.Stages))
+	tarStage := osbuildPipeline.Stages[0]
+	assert.Equal(t, &osbuild.TarStageOptions{
+		Filename:  "filename.tar",
+		Transform: "s/foo/bar",
+	}, tarStage.Options.(*osbuild.TarStageOptions))
+}
