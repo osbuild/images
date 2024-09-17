5ecdeea9c08e39d13699391ae660b5c5016dc677
Michael Vogt <michael.vogt@gmail.com>
2024-09-17 17:14:47 +0200

    many: extract newGCETarPipelineForImg() helper
    This commit extracts a common helper to setup the right tar
    options for the GCE tar file generation. The options that need
    to be passed to tar for GCE need to be exactly right so let's
    make sure we only have a single place where they are set.

---
+++ pkg/image/gce_test.go
@@ -0,0 +1,33 @@
+package image
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/pkg/manifest"
+	"github.com/osbuild/images/pkg/osbuild"
+	"github.com/osbuild/images/pkg/rpmmd"
+	"github.com/osbuild/images/pkg/runner"
+)
+
+func TestNewGCETarPipeline(t *testing.T) {
+	var repos []rpmmd.RepoConfig
+	m := &manifest.Manifest{}
+	runner := &runner.Fedora{}
+
+	buildPipeline := manifest.NewBuild(m, runner, repos, nil)
+	buildPipeline.Checkpoint()
+
+	imgPipeline := manifest.NewRawImage(buildPipeline, nil)
+	imgPipeline.SetFilename("foo.img")
+
+	tar := newGCETarPipelineForImg(buildPipeline, imgPipeline, "my-test")
+	assert.NotNil(t, tar)
+	assert.Equal(t, tar.Format, osbuild.TarArchiveFormatOldgnu)
+	assert.Equal(t, tar.RootNode, osbuild.TarRootNodeOmit)
+	assert.Equal(t, *tar.ACLs, false)
+	assert.Equal(t, *tar.SELinux, false)
+	assert.Equal(t, *tar.Xattrs, false)
+	assert.Equal(t, tar.Transform, `s/foo\.img/disk.raw/`)
+}
