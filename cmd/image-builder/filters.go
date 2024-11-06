ac0dd397c6a405b6bc67eb7b3a821f1b3c62e97d
Michael Vogt <michael.vogt@gmail.com>
2024-11-06 17:10:05 +0100

    fixup! rebase on distrofilter branch

--- cmd/image-builder/filters.go
+++ cmd/image-builder/filters.go
@@ -2,30 +2,32 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/sirupsen/logrus"
-
-	"github.com/osbuild/images/pkg/distro"
 	"github.com/osbuild/images/pkg/distrofactory"
-	"github.com/osbuild/images/pkg/distrosort"
+	"github.com/osbuild/images/pkg/imagefilter"
 )
 
-// XXX: move this file into distro factory?
-
-type FilterResult struct {
-	Distro  distro.Distro
-	Arch    distro.Arch
-	ImgType distro.ImageType
+func newImageFilterDefault() (*imagefilter.ImageFilter, error) {
+	fac := distrofactory.NewDefault()
+	repos, err := newRepoRegistry()
+	if err != nil {
+		return nil, err
+	}
+	return imagefilter.New(fac, repos)
 }
 
-func getOneImage(distroName, imgTypeStr, archStr string) (*FilterResult, error) {
+func getOneImage(distroName, imgTypeStr, archStr string) (*imagefilter.Result, error) {
+	imageFilter, err := newImageFilterDefault()
+	if err != nil {
+		return nil, err
+	}
+
 	filterExprs := []string{
 		fmt.Sprintf("name:%s", distroName),
 		fmt.Sprintf("arch:%s", archStr),
 		fmt.Sprintf("type:%s", imgTypeStr),
 	}
-	filteredResults, err := getFilteredImages(filterExprs)
+	filteredResults, err := imageFilter.Filter(filterExprs...)
 	if err != nil {
 		return nil, err
 	}
@@ -37,116 +39,4 @@ func getOneImage(distroName, imgTypeStr, archStr string) (*FilterResult, error) 
 	default:
 		return nil, fmt.Errorf("internal error: found %v results for %s %s %s", len(filteredResults), distroName, imgTypeStr, archStr)
 	}
-}
-
-// XXX: rename FilterDistros to FilterImages(?)
-func FilterDistros(fac *distrofactory.Factory, distroNames []string, filters Filters) ([]FilterResult, error) {
-	var res []FilterResult
-
-	if err := distrosort.Names(distroNames); err != nil {
-		return nil, err
-	}
-	for _, distroName := range distroNames {
-		distro := fac.GetDistro(distroName)
-		if distro == nil {
-			logrus.Debugf("skipping %v: has repositories but unsupported", distroName)
-		}
-		for _, archName := range distro.ListArches() {
-			a, err := distro.GetArch(archName)
-			if err != nil {
-				return nil, err
-			}
-			for _, imgTypeName := range a.ListImageTypes() {
-				imgType, err := a.GetImageType(imgTypeName)
-				if err != nil {
-					return nil, err
-				}
-				if filters.Matches(distro, a, imgType) {
-					res = append(res, FilterResult{distro, a, imgType})
-				}
-			}
-		}
-	}
-
-	return res, nil
-}
-
-// Filters is a way to filter a list of distros
-type Filters []filter
-
-// NewFilters creates a filtering for a list of distros
-func NewFilters(sl []string) (Filters, error) {
-	var filters []filter
-	for _, s := range sl {
-		l := strings.SplitN(s, ":", 2)
-		switch l[0] {
-		case s:
-			filters = append(filters, &distroNameFilter{
-				filter: l[0],
-				exact:  false,
-			})
-		case "name":
-			filters = append(filters, &distroNameFilter{l[1], true})
-		case "arch":
-			filters = append(filters, &archFilter{l[1]})
-		case "type":
-			filters = append(filters, &imgTypeFilter{l[1]})
-			// mostly here to show how powerful this is
-		case "bootmode":
-			filters = append(filters, &bootmodeFilter{l[1]})
-		default:
-			return nil, fmt.Errorf("unsupported filter prefix: %q", l[0])
-		}
-	}
-	return filters, nil
-}
-
-// Matches returns true if the given (distro,arch,imgType) tuple matches
-// the filter expressions
-func (fl Filters) Matches(distro distro.Distro, arch distro.Arch, imgType distro.ImageType) bool {
-	matches := true
-	for _, f := range fl {
-		matches = matches && f.Matches(distro, arch, imgType)
-	}
-	return matches
-}
-
-type filter interface {
-	Matches(distro distro.Distro, arch distro.Arch, imgType distro.ImageType) bool
-}
-
-type distroNameFilter struct {
-	filter string
-	exact  bool
-}
-
-func (d *distroNameFilter) Matches(distro distro.Distro, arch distro.Arch, imgType distro.ImageType) bool {
-	if d.exact {
-		return distro.Name() == d.filter
-	}
-	return strings.Contains(distro.Name(), d.filter)
-}
-
-type archFilter struct {
-	filter string
-}
-
-func (d *archFilter) Matches(distro distro.Distro, arch distro.Arch, imgType distro.ImageType) bool {
-	return strings.Contains(arch.Name(), d.filter)
-}
-
-type imgTypeFilter struct {
-	filter string
-}
-
-func (d *imgTypeFilter) Matches(distro distro.Distro, arch distro.Arch, imgType distro.ImageType) bool {
-	return strings.Contains(imgType.Name(), d.filter)
-}
-
-type bootmodeFilter struct {
-	filter string
-}
-
-func (d *bootmodeFilter) Matches(distro distro.Distro, arch distro.Arch, imgType distro.ImageType) bool {
-	return imgType.BootMode().String() == d.filter
 }
--- cmd/image-builder/list_images.go
+++ cmd/image-builder/list_images.go
@@ -2,25 +2,15 @@ package main
 
 import (
 	"io"
-
-	"github.com/osbuild/images/pkg/distrofactory"
 )
 
-func getFilteredImages(filterExprs []string) ([]FilterResult, error) {
-	repos, err := newRepoRegistry()
-	if err != nil {
-		return nil, err
-	}
-	filters, err := NewFilters(filterExprs)
-	if err != nil {
-		return nil, err
-	}
-	fac := distrofactory.NewDefault()
-	return FilterDistros(fac, repos.ListDistros(), filters)
-}
-
 func listImages(out io.Writer, format string, filterExprs []string) error {
-	filteredResult, err := getFilteredImages(filterExprs)
+	imageFilter, err := newImageFilterDefault()
+	if err != nil {
+		return err
+	}
+
+	filteredResult, err := imageFilter.Filter(filterExprs...)
 	if err != nil {
 		return err
 	}
