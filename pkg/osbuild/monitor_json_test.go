5ecf4b58587abec9a0e1c6b02ab123d9fb91da92
Michael Vogt <michael.vogt@gmail.com>
2024-11-15 15:56:55 +0100

    osbuild: add support for jsonseq monitor/status message
    This commit adds support for scanning/parsing the jsonseq based
    monitoring that osbuild provides. This is useful for machine
    readable status reporting and progress information for e.g.
    `bootc-image-builder`.
    
    The API consists of a new `osbuild.Status` struct that contains
    parsed details about the osbuild status/progress and a scanner
    that can scan a stream of osbuild jsonseq based progress and
    emit `osbuild.Status` structs.
    
    Note that the status is broadly of two categories:
    1. information amining at a user interface (like
       `status.Message` or `status.Progress`)
    2. low-level details for logging/error reporting (like
       `status.Trace` and `status.Timestamp`
    
    Note that error handline a bit weak right now. The consumer
    of the API will not get an error via the monitor currently
    but only know that something went wrong from osbuild exiting
    and then the consumer needs to reconstruct the error log from
    the trace messages. This is not a show-stopper but we could
    improve this by merging https://github.com/osbuild/osbuild/pull/1831
    and adding `Result` to the `statusJSON` and `Status` (but this
    needs more thinking and probably best done in followups).
    
    See https://github.com/osbuild/bootc-image-builder/compare/main...mvo5:progress3?expand=1 how this will be used.

---
+++ pkg/osbuild/monitor_json_test.go
@@ -0,0 +1,56 @@
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+const osbuildMonitorJSON_1 = `
+{
+  "message": "Top level message",
+  "context": {
+    "origin": "osbuild.monitor",
+    "pipeline": {
+      "name": "source org.osbuild.curl",
+      "id": "598849389c35f93efe2412446f5ca6919434417b9bcea040ea5f9203de81db2c",
+      "stage": {}
+    },
+    "id": "69816755441434713b7567970edfdd42d58193f163e1fdd506274d52246e87f2"
+  },
+  "timestamp": 1731585664.9090264,
+  "progress": {
+    "name": "name",
+    "total": 4,
+    "done": 1,
+    "progress": {
+      "name": "nested-name",
+      "total": 8,
+      "done": 2,
+      "progress": {
+        "name": "nested-nested-name",
+        "total": 16,
+        "done": 4
+      }
+    }
+  }
+}`
+
+func TestOsbuildStatusNestingWorks(t *testing.T) {
+	var status osbuild.StatusJSON
+
+	err := json.Unmarshal([]byte(osbuildMonitorJSON_1), &status)
+	assert.NoError(t, err)
+	assert.Equal(t, "Top level message", status.Message)
+	assert.Equal(t, "name", status.Progress.Name)
+	assert.Equal(t, "69816755441434713b7567970edfdd42d58193f163e1fdd506274d52246e87f2", status.Context.ID)
+	assert.Equal(t, 4, status.Progress.Total)
+	assert.Equal(t, "nested-name", status.Progress.SubProgress.Name)
+	assert.Equal(t, 8, status.Progress.SubProgress.Total)
+	assert.Equal(t, "nested-nested-name", status.Progress.SubProgress.SubProgress.Name)
+	assert.Equal(t, 16, status.Progress.SubProgress.SubProgress.Total)
+	assert.Nil(t, status.Progress.SubProgress.SubProgress.SubProgress)
+}
