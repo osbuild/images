c1b1b9c6fd495127f13064122356fc16a30d429c
Michael Vogt <michael.vogt@gmail.com>
2024-11-18 17:35:50 +0100

    datasizes: add new `Size` type with json/toml decoding support
    This commit adds a new `datasizes.Size` type that is an alias
    for uint64 but has supports direct json/toml decoding. So sizes
    can be specified as 123, "123" or "123 GiB" and this is transparently
    handled.
    
    datasizes: tweak error return to avoid "stuttering"
    
    The `datasizes.Size.UnmarshalJSON()` returns a `JSON unmarshal`
    prefix. However this is a bit too generic as other (nested)
    unmarshalers may also use the prefix. So instead just indicate
    the type in the error string itself:
    ```
    error decoding {TOML,JSON} size: ...
    ```

---
+++ pkg/datasizes/size_test.go
@@ -0,0 +1,122 @@
+package datasizes_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/BurntSushi/toml"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/pkg/datasizes"
+)
+
+func TestSizeUnmarshalTOMLUnhappy(t *testing.T) {
+	cases := []struct {
+		name  string
+		input string
+		err   string
+	}{
+		{
+			name:  "wrong datatype/bool",
+			input: `size = true`,
+			err:   `toml: line 1 (last key "size"): error decoding TOML size: failed to convert value "true" to number`,
+		},
+		{
+			name:  "wrong datatype/float",
+			input: `size = 3.14`,
+			err:   `toml: line 1 (last key "size"): error decoding TOML size: cannot be float`,
+		},
+		{
+			name:  "wrong unit",
+			input: `size = "20 KG"`,
+			err:   `toml: line 1 (last key "size"): error decoding TOML size: unknown data size units in string: 20 KG`,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var v struct {
+				Size datasizes.Size `toml:"size"`
+			}
+			err := toml.Unmarshal([]byte(tc.input), &v)
+			assert.EqualError(t, err, tc.err, tc.input)
+		})
+	}
+}
+
+func TestSizeUnmarshalJSONUnhappy(t *testing.T) {
+	cases := []struct {
+		name  string
+		input string
+		err   string
+	}{
+		{
+			name:  "misize nor string nor int",
+			input: `{"size": true}`,
+			err:   `error decoding JSON size: failed to convert value "true" to number`,
+		},
+		{
+			name:  "wrong datatype/float",
+			input: `{"size": 3.14}`,
+			err:   `error decoding JSON size: strconv.ParseInt: parsing "3.14": invalid syntax`,
+		},
+		{
+			name:  "misize not parseable",
+			input: `{"size": "20 KG"}`,
+			err:   `error decoding JSON size: unknown data size units in string: 20 KG`,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var v struct {
+				Size datasizes.Size `json:"size"`
+			}
+			err := json.Unmarshal([]byte(tc.input), &v)
+			assert.EqualError(t, err, tc.err, tc.input)
+		})
+	}
+}
+
+func TestSizeUnmarshalHappy(t *testing.T) {
+	cases := []struct {
+		name      string
+		inputJSON string
+		inputTOML string
+		expected  datasizes.Size
+	}{
+		{
+			name:      "int",
+			inputJSON: `{"size": 1234}`,
+			inputTOML: `size = 1234`,
+			expected:  1234,
+		},
+		{
+			name:      "str",
+			inputJSON: `{"size": "1234"}`,
+			inputTOML: `size = "1234"`,
+			expected:  1234,
+		},
+		{
+			name:      "str/with-unit",
+			inputJSON: `{"size": "1234 MiB"}`,
+			inputTOML: `size = "1234 MiB"`,
+			expected:  1234 * datasizes.MiB,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var v struct {
+				Size datasizes.Size `json:"size" toml:"size"`
+			}
+			err := toml.Unmarshal([]byte(tc.inputTOML), &v)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, v.Size, tc.inputTOML)
+			err = json.Unmarshal([]byte(tc.inputJSON), &v)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, v.Size, tc.inputJSON)
+		})
+	}
+}
