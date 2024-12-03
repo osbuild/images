b64ba97cb23cfcfe6e3f2e98d64078a8e68ff4f5
Ondřej Budai <ondrej@budai.cz>
2024-12-03 11:08:24 +0100

    distro: add tests for ESP partition availability
    TestESP checks whether all images marked as UEFI or hybrid bootable have
    an ESP partition in their base partition table. The opposite is
    checked as well: legacy-only images shall not have an ESP partition.
    
    The code is deduplicated as much as possible. It meant adding a proxy
    function argument to the shared TestESP function because partition
    tables are not a public interface, but it's imho good enough.
    
    Maybe we can go one step further once the GetPartitionTable interface
    is unified across rhel and fedora, and just make it public. Maybe.

---
+++ pkg/distro/fedora/distro_internal_test.go
@@ -0,0 +1,28 @@
+package fedora
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/osbuild/images/pkg/blueprint"
+	"github.com/osbuild/images/pkg/disk"
+	"github.com/osbuild/images/pkg/distro"
+	"github.com/osbuild/images/pkg/distro/distro_test_common"
+)
+
+// math/rand is good enough in this case
+/* #nosec G404 */
+var rng = rand.New(rand.NewSource(0))
+
+func TestESP(t *testing.T) {
+	var distros []distro.Distro
+	for _, distroName := range []string{"fedora-40", "fedora-41", "fedora-42"} {
+		d := DistroFactory(distroName)
+		distros = append(distros, d)
+	}
+
+	distro_test_common.TestESP(t, distros, func(i distro.ImageType) (*disk.PartitionTable, error) {
+		it := i.(*imageType)
+		return it.getPartitionTable(&blueprint.Customizations{}, distro.ImageOptions{}, rng)
+	})
+}
