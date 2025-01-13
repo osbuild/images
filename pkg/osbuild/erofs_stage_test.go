85183d3e9a184629837cbc9393f1cd71d01bf080
Michael Vogt <michael.vogt@gmail.com>
2025-01-13 09:00:40 +0100

    osbuild: add minimal json test for the erofs stage
    This commit adds a minimal json test for the erof stage to ensure
    that the json struct tags are correct and to also in the
    "test-as-documentation" sense to hint at what the json looks like.

---
+++ pkg/osbuild/erofs_stage_test.go
@@ -0,0 +1,83 @@
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/internal/common"
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+func TestErofStageJsonMinimal(t *testing.T) {
+	expectedJson := `{
+        "type": "org.osbuild.erofs",
+        "inputs": {
+                "tree": {
+                        "type": "org.osbuild.tree",
+                        "origin": "org.osbuild.pipeline",
+                        "references": [
+                                "name:input-pipeline"
+                        ]
+                }
+        },
+        "options": {
+                "filename": "foo.ero"
+        }
+}`
+
+	opts := &osbuild.ErofsStageOptions{
+		Filename: "foo.ero",
+	}
+	stage := osbuild.NewErofsStage(opts, "input-pipeline")
+	require.NotNil(t, stage)
+
+	json, err := json.MarshalIndent(stage, "", "        ")
+	require.Nil(t, err)
+	assert.Equal(t, string(json), expectedJson)
+}
+
+func TestErofStageJsonFull(t *testing.T) {
+	expectedJson := `{
+        "type": "org.osbuild.erofs",
+        "inputs": {
+                "tree": {
+                        "type": "org.osbuild.tree",
+                        "origin": "org.osbuild.pipeline",
+                        "references": [
+                                "name:input-pipeline"
+                        ]
+                }
+        },
+        "options": {
+                "filename": "foo.ero",
+                "compression": {
+                        "method": "lz4hc",
+                        "level": 9
+                },
+                "options": [
+                        "all-fragments",
+                        "dedupe"
+                ],
+                "cluster-size": 131072
+        }
+}`
+
+	opts := &osbuild.ErofsStageOptions{
+		Filename: "foo.ero",
+		Compression: &osbuild.ErofsCompression{
+			Method: "lz4hc",
+			Level:  common.ToPtr(9),
+		},
+		ExtendedOptions: []string{"all-fragments", "dedupe"},
+		ClusterSize:     common.ToPtr(131072),
+	}
+	stage := osbuild.NewErofsStage(opts, "input-pipeline")
+	require.NotNil(t, stage)
+
+	json, err := json.MarshalIndent(stage, "", "        ")
+	require.Nil(t, err)
+	assert.Equal(t, string(json), expectedJson)
+}
