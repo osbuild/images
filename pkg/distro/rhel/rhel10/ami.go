9e334dd7dcf1021e4f00eb9183fb6ea36739885f
Achilleas Koutsou <achilleas@koutsou.net>
2025-01-16 16:42:04 +0100

    distro/rhel10: all image locales are C.UTF-8 by default
    Default to C.UTF-8 for all disk image locales for RHEL 10.

--- pkg/distro/rhel/rhel10/ami.go
+++ pkg/distro/rhel/rhel10/ami.go
@@ -166,7 +166,7 @@ const (
 // default EC2 images config (common for all architectures)
 func defaultEc2ImageConfig() *distro.ImageConfig {
 	return &distro.ImageConfig{
-		Locale:   common.ToPtr("en_US.UTF-8"),
+		Locale:   common.ToPtr("C.UTF-8"),
 		Timezone: common.ToPtr("UTC"),
 		TimeSynchronization: &osbuild.ChronyStageOptions{
 			Servers: []osbuild.ChronyConfigServer{
