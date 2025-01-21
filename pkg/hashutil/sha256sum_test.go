db1dd645eaa1a3f50def89de5cc114b1ecf01f72
Michael Vogt <michael.vogt@gmail.com>
2025-01-21 12:28:16 +0100

    hashutil: add new package and `hashutil.Sha256sum` helper
    This commit adds a convenience package `hashutil` with a first
    `Sha256sum` helper that can be used like the "sha256sum" cmdline
    util. It will generate the hex encoded sha256 sum of the input
    file.

---
+++ pkg/hashutil/sha256sum_test.go
@@ -0,0 +1,43 @@
+package hashutil_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/pkg/hashutil"
+)
+
+func TestSha256sum(t *testing.T) {
+	for _, tc := range []struct {
+		inp      string
+		expected string
+	}{
+		// test vectors from
+		// https://www.di-mgt.com.au/sha_testvectors.html
+		{
+			"", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+		}, {
+			"abc", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+		}, {
+			"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+			"248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+		},
+	} {
+		t.Run(tc.inp, func(t *testing.T) {
+			tmpf := filepath.Join(t.TempDir(), "inp.txt")
+			err := os.WriteFile(tmpf, []byte(tc.inp), 0644)
+			assert.NoError(t, err)
+			hash, err := hashutil.Sha256sum(tmpf)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, hash)
+		})
+	}
+}
+
+func TestSha256sumError(t *testing.T) {
+	_, err := hashutil.Sha256sum("non-existing-file")
+	assert.EqualError(t, err, `open non-existing-file: no such file or directory`)
+}
