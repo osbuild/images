110a8a960761ca2a5c9093efcdc93988e2306c43
Simon de Vlieger <supakeen@redhat.com>
2025-01-30 13:00:16 +0100

    rpmmd: modularity structs
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- pkg/rpmmd/repository.go
+++ pkg/rpmmd/repository.go
@@ -199,6 +199,28 @@ type PackageInfo struct {
 	Dependencies []PackageSpec  `json:"dependencies,omitempty"`
 }
 
+type ModuleSpec struct {
+	ModuleConfigFile ModuleConfigFile   `json:"module-file"`
+	FailsafeFile     ModuleFailsafeFile `json:"failsafe-file"`
+}
+
+type ModuleConfigFile struct {
+	Path string           `json:"path"`
+	Data ModuleConfigData `json:"data"`
+}
+
+type ModuleConfigData struct {
+	Name     string   `json:"name"`
+	Stream   string   `json:"stream"`
+	Profiles []string `json:"profiles"`
+	State    string   `json:"state"`
+}
+
+type ModuleFailsafeFile struct {
+	Path string `json:"path"`
+	Data string `json:"data"`
+}
+
 // GetEVRA returns the package's Epoch:Version-Release.Arch string
 func (ps *PackageSpec) GetEVRA() string {
 	if ps.Epoch == 0 {
