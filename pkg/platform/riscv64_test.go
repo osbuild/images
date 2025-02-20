7bb198e4bab5b379946417e53e3b9cf1f1cb0d9b
Michael Vogt <michael.vogt@gmail.com>
2025-02-20 11:40:18 +0100

    platform: add UEFIVendor to the riscv64 platform
    This is prep work to boot a riscv64 system.

--- pkg/platform/riscv64_test.go
+++ pkg/platform/riscv64_test.go
@@ -13,4 +13,14 @@ func TestPlatformRiscv64Arch(t *testing.T) {
 	platform := &platform.RISCV64{}
 
 	assert.Equal(t, arch.ARCH_RISCV64, platform.GetArch())
+	assert.Equal(t, 0, len(platform.GetPackages()))
+}
+
+func TestPlatformRiscv64UefiVendor(t *testing.T) {
+	platform := &platform.RISCV64{
+		UEFIVendor: "fedora",
+	}
+
+	assert.Equal(t, "fedora", platform.GetUEFIVendor())
+	assert.Contains(t, platform.GetPackages(), "dracut-config-generic")
 }
