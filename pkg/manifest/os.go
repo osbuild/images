57731aa7a01a13aca24a5edd20b8b416c4ebcd49
Simon de Vlieger <supakeen@redhat.com>
2025-02-25 17:25:38 +0100

    manifest: don't write locale when unset
    Don't write out the locale file to disk when it is unset in the
    OSCustomizations.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- pkg/manifest/os.go
+++ pkg/manifest/os.go
@@ -480,7 +480,9 @@ func (p *OS) serialize() osbuild.Pipeline {
 		}
 	}
 
-	pipeline.AddStage(osbuild.NewLocaleStage(&osbuild.LocaleStageOptions{Language: p.Language}))
+	if p.Language != "" {
+		pipeline.AddStage(osbuild.NewLocaleStage(&osbuild.LocaleStageOptions{Language: p.Language}))
+	}
 
 	if p.Keyboard != nil {
 		keymapOptions := &osbuild.KeymapStageOptions{Keymap: *p.Keyboard}
