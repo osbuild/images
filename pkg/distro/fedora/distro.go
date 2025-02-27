282091cd7e1d366206872165e70ca49feb72bd2d
Simon de Vlieger <supakeen@redhat.com>
2025-02-27 21:36:23 +0100

    fedora: machine-id for iot
    Don't turn off the regeneration of the machine id for the iot image
    types, it breaks in rpm-ostree.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- pkg/distro/fedora/distro.go
+++ pkg/distro/fedora/distro.go
@@ -127,8 +127,9 @@ func mkIotCommitImgType(d distribution) imageType {
 			osPkgsKey: packageSetLoader,
 		},
 		defaultImageConfig: &distro.ImageConfig{
-			EnabledServices: iotServicesForVersion(&d),
-			DracutConf:      []*osbuild.DracutConfStageOptions{osbuild.FIPSDracutConfStageOptions},
+			EnabledServices:        iotServicesForVersion(&d),
+			DracutConf:             []*osbuild.DracutConfStageOptions{osbuild.FIPSDracutConfStageOptions},
+			MachineIdUninitialized: common.ToPtr(false),
 		},
 		rpmOstree:              true,
 		image:                  iotCommitImage,
@@ -146,6 +147,9 @@ func mkIotBootableContainer(d distribution) imageType {
 		mimeType: "application/x-tar",
 		packageSets: map[string]packageSetFunc{
 			osPkgsKey: packageSetLoader,
+		},
+		defaultImageConfig: &distro.ImageConfig{
+			MachineIdUninitialized: common.ToPtr(false),
 		},
 		rpmOstree:              true,
 		image:                  bootableContainerImage,
@@ -169,8 +173,9 @@ func mkIotOCIImgType(d distribution) imageType {
 			},
 		},
 		defaultImageConfig: &distro.ImageConfig{
-			EnabledServices: iotServicesForVersion(&d),
-			DracutConf:      []*osbuild.DracutConfStageOptions{osbuild.FIPSDracutConfStageOptions},
+			EnabledServices:        iotServicesForVersion(&d),
+			DracutConf:             []*osbuild.DracutConfStageOptions{osbuild.FIPSDracutConfStageOptions},
+			MachineIdUninitialized: common.ToPtr(false),
 		},
 		rpmOstree:              true,
 		bootISO:                false,
