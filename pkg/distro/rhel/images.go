2cd165506a59722a97f17592636551600d08d9c7
Brian C. Lane <bcl@redhat.com>
2025-03-04 15:13:53 -0800

    image: Add ISOBoot to NewAnacondaOSTreeInstaller
    On Fedora select Grub2ISOBoot when building on x86, otherwise the ISO is
    UEFI only.
    
    On RHEL x86 builds use Grub2ISOBoot for RHEL10, SyslinuxISOBoot for
    other versions and on non-x86 use UEFI only.

--- pkg/distro/rhel/images.go
+++ pkg/distro/rhel/images.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 
 	"github.com/osbuild/images/internal/workload"
+	"github.com/osbuild/images/pkg/arch"
 	"github.com/osbuild/images/pkg/blueprint"
 	"github.com/osbuild/images/pkg/container"
 	"github.com/osbuild/images/pkg/customizations/anaconda"
@@ -510,6 +511,17 @@ func EdgeInstallerImage(workload workload.Workload,
 	img.RootfsCompression = "xz"
 	if t.Arch().Distro().Releasever() == "10" {
 		img.RootfsType = manifest.SquashfsRootfs
+	}
+
+	// Enable BIOS iso on x86_64 only
+	// Use grub2 on RHEL10, otherwise use syslinux
+	// NOTE: Will need to be updated for RHEL11 and later
+	if img.Platform.GetArch() == arch.ARCH_X86_64 {
+		if t.Arch().Distro().Releasever() == "10" {
+			img.ISOBoot = manifest.Grub2ISOBoot
+		} else {
+			img.ISOBoot = manifest.SyslinuxISOBoot
+		}
 	}
 
 	installerConfig, err := t.getDefaultInstallerConfig()
