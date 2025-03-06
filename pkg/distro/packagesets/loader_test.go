d36304e42df4cdebd64ed57898fc8e104c6c5600
Michael Vogt <michael.vogt@gmail.com>
2025-03-06 13:56:44 +0100

    packagesets: add basic imagetype yaml to merge pkgSets
    This commit adds a basic imagetype yaml to support merging
    of package sets  This is a useful feature so that we can e.g.
    avoid duplicating the `cloud_base` or `anaconda` package sets.
    
    It works by adding new toplevels "image_types" and
    "virtual_package_sets" in the package_sets.yaml and then using
    anchors/refs to refer to them in the image_type yaml.
    
    It allows us to write:
    ```yaml
    virtual_package_sets:
      base: &base_pkgset
        include: [from-base-inc]
        exclude: [from-base-exc]
        condition:
          distro_name:
            test-distro:
              include: [from-base-condition-inc]
              exclude: [from-base-condition-exc]
    
    image_types:
      other_type:
        package_sets:
          - &other_type_pkgset
            include: [from-other-type-inc]
            exclude: [from-other-type-exc]
      test_type:
        package_sets:
          - *base_pkgset
          - *other_type_pkgset
          - include: [from-type-inc]
            exclude: [from-type-exc]
            condition:
              distro_name:
                test-distro:
                  include: [from-condition-inc]
                  exclude: [from-condition-exc]
    ```
    The downside of this approach is that:
    1. it requires the anchor to be there before the ref, which can
       be confusing (we could solve this later via includes)
    2. if the anchor is forgotten the error becomes very obscure
    3. its a bit "hardcore" yaml
    
    The upside is that it is very symetrical and actually feels quite
    nice.

--- pkg/distro/packagesets/loader_test.go
+++ pkg/distro/packagesets/loader_test.go
@@ -37,17 +37,19 @@ func makeFakePkgsSet(t *testing.T, distroName, content string) string {
 func TestLoadConditionDistro(t *testing.T) {
 	it := makeTestImageType(t)
 	fakePkgsSetYaml := `
-test_type:
-  include: [inc1]
-  exclude: [exc1]
-  condition:
-    distro_name:
-      test-distro:
-        include: [from-condition-inc2]
-        exclude: [from-condition-exc2]
-      other-distro:
-        include: [inc3]
-        exclude: [exc3]
+image_types:
+  test_type:
+    package_sets:
+      - include: [inc1]
+        exclude: [exc1]
+        condition:
+          distro_name:
+            test-distro:
+              include: [from-condition-inc2]
+              exclude: [from-condition-exc2]
+            other-distro:
+              include: [inc3]
+              exclude: [exc3]
 `
 	// XXX: we cannot use distro.Name() as it will give us a name+ver
 	baseDir := makeFakePkgsSet(t, test_distro.TestDistroNameBase, fakePkgsSetYaml)
@@ -57,7 +59,52 @@ test_type:
 	pkgSet := packagesets.Load(it, nil)
 	assert.NotNil(t, pkgSet)
 	assert.Equal(t, rpmmd.PackageSet{
-		Include: []string{"inc1", "from-condition-inc2"},
+		Include: []string{"from-condition-inc2", "inc1"},
 		Exclude: []string{"exc1", "from-condition-exc2"},
+	}, pkgSet)
+}
+
+func TestLoadYamlMergingWorks(t *testing.T) {
+	it := makeTestImageType(t)
+	fakePkgsSetYaml := `
+# package_sets not associated with an imagetype can be defined here
+virtual_package_sets:
+  base: &base_pkgset
+    include: [from-base-inc]
+    exclude: [from-base-exc]
+    condition:
+      distro_name:
+        test-distro:
+          include: [from-base-condition-inc]
+          exclude: [from-base-condition-exc]
+
+image_types:
+  other_type:
+    package_sets:
+      - &other_type_pkgset
+        include: [from-other-type-inc]
+        exclude: [from-other-type-exc]
+  test_type:
+    package_sets:
+      - *base_pkgset
+      - *other_type_pkgset
+      - include: [from-type-inc]
+        exclude: [from-type-exc]
+        condition:
+          distro_name:
+            test-distro:
+              include: [from-condition-inc]
+              exclude: [from-condition-exc]
+`
+	// XXX: we cannot use distro.Name() as it will give us a name+ver
+	baseDir := makeFakePkgsSet(t, test_distro.TestDistroNameBase, fakePkgsSetYaml)
+	restore := packagesets.MockDataFS(baseDir)
+	defer restore()
+
+	pkgSet := packagesets.Load(it, nil)
+	assert.NotNil(t, pkgSet)
+	assert.Equal(t, rpmmd.PackageSet{
+		Include: []string{"from-base-condition-inc", "from-base-inc", "from-condition-inc", "from-other-type-inc", "from-type-inc"},
+		Exclude: []string{"from-base-condition-exc", "from-base-exc", "from-condition-exc", "from-other-type-exc", "from-type-exc"},
 	}, pkgSet)
 }
