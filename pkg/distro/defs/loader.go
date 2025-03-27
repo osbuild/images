989127448c7a3060ab2bc3af414b75ce35b43972
Michael Vogt <michael.vogt@gmail.com>
2025-03-27 10:33:57 +0100

    defs: add `partition_table_override` to `distro.yaml`
    With the most recent changes for the `iot-raw` imagetype
    in fedora we need a way to conditionally override parts
    of the partition table.
    
    This commit adds this support via a simple:
    ```yaml
        partition_table_override:
          condition:
            version_greater_or_equal:
              "42":
                - select_partition: 2
                  fstab_options: "defaults,ro"
    ```
    It currently supports `size` (we will need this for RHEL) and
    `fstab_options`. It is sadly not generic (we can look into
    this using reflection) and a bit ad-hoc, i.e. the selector
    is currently very primitive and will fail for nested payloads
    (like overrides for a FS inside LUKS). We have no need for
    the nesting yet though and if it comes up we could invent a
    notion like (`select_partiton: 2:1` or similar /if/ we need
    it).

--- pkg/distro/defs/loader.go
+++ pkg/distro/defs/loader.go
@@ -35,19 +35,78 @@ type imageType struct {
 	PackageSets []packageSet `yaml:"package_sets"`
 	// archStr->partitionTable
 	PartitionTables map[string]*disk.PartitionTable `yaml:"partition_table"`
+	// override specific aspects of the partition table
+	PartitionTablesOverrides *partitionTablesOverrides `yaml:"partition_table_override"`
 }
 
 type packageSet struct {
-	Include   []string    `yaml:"include"`
-	Exclude   []string    `yaml:"exclude"`
-	Condition *conditions `yaml:"condition,omitempty"`
+	Include   []string          `yaml:"include"`
+	Exclude   []string          `yaml:"exclude"`
+	Condition *pkgSetConditions `yaml:"condition,omitempty"`
 }
 
-type conditions struct {
+type pkgSetConditions struct {
 	Architecture          map[string]packageSet `yaml:"architecture,omitempty"`
 	VersionLessThan       map[string]packageSet `yaml:"version_less_than,omitempty"`
 	VersionGreaterOrEqual map[string]packageSet `yaml:"version_greater_or_equal,omitempty"`
 	DistroName            map[string]packageSet `yaml:"distro_name,omitempty"`
+}
+
+type partitionTablesOverrides struct {
+	Conditional *partitionTablesOverwriteConditional `yaml:"condition"`
+}
+
+func (po *partitionTablesOverrides) Apply(it distro.ImageType, pt *disk.PartitionTable, replacements map[string]string) error {
+	if po == nil {
+		return nil
+	}
+	cond := po.Conditional
+	_, distroVersion := splitDistroNameVer(it.Arch().Distro().Name())
+
+	for gteqVer, geOverrides := range cond.VersionGreaterOrEqual {
+		if r, ok := replacements[gteqVer]; ok {
+			gteqVer = r
+		}
+		if common.VersionGreaterThanOrEqual(distroVersion, gteqVer) {
+			for _, overrideOp := range geOverrides {
+				if err := overrideOp.Apply(pt); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+type partitionTablesOverwriteConditional struct {
+	VersionGreaterOrEqual map[string][]partitionTablesOverrideOp `yaml:"version_greater_or_equal,omitempty"`
+}
+
+type partitionTablesOverrideOp struct {
+	PartitionIndex int    `yaml:"partition_index"`
+	Size           uint64 `yaml:"size"`
+	FSTabOptions   string `yaml:"fstab_options"`
+}
+
+func (op *partitionTablesOverrideOp) Apply(pt *disk.PartitionTable) error {
+	selectPart := op.PartitionIndex
+	if selectPart > len(pt.Partitions) {
+		return fmt.Errorf("override %q part %v outside of partitionTable %+v", op, selectPart, pt)
+	}
+	if op.Size > 0 {
+		pt.Partitions[selectPart].Size = op.Size
+	}
+	if op.FSTabOptions != "" {
+		part := pt.Partitions[selectPart]
+		fs, ok := part.Payload.(*disk.Filesystem)
+		if !ok {
+			return fmt.Errorf("override %q part %v for fstab_options expecting filesystem got %T", op, selectPart, part)
+		}
+		fs.FSTabOptions = op.FSTabOptions
+	}
+
+	return nil
 }
 
 // PackageSet loads the PackageSet from the yaml source file discovered via the
@@ -134,7 +193,7 @@ func PackageSet(it distro.ImageType, overrideTypeName string, replacements map[s
 }
 
 // PartitionTable returns the partionTable for the given distro/imgType.
-func PartitionTable(it distro.ImageType) (*disk.PartitionTable, error) {
+func PartitionTable(it distro.ImageType, replacements map[string]string) (*disk.PartitionTable, error) {
 	distroNameVer := it.Arch().Distro().Name()
 	typeName := strings.ReplaceAll(it.Name(), "-", "_")
 
@@ -153,6 +212,10 @@ func PartitionTable(it distro.ImageType) (*disk.PartitionTable, error) {
 	pt, ok := imgType.PartitionTables[archName]
 	if !ok {
 		return nil, fmt.Errorf("no partition table for %q", arch)
+	}
+
+	if err := imgType.PartitionTablesOverrides.Apply(it, pt, replacements); err != nil {
+		return nil, err
 	}
 
 	return pt, nil
