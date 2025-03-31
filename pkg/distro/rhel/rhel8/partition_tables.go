fea81fccd4273a62555fe126dabf5f9a31a31185
Michael Vogt <michael.vogt@gmail.com>
2025-03-31 09:56:21 +0200

    distro: move rhel8 partition tables into YAML

--- pkg/distro/rhel/rhel8/partition_tables.go
+++ pkg/distro/rhel/rhel8/partition_tables.go
@@ -1,276 +1,40 @@
 package rhel8
 
 import (
-	"github.com/osbuild/images/internal/common"
-	"github.com/osbuild/images/pkg/arch"
-	"github.com/osbuild/images/pkg/datasizes"
+	"errors"
+
 	"github.com/osbuild/images/pkg/disk"
+	"github.com/osbuild/images/pkg/distro/defs"
 	"github.com/osbuild/images/pkg/distro/rhel"
 )
 
-func defaultBasePartitionTables(t *rhel.ImageType) (disk.PartitionTable, bool) {
-	switch t.Arch().Name() {
-	case arch.ARCH_X86_64.String():
-		return disk.PartitionTable{
-			UUID: "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
-			Type: disk.PT_GPT,
-			Partitions: []disk.Partition{
-				{
-					Size:     1 * datasizes.MebiByte,
-					Bootable: true,
-					Type:     disk.BIOSBootPartitionGUID,
-					UUID:     disk.BIOSBootPartitionUUID,
-				},
-				{
-					Size: 100 * datasizes.MebiByte,
-					Type: disk.EFISystemPartitionGUID,
-					UUID: disk.EFISystemPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "vfat",
-						UUID:         disk.EFIFilesystemUUID,
-						Mountpoint:   "/boot/efi",
-						FSTabOptions: "defaults,uid=0,gid=0,umask=077,shortname=winnt",
-						FSTabFreq:    0,
-						FSTabPassNo:  2,
-					},
-				},
-				{
-					Size: 2 * datasizes.GibiByte,
-					Type: disk.FilesystemDataGUID,
-					UUID: disk.RootPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "xfs",
-						Label:        "root",
-						Mountpoint:   "/",
-						FSTabOptions: "defaults",
-						FSTabFreq:    0,
-						FSTabPassNo:  0,
-					},
-				},
-			},
-		}, true
-
-	case arch.ARCH_AARCH64.String():
-		return disk.PartitionTable{
-			UUID: "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
-			Type: disk.PT_GPT,
-			Partitions: []disk.Partition{
-				{
-					Size: 100 * datasizes.MebiByte,
-					Type: disk.EFISystemPartitionGUID,
-					UUID: disk.EFISystemPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "vfat",
-						UUID:         disk.EFIFilesystemUUID,
-						Mountpoint:   "/boot/efi",
-						FSTabOptions: "defaults,uid=0,gid=0,umask=077,shortname=winnt",
-						FSTabFreq:    0,
-						FSTabPassNo:  2,
-					},
-				},
-				{
-					Size: 2 * datasizes.GibiByte,
-					Type: disk.FilesystemDataGUID,
-					UUID: disk.RootPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "xfs",
-						Label:        "root",
-						Mountpoint:   "/",
-						FSTabOptions: "defaults",
-						FSTabFreq:    0,
-						FSTabPassNo:  0,
-					},
-				},
-			},
-		}, true
-
-	case arch.ARCH_PPC64LE.String():
-		return disk.PartitionTable{
-			UUID: "0x14fc63d2",
-			Type: disk.PT_DOS,
-			Partitions: []disk.Partition{
-				{
-					Size:     4 * datasizes.MebiByte,
-					Type:     disk.PRepPartitionDOSID,
-					Bootable: true,
-				},
-				{
-					Size: 2 * datasizes.GibiByte,
-					Payload: &disk.Filesystem{
-						Type:         "xfs",
-						Mountpoint:   "/",
-						FSTabOptions: "defaults",
-						FSTabFreq:    0,
-						FSTabPassNo:  0,
-					},
-				},
-			},
-		}, true
-
-	case arch.ARCH_S390X.String():
-		return disk.PartitionTable{
-			UUID: "0x14fc63d2",
-			Type: disk.PT_DOS,
-			Partitions: []disk.Partition{
-				{
-					Size:     2 * datasizes.GibiByte,
-					Bootable: true,
-					Payload: &disk.Filesystem{
-						Type:         "xfs",
-						Mountpoint:   "/",
-						FSTabOptions: "defaults",
-						FSTabFreq:    0,
-						FSTabPassNo:  0,
-					},
-				},
-			},
-		}, true
-
-	default:
+func partitionTables(t *rhel.ImageType) (disk.PartitionTable, bool) {
+	partitionTable, err := defs.PartitionTable(t, nil)
+	if errors.Is(err, defs.ErrNoPartitionTableForImgType) {
 		return disk.PartitionTable{}, false
 	}
+	if err != nil {
+		panic(err)
+	}
+	if partitionTable == nil {
+		return disk.PartitionTable{}, false
+	}
+	return *partitionTable, true
+}
+
+func defaultBasePartitionTables(t *rhel.ImageType) (disk.PartitionTable, bool) {
+	return partitionTables(t)
 }
 
 func edgeBasePartitionTables(t *rhel.ImageType) (disk.PartitionTable, bool) {
-	switch t.Arch().Name() {
-	case arch.ARCH_X86_64.String():
-		return disk.PartitionTable{
-			UUID: "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
-			Type: disk.PT_GPT,
-			Partitions: []disk.Partition{
-				{
-					Size:     1 * datasizes.MebiByte,
-					Bootable: true,
-					Type:     disk.BIOSBootPartitionGUID,
-					UUID:     disk.BIOSBootPartitionUUID,
-				},
-				{
-					Size: 127 * datasizes.MebiByte,
-					Type: disk.EFISystemPartitionGUID,
-					UUID: disk.EFISystemPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "vfat",
-						UUID:         disk.EFIFilesystemUUID,
-						Mountpoint:   "/boot/efi",
-						Label:        "EFI-SYSTEM",
-						FSTabOptions: "defaults,uid=0,gid=0,umask=077,shortname=winnt",
-						FSTabFreq:    0,
-						FSTabPassNo:  2,
-					},
-				},
-				{
-					Size: 384 * datasizes.MebiByte,
-					Type: disk.FilesystemDataGUID,
-					UUID: disk.DataPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "xfs",
-						Mountpoint:   "/boot",
-						Label:        "boot",
-						FSTabOptions: "defaults",
-						FSTabFreq:    1,
-						FSTabPassNo:  1,
-					},
-				},
-				{
-					Size: 2 * datasizes.GibiByte,
-					Type: disk.FilesystemDataGUID,
-					UUID: disk.RootPartitionUUID,
-					Payload: &disk.LUKSContainer{
-						Label:      "crypt_root",
-						Cipher:     "cipher_null",
-						Passphrase: "osbuild",
-						PBKDF: disk.Argon2id{
-							Memory:      32,
-							Iterations:  4,
-							Parallelism: 1,
-						},
-						Clevis: &disk.ClevisBind{
-							Pin:              "null",
-							Policy:           "{}",
-							RemovePassphrase: true,
-						},
-						Payload: &disk.Filesystem{
-							Type:         "xfs",
-							Label:        "root",
-							Mountpoint:   "/",
-							FSTabOptions: "defaults",
-							FSTabFreq:    0,
-							FSTabPassNo:  0,
-						},
-					},
-				},
-			},
-		}, true
-
-	case arch.ARCH_AARCH64.String():
-		return disk.PartitionTable{
-			UUID: "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
-			Type: disk.PT_GPT,
-			Partitions: []disk.Partition{
-				{
-					Size: 127 * datasizes.MebiByte,
-					Type: disk.EFISystemPartitionGUID,
-					UUID: disk.EFISystemPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "vfat",
-						UUID:         disk.EFIFilesystemUUID,
-						Mountpoint:   "/boot/efi",
-						Label:        "EFI-SYSTEM",
-						FSTabOptions: "defaults,uid=0,gid=0,umask=077,shortname=winnt",
-						FSTabFreq:    0,
-						FSTabPassNo:  2,
-					},
-				},
-				{
-					Size: 384 * datasizes.MebiByte,
-					Type: disk.FilesystemDataGUID,
-					UUID: disk.DataPartitionUUID,
-					Payload: &disk.Filesystem{
-						Type:         "xfs",
-						Mountpoint:   "/boot",
-						Label:        "boot",
-						FSTabOptions: "defaults",
-						FSTabFreq:    1,
-						FSTabPassNo:  1,
-					},
-				},
-				{
-					Size: 2 * datasizes.GibiByte,
-					Type: disk.FilesystemDataGUID,
-					UUID: disk.RootPartitionUUID,
-					Payload: &disk.LUKSContainer{
-						Label:      "crypt_root",
-						Cipher:     "cipher_null",
-						Passphrase: "osbuild",
-						PBKDF: disk.Argon2id{
-							Memory:      32,
-							Iterations:  4,
-							Parallelism: 1,
-						},
-						Clevis: &disk.ClevisBind{
-							Pin:              "null",
-							Policy:           "{}",
-							RemovePassphrase: true,
-						},
-						Payload: &disk.Filesystem{
-							Type:         "xfs",
-							Label:        "root",
-							Mountpoint:   "/",
-							FSTabOptions: "defaults",
-							FSTabFreq:    0,
-							FSTabPassNo:  0,
-						},
-					},
-				},
-			},
-		}, true
-
-	default:
-		return disk.PartitionTable{}, false
-	}
+	return partitionTables(t)
 }
 
 func ec2PartitionTables(t *rhel.ImageType) (disk.PartitionTable, bool) {
+	return partitionTables(t)
+}
+
+/*
 	// x86_64 - without /boot
 	// aarch  - <= 8.9 - 512MiB, 8.10 and centos: 1 GiB
 	var aarch64BootSize uint64
@@ -402,3 +166,4 @@ func ec2PartitionTables(t *rhel.ImageType) (disk.PartitionTable, bool) {
 		return disk.PartitionTable{}, false
 	}
 }
+*/
