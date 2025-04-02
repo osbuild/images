a554a8e94aae26726a7f356a9a9645072686b39a
Achilleas Koutsou <achilleas@koutsou.net>
2025-04-02 18:13:47 +0200

    tools/gen-manifest-checksums: exit and skip if binary doesn't compile

--- tools/gen-manifest-checksums.sh
+++ tools/gen-manifest-checksums.sh
@@ -15,14 +15,24 @@ cleanup() {
 trap cleanup EXIT
 
 export OSBUILD_TESTING_RNG_SEED=0
+
+# For the purposes of this script, failing to compile is not an error. It is
+# preferable for all commits to compile, but sometimes it's necessary or
+# desirable to relax this requirement and in those cases we want to ignore the
+# specific commit.
+if ! go build -v -o "${tmpdir}/bin/" ./cmd/gen-manifests; then
+    echo "Failed to compile gen-manifests. Skipping..."
+    exit 0
+fi
+
 # NOTE: fedora-41 riscv has no test repositories so we need to skip it.
 # NOTE: silence stdout as it gets way too noisy in the GitHub action log (until
 # gen-manifests gets a verbosity or progress option).
-go run ./cmd/gen-manifests \
+"${tmpdir}/bin/gen-manifests" \
     --packages=false --containers=false --commits=false \
     --metadata=false \
     --arches "x86_64,aarch64,ppc64le,s390x" \
-    --output "${tmpdir}" \
+    --output "${tmpdir}/manifests" \
     > /dev/null
 
 
@@ -32,6 +42,6 @@ go run ./cmd/gen-manifests \
 # For mocked manifests though we want those things to be visible changes, so we
 # calculate the checksum of the file directly. Also it's faster.
 checksums_file="./test/data/manifest-checksums.txt"
-(cd "${tmpdir}" && sha1sum -- *) | sort > "${checksums_file}"
+(cd "${tmpdir}/manifests" && sha1sum -- *) | sort > "${checksums_file}"
 
 echo "Checksums saved to ${checksums_file}"
