a7c46415ac526901e04e9fe702c3e8703175a75d
Michael Vogt <michael.vogt@gmail.com>
2025-04-03 11:12:01 +0200

    manifest,image: stop embedding `OSCustomizations` in `manifest.OS`
    This commit is very mechanical, it stops embedding the
    `manifest.OSCustomizations` directly inside `manifest.OS`.
    
    This makes it more explicit what comes from a OSCustomization
    and what is just part of OS which makes it easier to refactor
    `ImageConfig` and `OSCustomizations` as there is currently much
    manual copying.

--- cmd/osbuild-playground/my-container.go
+++ cmd/osbuild-playground/my-container.go
@@ -51,7 +51,7 @@ func (img *MyContainer) InstantiateManifest(m *manifest.Manifest,
 
 	// create a minimal non-bootable OS tree
 	os := manifest.NewOS(build, &platform.X86{}, repos)
-	os.BasePackages = []string{"@core"}
+	os.OSCustomizations.BasePackages = []string{"@core"}
 	os.OSCustomizations.Language = "en_US.UTF-8"
 	os.OSCustomizations.Hostname = "my-host"
 	os.OSCustomizations.Timezone = "UTC"
