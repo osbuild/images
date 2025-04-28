5c451de0b4760339bc1cd48f60108c2ceaec8e90
Achilleas Koutsou <achilleas@koutsou.net>
2025-04-28 12:54:54 +0300

    distro/el10/azure: update kernel args to match RHEL 9
    Adapt kernel args for RHEL 10 and CentOS Stream 10 Azure images to match
    RHEL 9.
    
    This is a combination of the same changes that were made in RHEL 9.6:
    - Add nvme_core.io_timeout=240: 17f32e8cf08a5221bca15bc91bb32edf2845cd54
    - Change console kernel args for aarch64:
      2e583dac66570caeaeab01ce746e9cc092feaf44
    
    Note that the net.ifnames=0 argument is not added since we dropped it
    across the board for EL10 images (see PR #818).

--- pkg/distro/rhel/rhel10/azure.go
+++ pkg/distro/rhel/rhel10/azure.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Azure image type
-func mkAzureImgType(rd *rhel.Distribution) *rhel.ImageType {
+func mkAzureImgType(rd *rhel.Distribution, a arch.Arch) *rhel.ImageType {
 	it := rhel.NewImageType(
 		"vhd",
 		"disk.vhd",
@@ -25,7 +25,7 @@ func mkAzureImgType(rd *rhel.Distribution) *rhel.ImageType {
 		[]string{"vpc"},
 	)
 
-	it.KernelOptions = defaultAzureKernelOptions()
+	it.KernelOptions = defaultAzureKernelOptions(a)
 	it.Bootable = true
 	it.DefaultSize = 4 * datasizes.GibiByte
 	it.DefaultImageConfig = defaultAzureImageConfig(rd)
@@ -35,7 +35,7 @@ func mkAzureImgType(rd *rhel.Distribution) *rhel.ImageType {
 }
 
 // Azure RHEL-internal image type
-func mkAzureInternalImgType(rd *rhel.Distribution) *rhel.ImageType {
+func mkAzureInternalImgType(rd *rhel.Distribution, a arch.Arch) *rhel.ImageType {
 	it := rhel.NewImageType(
 		"azure-rhui",
 		"disk.vhd.xz",
@@ -50,7 +50,7 @@ func mkAzureInternalImgType(rd *rhel.Distribution) *rhel.ImageType {
 	)
 
 	it.Compression = "xz"
-	it.KernelOptions = defaultAzureKernelOptions()
+	it.KernelOptions = defaultAzureKernelOptions(a)
 	it.Bootable = true
 	it.DefaultSize = 64 * datasizes.GibiByte
 	it.DefaultImageConfig = defaultAzureImageConfig(rd)
@@ -59,7 +59,7 @@ func mkAzureInternalImgType(rd *rhel.Distribution) *rhel.ImageType {
 	return it
 }
 
-func mkAzureSapInternalImgType(rd *rhel.Distribution) *rhel.ImageType {
+func mkAzureSapInternalImgType(rd *rhel.Distribution, a arch.Arch) *rhel.ImageType {
 	it := rhel.NewImageType(
 		"azure-sap-rhui",
 		"disk.vhd.xz",
@@ -74,7 +74,7 @@ func mkAzureSapInternalImgType(rd *rhel.Distribution) *rhel.ImageType {
 	)
 
 	it.Compression = "xz"
-	it.KernelOptions = defaultAzureKernelOptions()
+	it.KernelOptions = defaultAzureKernelOptions(a)
 	it.Bootable = true
 	it.DefaultSize = 64 * datasizes.GibiByte
 	it.DefaultImageConfig = sapAzureImageConfig(rd)
@@ -307,9 +307,15 @@ func azureInternalBasePartitionTables(t *rhel.ImageType) (disk.PartitionTable, b
 
 // IMAGE CONFIG
 
-// use loglevel=3 as described in the RHEL documentation and used in existing RHEL images built by MSFT
-func defaultAzureKernelOptions() []string {
-	return []string{"ro", "loglevel=3", "console=tty1", "console=ttyS0", "earlyprintk=ttyS0", "rootdelay=300"}
+func defaultAzureKernelOptions(a arch.Arch) []string {
+	kargs := []string{"ro", "loglevel=3", "nvme_core.io_timeout=240"}
+	switch a {
+	case arch.ARCH_AARCH64:
+		kargs = append(kargs, "console=ttyAMA0")
+	case arch.ARCH_X86_64:
+		kargs = append(kargs, "console=tty1", "console=ttyS0", "earlyprintk=ttyS0", "rootdelay=300")
+	}
+	return kargs
 }
 
 // based on https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/9/html/deploying_rhel_9_on_microsoft_azure/assembly_deploying-a-rhel-image-as-a-virtual-machine-on-microsoft-azure_cloud-content-azure#making-configuration-changes_configure-the-image-azure
