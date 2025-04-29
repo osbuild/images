bc166716596c334e84149fcf8404fdfcac988fb5
Michael Vogt <michael.vogt@gmail.com>
2025-04-29 12:34:38 +0200

    platform: add JSON/YAML support for ImageFormat
    When we move `platform` into YAML we will need support for
    `ImageFormat` as it is used to define the platforms.
    
    This commit adds support for JSON/YAML unmarshaling.

--- pkg/platform/platform_test.go
+++ pkg/platform/platform_test.go
@@ -1,9 +1,12 @@
 package platform_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 
 	"github.com/osbuild/images/pkg/platform"
 )
@@ -17,4 +20,30 @@ func TestImageFormatStringUnknown(t *testing.T) {
 	assert.PanicsWithError(t, "unknown image format 999", func() {
 		_ = platform.ImageFormat(999).String()
 	})
+}
+
+func TestImageFormatUnmarshal(t *testing.T) {
+	ifmts := []platform.ImageFormat{
+		platform.FORMAT_UNSET,
+		platform.FORMAT_RAW,
+		platform.FORMAT_ISO,
+		platform.FORMAT_QCOW2,
+		platform.FORMAT_VMDK,
+		platform.FORMAT_VHD,
+		platform.FORMAT_GCE,
+		platform.FORMAT_OVA,
+	}
+	for _, ifmt := range ifmts {
+		inpJSON := fmt.Sprintf("%q", ifmt.String())
+		inpYAML := ifmt.String()
+		// json
+		var f platform.ImageFormat
+		err := json.Unmarshal([]byte(inpJSON), &f)
+		assert.NoError(t, err)
+		assert.Equal(t, ifmt, f)
+		// now YAML
+		err = yaml.Unmarshal([]byte(inpYAML), &f)
+		assert.NoError(t, err)
+		assert.Equal(t, ifmt, f)
+	}
 }
