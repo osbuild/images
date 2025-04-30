c1416c615f9b758cdbf109ae18e374c1d8220c9a
Michael Vogt <michael.vogt@gmail.com>
2025-04-30 12:50:25 +0200

    policies: allow `/boot/efi` for disk customizations
    With the new customization support for `/boot/efi` we will
    need to allow `/boot/efi` as a valid custom mountpoint for
    disk customizations.
    
    The disk customizations will already double check that
    `/boot/efi` is the right filesystem type (vfat) so allowing it
    for this should also be fine.
    
    However we cannot allow this for filesystem customizations
    yet. Manual testing shows that the code does not seem DTRT
    so we disallow it for filesytem customizations still until
    this gets fixed.

--- pkg/policies/policies_test.go
+++ pkg/policies/policies_test.go
@@ -4,76 +4,87 @@ import "testing"
 
 func TestMountpointPolicies(t *testing.T) {
 	type testCase struct {
-		path    string
-		allowed bool
+		path        string
+		allowedFS   bool
+		allowedDisk bool
 	}
 
 	testCases := []testCase{
-		{"/", true},
+		{"/", true, true},
 
-		{"/bin", false},
-		{"/dev", false},
-		{"/etc", false},
-		{"/lib", false},
-		{"/lib64", false},
-		{"/lost+found", false},
-		{"/proc", false},
-		{"/run", false},
-		{"/sbin", false},
-		{"/sys", false},
-		{"/sysroot", false},
+		{"/bin", false, false},
+		{"/dev", false, false},
+		{"/etc", false, false},
+		{"/lib", false, false},
+		{"/lib64", false, false},
+		{"/lost+found", false, false},
+		{"/proc", false, false},
+		{"/run", false, false},
+		{"/sbin", false, false},
+		{"/sys", false, false},
+		{"/sysroot", false, false},
 
-		{"/mnt", true},
-		{"/root", true},
+		{"/mnt", true, true},
+		{"/root", true, true},
 
-		{"/custom", true},
-		{"/custom/dir", true},
+		{"/custom", true, true},
+		{"/custom/dir", true, true},
 
-		{"/boot", true},
-		{"/boot/dir", true},
-		{"/boot/efi", false},
+		{"/boot", true, true},
+		{"/boot/dir", true, true},
+		// Note that /boot/efi is allowed for disk customizations
+		// but not for filesystem customizations
+		{"/boot/efi", false, true},
 
-		{"/var", true},
-		{"/var/lib", true},
-		{"/var/log", true},
-		{"/var/tmp", true},
-		{"/var/run", false},
-		{"/var/lock", false},
+		{"/var", true, true},
+		{"/var/lib", true, true},
+		{"/var/log", true, true},
+		{"/var/tmp", true, true},
+		{"/var/run", false, false},
+		{"/var/lock", false, false},
 
-		{"/opt", true},
-		{"/opt/fancyapp", true},
+		{"/opt", true, true},
+		{"/opt/fancyapp", true, true},
 
-		{"/srv", true},
-		{"/srv/www", true},
+		{"/srv", true, true},
+		{"/srv/www", true, true},
 
-		{"/usr", true},
-		{"/usr/bin", false},
-		{"/usr/sbin", false},
-		{"/usr/local", false},
-		{"/usr/local/bin", false},
-		{"/usr/lib", false},
-		{"/usr/lib64", false},
+		{"/usr", true, true},
+		{"/usr/bin", false, false},
+		{"/usr/sbin", false, false},
+		{"/usr/local", false, false},
+		{"/usr/local/bin", false, false},
+		{"/usr/lib", false, false},
+		{"/usr/lib64", false, false},
 
-		{"/tmp", true},
-		{"/tmp/foo", true},
+		{"/tmp", true, true},
+		{"/tmp/foo", true, true},
 
-		{"/app", true},
-		{"/app/bin", true},
+		{"/app", true, true},
+		{"/app/bin", true, true},
 
-		{"/data", true},
-		{"/data/foo", true},
+		{"/data", true, true},
+		{"/data/foo", true, true},
 
-		{"/home", true},
-		{"/home/user", true},
+		{"/home", true, true},
+		{"/home/user", true, true},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.path, func(t *testing.T) {
-			err := MountpointPolicies.Check(tc.path)
-			if err != nil && tc.allowed {
-				t.Errorf("expected %s to be allowed, but got error: %v", tc.path, err)
-			} else if err == nil && !tc.allowed {
-				t.Errorf("expected %s to be denied, but got no error", tc.path)
+			// check FS customizations policy
+			err := MountpointPoliciesFS.Check(tc.path)
+			if err != nil && tc.allowedFS {
+				t.Errorf("expected %s to be allowed for FS, but got error: %v", tc.path, err)
+			} else if err == nil && !tc.allowedFS {
+				t.Errorf("expected %s to be denied for FS, but got no error", tc.path)
+			}
+			// check disk customizations policy
+			err = MountpointPoliciesDisk.Check(tc.path)
+			if err != nil && tc.allowedDisk {
+				t.Errorf("expected %s to be allowed for disk, but got error: %v", tc.path, err)
+			} else if err == nil && !tc.allowedDisk {
+				t.Errorf("expected %s to be denied for disk, but got no error", tc.path)
 			}
 		})
 	}
