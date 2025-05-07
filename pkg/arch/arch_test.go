f04bbec46ab09597e3f0c73eb28e64d16549b9b5
Michael Vogt <michael.vogt@gmail.com>
2025-05-07 17:28:59 +0200

    arch: add unmarshal support for arch.Arch
    We will define the platform in YAML so we need a way to
    unmarshal the architecture (that is part of the platform).

--- pkg/arch/arch_test.go
+++ pkg/arch/arch_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 
 	"github.com/osbuild/images/internal/common"
 )
@@ -68,4 +69,24 @@ func TestFromString(t *testing.T) {
 	assert.Equal(t, ARCH_S390X, common.Must(FromString("s390x")))
 	assert.Equal(t, ARCH_PPC64LE, common.Must(FromString("ppc64le")))
 	assert.Equal(t, ARCH_RISCV64, common.Must(FromString("riscv64")))
+}
+
+func TestUnmarshal(t *testing.T) {
+	for _, tc := range []struct {
+		inp      string
+		expected Arch
+	}{
+		{"arch: arm64", ARCH_AARCH64},
+		{"arch: amd64", ARCH_X86_64},
+		{"arch: s390x", ARCH_S390X},
+		{"arch: ppc64le", ARCH_PPC64LE},
+		{"arch: riscv64", ARCH_RISCV64},
+	} {
+		var v struct {
+			Arch Arch
+		}
+		err := yaml.Unmarshal([]byte(tc.inp), &v)
+		assert.NoError(t, err)
+		assert.Equal(t, tc.expected, v.Arch)
+	}
 }
