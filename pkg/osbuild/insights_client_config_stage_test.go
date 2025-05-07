3be6313af5efd14beb49e90080aed203a2ed1570
rverdile <ryanverdile@gmail.com>
2025-05-07 11:50:05 -0400

    osbuild: Add config to InsightsClientConfigStageOptions
    Fixes a bug because the insights_client_config stage expects the options to be nested under a config object.

--- pkg/osbuild/insights_client_config_stage_test.go
+++ pkg/osbuild/insights_client_config_stage_test.go
@@ -1,15 +1,31 @@
 package osbuild
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewInsightsClientConfigStage(t *testing.T) {
+	stageOptions := &InsightsClientConfigStageOptions{
+		Config: InsightsClientConfig{Path: "foo/bar", Proxy: "proxy"},
+	}
 	expectedStage := &Stage{
 		Type:    "org.osbuild.insights-client.config",
-		Options: &InsightsClientConfigStageOptions{},
+		Options: stageOptions,
 	}
-	actualStage := NewInsightsClientConfigStage(&InsightsClientConfigStageOptions{})
+	actualStage := NewInsightsClientConfigStage(stageOptions)
 	assert.Equal(t, expectedStage, actualStage)
+}
+
+func TestInsightsClientConfigOptionsJSON(t *testing.T) {
+	stageOptions := &InsightsClientConfigStageOptions{
+		Config: InsightsClientConfig{
+			Proxy: "some-proxy",
+			Path:  "some-path",
+		},
+	}
+	b, err := json.Marshal(stageOptions)
+	assert.NoError(t, err)
+	assert.Equal(t, string(b), `{"config":{"proxy":"some-proxy","path":"some-path"}}`)
 }
