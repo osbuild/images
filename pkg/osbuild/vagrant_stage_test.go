a5cea777c9e67ac5294579044c4dd7df4a5928f1
Simon de Vlieger <supakeen@redhat.com>
2025-05-20 11:06:07 +0200

    osbuild: implement vagrant
    This implements the vagrant stage as it currently exists in `osbuild`.
    Note that there is an active PR now to add `virtualbox` support as a
    provider but that hasn't landed *yet*.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ pkg/osbuild/vagrant_stage_test.go
@@ -0,0 +1,54 @@
+package osbuild
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewVagrantStage(t *testing.T) {
+	input := NewFilesInput(NewFilesInputPipelineObjectRef("stage", "img.raw", nil))
+	inputs := VagrantStageInputs{Image: input}
+
+	options := VagrantStageOptions{
+		Provider: VagrantProviderLibvirt,
+	}
+
+	expectedStage := &Stage{
+		Type:    "org.osbuild.vagrant",
+		Options: &options,
+		Inputs:  &inputs,
+	}
+
+	actualStage := NewVagrantStage(&options, &inputs)
+	assert.Equal(t, expectedStage, actualStage)
+}
+
+func TestVagrantStageOptions(t *testing.T) {
+	tests := []struct {
+		Provider VagrantProvider
+		Error    bool
+	}{
+		{
+			Provider: VagrantProviderLibvirt,
+		},
+		// mismatch between format and format options type
+		{
+			Provider: "DoesNotExist",
+			Error:    true,
+		},
+	}
+
+	for idx, test := range tests {
+		t.Run(fmt.Sprintf("test-(%d/%d)", idx, len(tests)), func(t *testing.T) {
+			stageOptions := NewVagrantStageOptions(test.Provider)
+
+			if test.Error {
+				assert.Panics(t, func() { NewVagrantStage(stageOptions, nil) })
+			} else {
+				assert.EqualValues(t, test.Provider, stageOptions.Provider)
+			}
+		})
+	}
+}
