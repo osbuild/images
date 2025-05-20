36323f68bc8cba3e78d88b11eccec6af411dd893
Michael Vogt <michael.vogt@gmail.com>
2025-05-20 11:50:26 +0200

    disk: add unit test for new MkfsOption marshal/unmarshal
    Tiny PR that adds unit tests around the new marshal/unmarshal
    feature of `MkfsOption`.

--- pkg/disk/filesystem_test.go
+++ pkg/disk/filesystem_test.go
@@ -1,7 +1,10 @@
 package disk_test
 
 import (
+	"encoding/json"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/osbuild/images/pkg/disk"
 )
@@ -10,4 +13,29 @@ func TestImplementsInterfacesCompileTimeCheckFilesystem(t *testing.T) {
 	var _ = disk.Mountable(&disk.Filesystem{})
 	var _ = disk.UniqueEntity(&disk.Filesystem{})
 	var _ = disk.FSTabEntity(&disk.Filesystem{})
+}
+
+func TestMkfsOptionUnmarshalHappy(t *testing.T) {
+	for _, tc := range []struct {
+		inp      string
+		expected disk.MkfsOption
+	}{
+		{`"verity"`, disk.MkfsVerity},
+	} {
+		var opt disk.MkfsOption
+		err := json.Unmarshal([]byte(tc.inp), &opt)
+		assert.NoError(t, err)
+		assert.Equal(t, tc.expected, opt)
+
+		// encoding again yields the same result
+		encoded, err := json.Marshal(opt)
+		assert.NoError(t, err)
+		assert.Equal(t, tc.inp, string(encoded))
+	}
+}
+
+func TestMkfsOptionUnmarshalSad(t *testing.T) {
+	var opt disk.MkfsOption
+	err := json.Unmarshal([]byte(`"invalid-mkfs-option"`), &opt)
+	assert.EqualError(t, err, `invalid mkfsoption: invalid-mkfs-option`)
 }
