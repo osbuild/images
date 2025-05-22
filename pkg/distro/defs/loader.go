666f7a5c1e041897d4355dfe3242d5bfdcac24a0
Michael Vogt <michael.vogt@gmail.com>
2025-05-22 19:52:44 +0200

    fedora: move hardcoded unattneded kickstart install handling into YAML
    This moves the handling of unattended kickstart into YAML, we have
    drift between the `imageInstallerImage()` and in fedora we set
    additional kernel options (but also have a note there that it
    does not work) when the kickstart is set to unattended. But we
    do not do that on rhel.

--- pkg/distro/defs/loader.go
+++ pkg/distro/defs/loader.go
@@ -156,6 +156,10 @@ type imageType struct {
 	// XXX: or iso_variant?
 	Variant string `yaml:"variant"`
 
+	// XXX: this is really here only for compatibility/because of drift in the "imageInstallerImage"
+	// between fedora/rhel
+	KickstartUnattendedExtraKernelOpts []string `yaml:"kickstart_unattended_extra_kernel_opts"`
+
 	RPMOSTree bool `yaml:"rpm_ostree"`
 
 	DefaultSize uint64 `yaml:"default_size"`
