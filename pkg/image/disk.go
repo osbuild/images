e265768cf402a8a9e05670ad3a7bdfb03a3199ae
Simon de Vlieger <supakeen@redhat.com>
2025-05-22 11:58:00 +0200

    many: virtualbox provider and stage updates
    There's new support in osbuild's vagrant stage (and OVF stage) to do
    Vagrant virtualbox specific things. Implement the new stage options,
    provider(s), and plumb them through in manifest and disk.

--- pkg/image/disk.go
+++ pkg/image/disk.go
@@ -76,10 +76,24 @@ func (img *DiskImage) InstantiateManifest(m *manifest.Manifest,
 		qcow2Pipeline := manifest.NewQCOW2(buildPipeline, rawImagePipeline)
 		qcow2Pipeline.Compat = img.Platform.GetQCOW2Compat()
 
-		vagrantPipeline := manifest.NewVagrant(buildPipeline, qcow2Pipeline)
+		vagrantPipeline := manifest.NewVagrant(buildPipeline, qcow2Pipeline, osbuild.VagrantProviderLibvirt)
 
 		tarPipeline := manifest.NewTar(buildPipeline, vagrantPipeline, "archive")
 		tarPipeline.Format = osbuild.TarArchiveFormatUstar
+
+		imagePipeline = tarPipeline
+	case platform.FORMAT_VAGRANT_VIRTUALBOX:
+		vmdkPipeline := manifest.NewVMDK(buildPipeline, rawImagePipeline)
+		vmdkPipeline.SetFilename("box.vmdk")
+
+		vagrantPipeline := manifest.NewVagrant(buildPipeline, vmdkPipeline, osbuild.VagrantProviderVirtualBox)
+
+		// Use the (potentially seeded) RNG to generate a new random mac address
+		vagrantPipeline.SetRandomMacAddress(rng)
+
+		tarPipeline := manifest.NewTar(buildPipeline, vagrantPipeline, "archive")
+		tarPipeline.Format = osbuild.TarArchiveFormatUstar
+		tarPipeline.SetFilename(img.Filename)
 
 		imagePipeline = tarPipeline
 	case platform.FORMAT_VHD:
--- pkg/manifest/vagrant.go
+++ pkg/manifest/vagrant.go
@@ -1,15 +1,33 @@
 package manifest
 
 import (
+	"fmt"
+	"math/rand"
+
 	"github.com/osbuild/images/pkg/artifact"
 	"github.com/osbuild/images/pkg/osbuild"
 )
 
 type Vagrant struct {
 	Base
-	filename string
+	filename   string
+	provider   osbuild.VagrantProvider
+	macAddress string
 
 	imgPipeline FilePipeline
+}
+
+// Create a randomized mac address for each build, but generated with a potentially seeded
+// PRNG.
+// See: https://github.com/mirror/vbox/blob/b9657cd5351cf17432b664009cc25bb480dc64c1/src/VBox/Main/src-server/HostImpl.cpp#L3258-L3269
+// for where this implementation comes from.
+func (p *Vagrant) SetRandomMacAddress(prng *rand.Rand) {
+	manafacturer := "080027"
+	serial := make([]byte, 3)
+
+	prng.Read(serial)
+
+	p.macAddress = fmt.Sprintf("%s%x", manafacturer, serial)
 }
 
 func (p Vagrant) Filename() string {
@@ -20,11 +38,12 @@ func (p *Vagrant) SetFilename(filename string) {
 	p.filename = filename
 }
 
-func NewVagrant(buildPipeline Build, imgPipeline FilePipeline) *Vagrant {
+func NewVagrant(buildPipeline Build, imgPipeline FilePipeline, provider osbuild.VagrantProvider) *Vagrant {
 	p := &Vagrant{
 		Base:        NewBase("vagrant", buildPipeline),
 		imgPipeline: imgPipeline,
 		filename:    "image.box",
+		provider:    provider,
 	}
 
 	if buildPipeline != nil {
@@ -39,8 +58,46 @@ func NewVagrant(buildPipeline Build, imgPipeline FilePipeline) *Vagrant {
 func (p *Vagrant) serialize() osbuild.Pipeline {
 	pipeline := p.Base.serialize()
 
+	vagrantOptions := osbuild.NewVagrantStageOptions(p.provider)
+
+	// For the VirtualBox provider we need to inject the ovf stage as well
+	if p.provider == osbuild.VagrantProviderVirtualBox {
+		// When generating a VirtualBox type Vagrant box the mac address *must* be set
+		if len(p.macAddress) == 0 {
+			panic("vagrant with virtualbox provider requires mac address")
+		}
+
+		// The ovf stage needs the vmdk at root, I really don't quite like this
+		// and don't see why it can't be through an input?
+		inputName := "vmdk-tree"
+		pipeline.AddStage(osbuild.NewCopyStageSimple(
+			&osbuild.CopyStageOptions{
+				Paths: []osbuild.CopyStagePath{
+					{
+						From: fmt.Sprintf("input://%s/%s", inputName, p.imgPipeline.Export().Filename()),
+						To:   "tree:///",
+					},
+				},
+			},
+			osbuild.NewPipelineTreeInputs(inputName, p.imgPipeline.Name()),
+		))
+
+		vagrantOptions.SyncedFolders = make(map[string]*osbuild.VagrantSyncedFolderStageOptions)
+		vagrantOptions.SyncedFolders["/vagrant"] = &osbuild.VagrantSyncedFolderStageOptions{
+			Type: osbuild.VagrantSyncedFolderTypeRsync,
+		}
+
+		vagrantOptions.VirtualBox = &osbuild.VagrantVirtualBoxStageOptions{
+			MacAddress: p.macAddress,
+		}
+
+		pipeline.AddStage(osbuild.NewOVFStage(&osbuild.OVFStageOptions{
+			Vmdk: p.imgPipeline.Filename(),
+		}))
+	}
+
 	pipeline.AddStage(osbuild.NewVagrantStage(
-		osbuild.NewVagrantStageOptions(osbuild.VagrantProviderLibvirt),
+		vagrantOptions,
 		osbuild.NewVagrantStagePipelineFilesInputs(p.imgPipeline.Name(), p.imgPipeline.Filename()),
 	))
 
