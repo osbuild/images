79741f67d55c29b6255731067798445f1f923e7e
Michael Vogt <michael.vogt@gmail.com>
2025-05-22 22:20:56 +0200

    rhel10: add support for ISORootKickstart selection
    Some image types (like rhel10/image-installer) put the
    kickstart files into the root of the ISO, some
    others (like fedora/image-installer) do not and because
    its not uniform we need to make it configurable.
    
    It would be nice if we could unify this for rhel-11
    or even rhel-10.x?

--- pkg/distro/defs/loader.go
+++ pkg/distro/defs/loader.go
@@ -322,6 +322,7 @@ type ImageTypeYAML struct {
 	// XXX: this is really here only for compatibility/because of drift in the "imageInstallerImage"
 	// between fedora/rhel
 	KickstartUnattendedExtraKernelOpts []string `yaml:"kickstart_unattended_extra_kernel_opts"`
+	ISORootKickstart                   bool     `yaml:"iso_root_kickstart"`
 
 	RPMOSTree bool `yaml:"rpm_ostree"`
 
