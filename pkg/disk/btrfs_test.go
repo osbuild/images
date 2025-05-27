e11ce0d8e2ccb7bb4cd3ca9fb93190dd7aa8bfd5
Tomáš Hozza <thozza@redhat.com>
2025-05-27 13:02:40 +0200

    pkg/disk/btrfs: support parsing size as string with unit
    Add support for unmarshaling JSON data that specifies size as a string
    with unit and converting it to bytes. This will allow much nicer static
    image definitions.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- pkg/disk/btrfs_test.go
+++ pkg/disk/btrfs_test.go
@@ -1,8 +1,11 @@
 package disk
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
+	"github.com/osbuild/images/pkg/datasizes"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -35,4 +38,51 @@ func TestImplementsInterfacesCompileTimeCheckBtrfs(t *testing.T) {
 	var _ = Mountable(&BtrfsSubvolume{})
 	var _ = Sizeable(&BtrfsSubvolume{})
 	var _ = FSTabEntity(&BtrfsSubvolume{})
+}
+
+func TestUnmarshalSizeUnitStringBtrfsSubvolume(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected uint64
+		err      error
+	}{
+		{
+			name:     "valid size with unit",
+			input:    `{"size": "1 GiB"}`,
+			expected: 1 * datasizes.GiB,
+			err:      nil,
+		},
+		{
+			name:     "valid size without unit",
+			input:    `{"size": 1073741824}`,
+			expected: 1 * datasizes.GiB,
+			err:      nil,
+		},
+		{
+			name:     "valid size without unit as string",
+			input:    `{"size": "123"}`,
+			expected: 123,
+			err:      nil,
+		},
+		{
+			name:     "invalid size with unit",
+			input:    `{"size": "1 GGB"}`,
+			expected: 0,
+			err:      fmt.Errorf("error parsing size in btrfs subvolume: failed to parse size field named \"size\" to bytes: unknown data size units in string: 1 GGB"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var bs BtrfsSubvolume
+			err := json.Unmarshal([]byte(tc.input), &bs)
+			if tc.err != nil {
+				assert.ErrorContains(t, err, tc.err.Error())
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, bs.Size)
+		})
+	}
 }
