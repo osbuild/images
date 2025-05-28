37357ac3ab7f387319904afd7fc24f11115ceb0e
Michael Vogt <michael.vogt@gmail.com>
2025-05-28 17:19:04 +0200

    distro: move legacy code into defs/loader_legacy.go
    We need to keep some compat code around until all distros
    are converted to use the generic distro. This commit moves
    all of this code into `loader_legacy.go` so that we can
    easily `git rm defs/loader_legacy.go` it when we are
    ready.

--- pkg/distro/defs/loader.go
+++ pkg/distro/defs/loader.go
@@ -3,16 +3,13 @@ package defs
 
 import (
 	"bytes"
-	"crypto/sha256"
 	"embed"
 	"errors"
 	"fmt"
-	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"sort"
-	"sync"
 	"text/template"
 
 	"github.com/gobwas/glob"
@@ -405,26 +402,6 @@ func DistroImageConfig(distroNameVer string) (*distro.ImageConfig, error) {
 	return imgConfig, nil
 }
 
-// XXX: compat only, will go away once we move to "generic" distros
-// everywhere
-func PackageSets(it distro.ImageType) (map[string]rpmmd.PackageSet, error) {
-	archName := it.Arch().Name()
-	distroNameVer := it.Arch().Distro().Name()
-
-	// each imagetype can have multiple package sets, so that we can
-	// use yaml aliases/anchors to de-duplicate them
-	toplevel, err := load(distroNameVer)
-	if err != nil {
-		return nil, err
-	}
-
-	imgType, ok := toplevel.ImageTypes[it.Name()]
-	if !ok {
-		return nil, fmt.Errorf("%w: %q", ErrImageTypeNotFound, it.Name())
-	}
-	return imgType.PackageSets(distroNameVer, archName)
-}
-
 // PackageSets loads the PackageSets from the yaml source file
 // discovered via the imagetype.
 func (imgType *imageType) PackageSets(distroNameVer, archName string) (map[string]rpmmd.PackageSet, error) {
@@ -462,23 +439,6 @@ func (imgType *imageType) PackageSets(distroNameVer, archName string) (map[strin
 	return res, nil
 }
 
-// XXX: compat only, will go away once we move to "generic" distros
-// everywhere
-func PartitionTable(it distro.ImageType) (*disk.PartitionTable, error) {
-	distroNameVer := it.Arch().Distro().Name()
-
-	toplevel, err := load(distroNameVer)
-	if err != nil {
-		return nil, err
-	}
-
-	imgType, ok := toplevel.ImageTypes[it.Name()]
-	if !ok {
-		return nil, fmt.Errorf("%w: %q", ErrImageTypeNotFound, it.Name())
-	}
-	return imgType.PartitionTable(distroNameVer, it.Arch().Name())
-}
-
 // PartitionTable returns the partionTable for the given distro/imgType.
 func (imgType *imageType) PartitionTable(distroNameVer, archName string) (*disk.PartitionTable, error) {
 	if imgType.PartitionTables == nil {
@@ -504,122 +464,6 @@ func (imgType *imageType) PartitionTable(distroNameVer, archName string) (*disk.
 	return pt, nil
 }
 
-// Cache the toplevel structure, loading/parsing YAML is quite
-// expensive. This can all be removed in the future where there
-// is a single load for each distroNameVer. Right now the various
-// helpers (like ParititonTable(), ImageConfig() are called a
-// gazillion times. However once we move into the "generic" distro
-// the distro will do a single load/parse of all image types and
-// just reuse them and this can go.
-type imageTypesCache struct {
-	cache map[string]*imageTypesYAML
-	mu    sync.Mutex
-}
-
-func newImageTypesCache() *imageTypesCache {
-	return &imageTypesCache{cache: make(map[string]*imageTypesYAML)}
-}
-
-func (i *imageTypesCache) Get(hash string) *imageTypesYAML {
-	i.mu.Lock()
-	defer i.mu.Unlock()
-
-	return i.cache[hash]
-}
-
-func (i *imageTypesCache) Set(hash string, ity *imageTypesYAML) {
-	i.mu.Lock()
-	defer i.mu.Unlock()
-
-	i.cache[hash] = ity
-}
-
-var (
-	itCache = newImageTypesCache()
-)
-
-func load(distroNameVer string) (*imageTypesYAML, error) {
-	id, err := distro.ParseID(distroNameVer)
-	if err != nil {
-		return nil, err
-	}
-
-	// XXX: this is only needed temporary until we have a "distros.yaml"
-	// that describes some high-level properties of each distro
-	// (like their yaml dirs)
-	var baseDir string
-	switch id.Name {
-	case "rhel", "almalinux", "centos", "almalinux_kitten":
-		// rhel yaml files are under ./rhel-$majorVer
-		// almalinux yaml is just rhel, we take only its major version
-		// centos and kitten yaml is just rhel but we have (sadly) no
-		// symlinks in "go:embed" so we have to have this slightly ugly
-		// workaround
-		baseDir = fmt.Sprintf("rhel-%v", id.MajorVersion)
-	case "test-distro":
-		// our other distros just have a single yaml dir per distro
-		// and use condition.version_gt etc
-		baseDir = id.Name
-	}
-
-	// take the base path from the distros.yaml
-	distro, err := Distro(distroNameVer)
-	if err != nil && !os.IsNotExist(err) {
-		return nil, err
-	}
-	if distro != nil && distro.DefsPath != "" {
-		baseDir = distro.DefsPath
-	}
-
-	f, err := dataFS().Open(filepath.Join(baseDir, "distro.yaml"))
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	// XXX: this is currently needed because rhel distros call
-	// ImageType() and ParitionTable() a gazillion times and
-	// each time the full yaml is loaded. Once things move to
-	// the "generic" distro this will no longer be the case and
-	// this cache can be removed and below we can decode directly
-	// from "f" again instead of wasting memory with "buf"
-	var buf bytes.Buffer
-	h := sha256.New()
-	if _, err := io.Copy(io.MultiWriter(&buf, h), f); err != nil {
-		return nil, fmt.Errorf("cannot read from %s: %w", baseDir, err)
-	}
-	inputHash := string(h.Sum(nil))
-	if cached := itCache.Get(inputHash); cached != nil {
-		return cached, nil
-	}
-
-	var toplevel imageTypesYAML
-	decoder := yaml.NewDecoder(&buf)
-	decoder.KnownFields(true)
-	if err := decoder.Decode(&toplevel); err != nil {
-		return nil, err
-	}
-
-	// XXX: remove once we no longer need caching
-	itCache.Set(inputHash, &toplevel)
-
-	return &toplevel, nil
-}
-
-// XXX: compat only, will go away once we move to "generic" distros
-// everywhere
-func ImageConfig(distroNameVer, archName, typeName string) (*distro.ImageConfig, error) {
-	toplevel, err := load(distroNameVer)
-	if err != nil {
-		return nil, err
-	}
-	imgType, ok := toplevel.ImageTypes[typeName]
-	if !ok {
-		return nil, fmt.Errorf("%w: %q", ErrImageTypeNotFound, typeName)
-	}
-	return imgType.ImageConfig(distroNameVer, archName)
-}
-
 // ImageConfig returns the image type specific ImageConfig
 func (imgType *imageType) ImageConfig(distroNameVer, archName string) (*distro.ImageConfig, error) {
 	imgConfig := imgType.ImageConfigYAML.ImageConfig
@@ -638,22 +482,6 @@ func (imgType *imageType) ImageConfig(distroNameVer, archName string) (*distro.I
 	}
 
 	return imgConfig, nil
-}
-
-// XXX: compat only, will go away once we move to "generic" distros
-// everywhere
-func InstallerConfig(distroNameVer, archName, typeName string) (*distro.InstallerConfig, error) {
-	toplevel, err := load(distroNameVer)
-	if err != nil {
-		return nil, err
-	}
-	imgType, ok := toplevel.ImageTypes[typeName]
-	if !ok {
-		return nil, fmt.Errorf("%w: %q", ErrImageTypeNotFound, typeName)
-	}
-	imgType.name = typeName
-
-	return imgType.InstallerConfig(distroNameVer, archName)
 }
 
 // InstallerConfig returns the InstallerConfig for the given imgType
