2486a4065b0fd6a9daae99cf5e66a27376e07cbc
Brian C. Lane <bcl@redhat.com>
2025-06-16 14:15:47 -0700

    osbuild/squashfs: Add exclude_paths support to org.osbuild.squashfs
    The paths are relative to the root of the filesystem, without leading
    slash. POSIX regular expressions are used to select the files to
    exclude.
    See the manpage for mksquashfs --regex

---
+++ pkg/osbuild/squashfs_stage_test.go
@@ -0,0 +1,93 @@
+package osbuild_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+func TestSquashfsStageJsonMinimal(t *testing.T) {
+	expectedJson := `{
+  "type": "org.osbuild.squashfs",
+  "inputs": {
+    "tree": {
+      "type": "org.osbuild.tree",
+      "origin": "org.osbuild.pipeline",
+      "references": [
+        "name:input-pipeline"
+      ]
+    }
+  },
+  "options": {
+    "filename": "disk.img",
+    "compression": {
+      "method": "xz"
+    }
+  }
+}`
+
+	opts := &osbuild.SquashfsStageOptions{
+		Filename: "disk.img",
+		Compression: osbuild.FSCompression{
+			Method: "xz",
+		},
+	}
+	stage := osbuild.NewSquashfsStage(opts, "input-pipeline")
+	require.NotNil(t, stage)
+
+	js, err := json.MarshalIndent(stage, "", "  ")
+	require.Nil(t, err)
+	assert.Equal(t, expectedJson, string(js))
+}
+
+func TestSquashfsStageJsonFull(t *testing.T) {
+	expectedJson := `{
+  "type": "org.osbuild.squashfs",
+  "inputs": {
+    "tree": {
+      "type": "org.osbuild.tree",
+      "origin": "org.osbuild.pipeline",
+      "references": [
+        "name:input-pipeline"
+      ]
+    }
+  },
+  "options": {
+    "filename": "disk.img",
+    "exclude_paths": [
+      "boot/efi/.*",
+      "boot/initramfs-.*"
+    ],
+    "compression": {
+      "method": "xz",
+      "options": {
+        "bcj": "x86"
+      }
+    }
+  }
+}`
+
+	opts := &osbuild.SquashfsStageOptions{
+		Filename: "disk.img",
+		Compression: osbuild.FSCompression{
+			Method: "xz",
+			Options: &osbuild.FSCompressionOptions{
+				BCJ: "x86",
+			},
+		},
+		ExcludePaths: []string{
+			"boot/efi/.*",
+			"boot/initramfs-.*",
+		},
+	}
+	stage := osbuild.NewSquashfsStage(opts, "input-pipeline")
+	require.NotNil(t, stage)
+
+	js, err := json.MarshalIndent(stage, "", "  ")
+	require.Nil(t, err)
+	assert.Equal(t, expectedJson, string(js))
+}
