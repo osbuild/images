124a1a7fd0654716beed03638e850948b74cdc70
Simon de Vlieger <supakeen@redhat.com>
2025-06-18 11:49:29 +0200

    image_config: extend WSL with distribution
    Extend the image configuration for WSL with the ability to set options
    for the distribution configuration file.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- pkg/distro/image_config.go
+++ pkg/distro/image_config.go
@@ -147,8 +147,24 @@ type WSLConfig struct {
 	BootSystemd bool `yaml:"boot_systemd,omitempty"`
 }
 
+type WSLDistributionOOBEConfig struct {
+	DefaultUID  *int   `yaml:"default_uid,omitempty"`
+	DefaultName string `yaml:"default_name,omitempty"`
+}
+
+type WSLDistributionShortcutConfig struct {
+	Enabled bool   `yaml:"enabled,omitempty"`
+	Icon    string `yaml:"icon,omitempty"`
+}
+
+type WSLDistributionConfig struct {
+	OOBE     *WSLDistributionOOBEConfig     `yaml:"oobe,omitempty"`
+	Shortcut *WSLDistributionShortcutConfig `yaml:"shortcut,omitempty"`
+}
+
 type WSL struct {
-	Config *WSLConfig `yaml:"config,omitempty"`
+	Config             *WSLConfig             `yaml:"config,omitempty"`
+	DistributionConfig *WSLDistributionConfig `yaml:"distribution_config,omitempty"`
 }
 
 // InheritFrom inherits unset values from the provided parent configuration and
