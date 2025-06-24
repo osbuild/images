681ec92a1a9ac32ffa80fdeab42700211a3ed785
Michael Vogt <michael.vogt@gmail.com>
2025-06-24 12:52:39 +0200

    hashutil: add support to hash for http,https as well
    This commit extend the `hashutil.Sha256sum()` to deal
    transparently with http{,s} URLs.

--- pkg/hashutil/sha256sum.go
+++ pkg/hashutil/sha256sum.go
@@ -2,25 +2,49 @@ package hashutil
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 // Sha256sum() is a convenience wrapper to generate
 // the sha256 hex digest of a file. The hash is the
 // same as from the sha256sum util.
-func Sha256sum(path string) (string, error) {
-	f, err := os.Open(path)
+func Sha256sum(uri string) (string, error) {
+	u, err := url.Parse(uri)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("invalid URI for sha256sum: %w", err)
 	}
-	defer f.Close()
+
+	var r io.ReadCloser
+	switch u.Scheme {
+	case "", "file":
+		var err error
+		r, err = os.Open(u.Path)
+		if err != nil {
+			return "", fmt.Errorf("sha256sum: %w", err)
+		}
+	case "http", "https":
+		resp, err := http.Get(u.String())
+		if err != nil {
+			return "", fmt.Errorf("sha256sum cannot get %s: %w", uri, err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			return "", fmt.Errorf("sha256sum cannot fetch %s: %s", uri, resp.Status)
+		}
+		r = resp.Body
+	default:
+		return "", fmt.Errorf("sha256sum does not support scheme %q", u.Scheme)
+	}
+	defer r.Close()
 
 	h := sha256.New()
-	if _, err := io.Copy(h, f); err != nil {
+	if _, err := io.Copy(h, r); err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf("%x", h.Sum(nil)), nil
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
--- pkg/hashutil/sha256sum_test.go
+++ pkg/hashutil/sha256sum_test.go
@@ -1,6 +1,9 @@
 package hashutil_test
 
 import (
+	"fmt"
+	"net"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
@@ -10,34 +13,58 @@ import (
 	"github.com/osbuild/images/pkg/hashutil"
 )
 
-func TestSha256sum(t *testing.T) {
-	for _, tc := range []struct {
-		inp      string
-		expected string
-	}{
-		// test vectors from
-		// https://www.di-mgt.com.au/sha_testvectors.html
-		{
-			"", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-		}, {
-			"abc", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
-		}, {
-			"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
-			"248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
-		},
-	} {
-		t.Run(tc.inp, func(t *testing.T) {
+// test vectors from https://www.di-mgt.com.au/sha_testvectors.html
+var testVectors = map[string]string{
+	"":    "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+	"abc": "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+	"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq": "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+}
+
+func TestSha256sumLocalFile(t *testing.T) {
+	for inp, expected := range testVectors {
+		t.Run(fmt.Sprintf("test-%s", inp), func(t *testing.T) {
 			tmpf := filepath.Join(t.TempDir(), "inp.txt")
-			err := os.WriteFile(tmpf, []byte(tc.inp), 0644)
+			err := os.WriteFile(tmpf, []byte(inp), 0644)
 			assert.NoError(t, err)
 			hash, err := hashutil.Sha256sum(tmpf)
 			assert.NoError(t, err)
-			assert.Equal(t, tc.expected, hash)
+			assert.Equal(t, expected, hash)
 		})
 	}
 }
 
-func TestSha256sumError(t *testing.T) {
+func TestSha256sumHttp(t *testing.T) {
+	ln, err := net.Listen("tcp", "localhost:0")
+	assert.NoError(t, err)
+	srv := &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := w.Write([]byte(r.URL.Path[1.:]))
+			assert.NoError(t, err)
+		}),
+	}
+	defer srv.Close()
+	go srv.Serve(ln)
+
+	for inp, expected := range testVectors {
+		t.Run(fmt.Sprintf("test-http-%s", inp), func(t *testing.T) {
+			hash, err := hashutil.Sha256sum("http://" + ln.Addr().String() + "/" + inp)
+			assert.NoError(t, err)
+			assert.Equal(t, expected, hash)
+		})
+	}
+}
+
+func TestSha256sumErrorLocal(t *testing.T) {
 	_, err := hashutil.Sha256sum("non-existing-file")
-	assert.EqualError(t, err, `open non-existing-file: no such file or directory`)
+	assert.EqualError(t, err, `sha256sum: open non-existing-file: no such file or directory`)
+}
+
+func TestSha256sumErrorHttp(t *testing.T) {
+	_, err := hashutil.Sha256sum("http://example.com/non-existing-file")
+	assert.EqualError(t, err, `sha256sum cannot fetch http://example.com/non-existing-file: 404 Not Found`)
+}
+
+func TestSha256sumErrorScheme(t *testing.T) {
+	_, err := hashutil.Sha256sum("unknown://example.com/file")
+	assert.EqualError(t, err, `sha256sum does not support scheme "unknown"`)
 }
