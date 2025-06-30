1766d1a1154469f922491d93d812ddd8e4c35a57
Simon de Vlieger <supakeen@redhat.com>
2025-06-30 17:56:59 +0200

    manifest/gzip: implement gzip pipeline
    Create the gzip pipeline generator. This is very duplicate from the xz
    and zstd ones, we might want to consolidate later.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ pkg/manifest/gzip_test.go
@@ -0,0 +1,33 @@
+package manifest_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/pkg/manifest"
+	"github.com/osbuild/images/pkg/osbuild"
+	"github.com/osbuild/images/pkg/runner"
+)
+
+func TestGzipSerialize(t *testing.T) {
+	mani := manifest.New()
+	runner := &runner.Linux{}
+	build := manifest.NewBuild(&mani, runner, nil, nil)
+
+	// setup
+	rawImage := manifest.NewRawImage(build, nil)
+	gzipPipeline := manifest.NewGzip(build, rawImage)
+	gzipPipeline.SetFilename("filename.gz")
+
+	// run
+	osbuildPipeline := gzipPipeline.Serialize()
+
+	// assert
+	assert.Equal(t, "gzip", osbuildPipeline.Name)
+	assert.Equal(t, 1, len(osbuildPipeline.Stages))
+	gzipStage := osbuildPipeline.Stages[0]
+	assert.Equal(t, &osbuild.GzipStageOptions{
+		Filename: "filename.gz",
+	}, gzipStage.Options.(*osbuild.GzipStageOptions))
+}
