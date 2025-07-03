d95838040251e7f4c431fb6992a353a6613af9f1
Michael Vogt <michael.vogt@gmail.com>
2025-07-03 09:06:35 +0200

    distro: move rhel-9 into the generic distro
    This commit makes rhel-9 a generic distro.

--- pkg/distro/generic/imagetype.go
+++ pkg/distro/generic/imagetype.go
@@ -321,8 +321,8 @@ func (t *imageType) checkOptions(bp *blueprint.Blueprint, options distro.ImageOp
 		return checkOptionsRhel7(t, bp, options)
 	case manifest.DISTRO_EL8:
 		return checkOptionsRhel8(t, bp, options)
-	// TODO: add checkOptionsRhel9 once we move them to
-	// generic distros
+	case manifest.DISTRO_EL9:
+		return checkOptionsRhel9(t, bp, options)
 	case manifest.DISTRO_EL10:
 		return checkOptionsRhel10(t, bp, options)
 	default:
