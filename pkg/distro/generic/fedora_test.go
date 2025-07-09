f2d384655985db6b4f3493dfc40d8f1bbe3d7aea
Michael Vogt <michael.vogt@gmail.com>
2025-07-09 14:59:25 +0200

    distro: add new `transform_re` to the distro
    This commit adds  a new `transform_re` expression to the distro
    yaml so that a regexp can be used to translate a name into the
    canonical "name-major.minor". This is needed for rhel-8 where
    there is support to create a distro with `rhel-810` instead of
    `rhel-8.10`.
    
    This commit also includes the regexp for rhel8

--- pkg/distro/generic/fedora_test.go
+++ pkg/distro/generic/fedora_test.go
@@ -969,10 +969,6 @@ func TestFedoraDistroFactory(t *testing.T) {
 			expected: nil,
 		},
 		{
-			strID:    "rhel-810",
-			expected: nil,
-		},
-		{
 			strID:    "rhel-8.4.1",
 			expected: nil,
 		},
