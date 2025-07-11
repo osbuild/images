24edf794e3326087ff169494f95a953f2b8682cd
Michael Vogt <michael.vogt@gmail.com>
2025-07-11 10:16:14 +0200

    workflow: run bib container tests like resolver contaner tests
    This commit changes the way the new `bib/container` tests are
    run so that they are similar to the local resolver tests. It
    also adds a `--fail-if-podman-missing` so that we can be sure
    that the tests run in the container workflow.
    
    To be able to share the "hasPodman()" helper the container_test.go
    was moved to the "container_test" package name.
    
    Note that the container tests cannot run in the regular workflow,
    because that is already running inside a podman container and
    nesting them does not work.

--- pkg/bib/container/container_test.go
+++ pkg/bib/container/container_test.go
@@ -1,8 +1,9 @@
-package container
+package container_test
 
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,6 +13,14 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/pkg/bib/container"
+)
+
+var failIfPodmanMissing = flag.Bool(
+	"fail-if-podman-missing",
+	false,
+	"fail instead of skip if podman is missing",
 )
 
 const testingImage = "registry.access.redhat.com/ubi9-micro:latest"
@@ -52,23 +61,53 @@ func getContainerInfo(id string) (containerInfo, error) {
 	return infos[0], nil
 }
 
+func hasExecutable(exe string) bool {
+	path, err := exec.LookPath(exe)
+	return path != "" && err == nil
+}
+
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func hasPodman() bool {
+	// check for podman executable
+	hasPodman := hasExecutable("podman")
+	hasContainerenv := pathExists("/run/.containerenv")
+	var inContainer bool
+	if err := exec.Command("systemd-detect-virt", "-c").Run(); err == nil {
+		inContainer = true
+	}
+	if hasPodman && !hasContainerenv && !inContainer {
+		return true
+	}
+	if *failIfPodmanMissing {
+		panic(fmt.Errorf("podman not fully working: hasPodman=%v, hasContainerenv=%v, inContainer=%v but --fail-if-podman-missing is set", hasPodman, hasContainerenv, inContainer))
+	}
+	return false
+}
+
 func TestNew(t *testing.T) {
 	if os.Geteuid() != 0 {
 		t.Skip("skipping test; not running as root")
 	}
+	if !hasPodman() {
+		t.Skip("skipping test: no podman")
+	}
 
-	c, err := New(testingImage)
+	c, err := container.New(testingImage)
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		err = c.Stop()
 		assert.NoError(t, err)
 
 		// double-check that the container indeed doesn't exist
-		_, infoErr := getContainerInfo(c.id)
-		assert.ErrorIs(t, infoErr, invalidContainerCountError{id: c.id, count: 0})
+		_, infoErr := getContainerInfo(c.ID())
+		assert.ErrorIs(t, infoErr, invalidContainerCountError{id: c.ID(), count: 0})
 	})
 
-	info, err := getContainerInfo(c.id)
+	info, err := getContainerInfo(c.ID())
 	require.NoError(t, err)
 	assert.Equal(t, testingImage, info.Image)
 	assert.Equal(t, "running", info.State)
@@ -84,8 +123,11 @@ func TestReadFile(t *testing.T) {
 	if os.Geteuid() != 0 {
 		t.Skip("skipping test; not running as root")
 	}
+	if !hasPodman() {
+		t.Skip("skipping test: no podman")
+	}
 
-	c, err := New(testingImage)
+	c, err := container.New(testingImage)
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		err = c.Stop()
@@ -101,12 +143,15 @@ func TestCopyInto(t *testing.T) {
 	if os.Geteuid() != 0 {
 		t.Skip("skipping test; not running as root")
 	}
+	if !hasPodman() {
+		t.Skip("skipping test: no podman")
+	}
 
 	tmpdir := t.TempDir()
 	testfile := path.Join(tmpdir, "testfile")
 	require.NoError(t, os.WriteFile(testfile, []byte("Hello, world!"), 0644))
 
-	c, err := New(testingImage)
+	c, err := container.New(testingImage)
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		err = c.Stop()
@@ -131,6 +176,13 @@ func makeFakePodman(t *testing.T, content string) {
 	assert.NoError(t, err)
 }
 func TestNewFakedUnhappy(t *testing.T) {
+	if os.Geteuid() != 0 {
+		t.Skip("skipping test; not running as root")
+	}
+	if !hasPodman() {
+		t.Skip("skipping test: no podman")
+	}
+
 	fakePodman := `#!/bin/sh
 if [ "$1" = "mount" ]; then
     >&2 echo "forced-crash"
@@ -139,7 +191,7 @@ fi
 exec /usr/bin/podman "$@"
 `
 	makeFakePodman(t, fakePodman)
-	_, err := New(testingImage)
+	_, err := container.New(testingImage)
 	assert.ErrorContains(t, err, fmt.Sprintf("mounting %s container failed: ", testingImage))
 	assert.ErrorContains(t, err, "stderr:\nforced-crash")
 }
@@ -153,7 +205,7 @@ func TestRootfsTypeHappy(t *testing.T) {
 		makeFakePodman(t, fmt.Sprintf(`#!/bin/sh
 echo '%s'
 `, jsonStr))
-		cnt := Container{}
+		cnt := container.Container{}
 		rootfs, err := cnt.DefaultRootfsType()
 		assert.NoError(t, err)
 		assert.Equal(t, tc, rootfs)
@@ -166,7 +218,7 @@ func TestRootfsTypeSad(t *testing.T) {
 		makeFakePodman(t, fmt.Sprintf(`#!/bin/sh
 echo '%s'
 `, jsonStr))
-		cnt := Container{}
+		cnt := container.Container{}
 		_, err := cnt.DefaultRootfsType()
 		assert.ErrorContains(t, err, "unsupported root filesystem type: ext1, supported: ")
 	}
