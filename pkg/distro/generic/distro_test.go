96157f79faf55d366eccbcc211c2712dbb8b75b4
Michael Vogt <michael.vogt@gmail.com>
2025-07-21 17:21:42 +0200

    defs: add missing `bootstrap_containers`
    This commit re-adds the `bootstrap_containers` for rhel{7,10}.

--- pkg/distro/generic/distro_test.go
+++ pkg/distro/generic/distro_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/osbuild/images/pkg/distro/defs"
+	testrepos "github.com/osbuild/images/test/data/repositories"
 )
 
 func TestISOLabel(t *testing.T) {
@@ -24,4 +25,21 @@ func TestISOLabel(t *testing.T) {
 
 	isoLabelFunc := d.getISOLabelFunc("iso-label")
 	assert.Equal(t, "name:rhel,major:9,minor:1,product:some-product,arch:some-arch,iso-label:iso-label", isoLabelFunc(imgType))
+}
+
+func TestBootstrapContainers(t *testing.T) {
+	repos, err := testrepos.New()
+	assert.NoError(t, err)
+
+	for _, distroName := range repos.ListDistros() {
+		t.Run(distroName, func(t *testing.T) {
+			d := DistroFactory(distroName)
+			// TODO: remove once everthing is a generic distro
+			if d == nil {
+				t.Skipf("%s not a generic distro yet", distroName)
+			}
+			assert.NotNil(t, d)
+			assert.NotEmpty(t, d.(*distribution).DistroYAML.BootstrapContainers)
+		})
+	}
 }
