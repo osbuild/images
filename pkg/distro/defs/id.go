d8f8a80253fc3169b47375b471e951df4f1138eb
Michael Vogt <michael.vogt@gmail.com>
2025-07-24 16:54:03 +0200

    distro: make `matchAndNormalize` more robust and add test
    This commit adds a test for the matchAndNormalize() helper
    that checks for invalid/missing capture group names. This
    uncovered a crash in the code that is also fixed.
    
    Thanks to Thozza and Achilleas for suggesting this.

--- pkg/distro/defs/id.go
+++ pkg/distro/defs/id.go
@@ -7,6 +7,15 @@ import (
 	"github.com/osbuild/images/pkg/distro"
 )
 
+func validateSubexpMatch(re *regexp.Regexp, names ...string) error {
+	for _, name := range names {
+		if re.SubexpIndex(name) == -1 {
+			return fmt.Errorf("cannot find submatch field %q", name)
+		}
+	}
+	return nil
+}
+
 // matchAndNormalize() matches and normalizes the given nameVer
 // based on the reStr. On match it returns the normalized version
 // of the given nameVer.
@@ -32,9 +41,15 @@ func matchAndNormalize(reStr, nameVer string) (string, error) {
 		return "", fmt.Errorf("invalid number of submatches for %q %q (%v)", reStr, nameVer, len(l))
 	case 3:
 		// distro only uses major ver and needs normalizing
+		if err := validateSubexpMatch(re, "name", "major"); err != nil {
+			return "", err
+		}
 		return fmt.Sprintf("%s-%s", l[re.SubexpIndex("name")], l[re.SubexpIndex("major")]), nil
 	case 4:
 		// common case, major/minor and normalizing
+		if err := validateSubexpMatch(re, "name", "major", "minor"); err != nil {
+			return "", err
+		}
 		return fmt.Sprintf("%s-%s.%s", l[re.SubexpIndex("name")], l[re.SubexpIndex("major")], l[re.SubexpIndex("minor")]), nil
 	default:
 		return "", fmt.Errorf("invalid number of submatches for %q %q (%v)", reStr, nameVer, len(l))
