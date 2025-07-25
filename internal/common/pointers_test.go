10c690b7e203058d90eb588f7791ac9c7b5630ef
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-07-25 12:32:14 +0200

    common: add ValueOrEmpty helper
    Derefs a pointer, if the pointer is nil returns the default value.

--- internal/common/pointers_test.go
+++ internal/common/pointers_test.go
@@ -24,3 +24,37 @@ func TestToPtr(t *testing.T) {
 	assert.Equal(t, valueStr, *gotStr)
 
 }
+
+func TestValueOrEmpty(t *testing.T) {
+	var ptrInt *int
+	valueInt := ValueOrEmpty(ptrInt)
+	assert.Equal(t, 0, valueInt)
+	helperInt := 20
+	ptrInt = &helperInt
+	valueInt = ValueOrEmpty(ptrInt)
+	assert.Equal(t, 20, valueInt)
+
+	var ptrBool *bool
+	valueBool := ValueOrEmpty(ptrBool)
+	assert.Equal(t, false, valueBool)
+	helperBool := true
+	ptrBool = &helperBool
+	valueBool = ValueOrEmpty(ptrBool)
+	assert.Equal(t, true, valueBool)
+
+	var ptrUint64 *uint64
+	valueUint64 := ValueOrEmpty(ptrUint64)
+	assert.Equal(t, uint64(0), valueUint64)
+	helperUint64 := uint64(20)
+	ptrUint64 = &helperUint64
+	valueUint64 = ValueOrEmpty(ptrUint64)
+	assert.Equal(t, uint64(20), valueUint64)
+
+	var ptrString *string
+	valueString := ValueOrEmpty(ptrString)
+	assert.Equal(t, "", valueString)
+	helperString := "the-greatest-test-value"
+	ptrString = &helperString
+	valueString = ValueOrEmpty(ptrString)
+	assert.Equal(t, "the-greatest-test-value", valueString)
+}
