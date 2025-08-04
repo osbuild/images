9d0c0e109cf4acf2245d777be94c7f41248a0a8f
Lukas Zapletal <lzap+git@redhat.com>
2025-08-04 19:18:58 +0200

    many: use type

--- pkg/distro/defs/loader.go
+++ pkg/distro/defs/loader.go
@@ -43,7 +43,7 @@ var defaultDataFS fs.FS = data
 func dataFS() fs.FS {
 	// XXX: this is a short term measure, pass a set of
 	// searchPaths down the stack instead
-	var dataFS fs.FS = defaultDataFS
+	dataFS := defaultDataFS
 	if overrideDir := experimentalflags.String("yamldir"); overrideDir != "" {
 		olog.Printf("WARNING: using experimental override dir %q", overrideDir)
 		dataFS = os.DirFS(overrideDir)
