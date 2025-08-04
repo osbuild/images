24b621fcfabf57ac343c669ea7d00742d4660b9e
Lukas Zapletal <lzap+git@redhat.com>
2025-08-04 17:04:31 +0200

    many: simplify vars via type inference
    Linter does not like separate variable declaration and assignment and
    since it is the default rule, this patch fixes several places.

--- internal/common/pointers_test.go
+++ internal/common/pointers_test.go
@@ -7,19 +7,19 @@ import (
 )
 
 func TestToPtr(t *testing.T) {
-	var valueInt int = 42
+	valueInt := 42
 	gotInt := ToPtr(valueInt)
 	assert.Equal(t, valueInt, *gotInt)
 
-	var valueBool bool = true
+	valueBool := true
 	gotBool := ToPtr(valueBool)
 	assert.Equal(t, valueBool, *gotBool)
 
-	var valueUint64 uint64 = 1
+	valueUint64 := uint64(1)
 	gotUint64 := ToPtr(valueUint64)
 	assert.Equal(t, valueUint64, *gotUint64)
 
-	var valueStr string = "the-greatest-test-value"
+	valueStr := "the-greatest-test-value"
 	gotStr := ToPtr(valueStr)
 	assert.Equal(t, valueStr, *gotStr)
 
