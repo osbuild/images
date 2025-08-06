1c347eed5733af2f8e3baae622dc60501aa5bcd5
Michael Vogt <michael.vogt@gmail.com>
2025-08-06 17:30:18 +0200

    manifest: determine pipelines as payload/build in manifest
    This commit makes it the job of the manifest package to
    determine if a pipeline is a build pipeline or a payload
    pipeline (instead of describing it in the image type and
    the YAML as we do currently).
    
    Moving the knownledge about payload/build pipelines out of
    the imagetypes will make the information more precise, the
    current way to report the payload/build is quite loose and
    easy to get wrong.
    
    We currently have a `manifest.Build` interface that is
    implemented by the traditional `BuildrootFromPackages`
    and the `BuildrootFromContainer` types. All payload
    pipelines use them when doing:
    ```go
    NewBase(name string, build Build)
    ```
    and the type system will ensure that all build pipelines
    used by payload pipelines implement the `Build`
    interface.
    
    With that we can implement "BuildPipelines()" by
    just checking if a pipeline implements the "Build"
    interface (and the inverse for PayloadPipelines).
    
    What is nice is that this approach is exteremly simple
    and does not require any new concepts (like the
    PipelineRole in the previous PR#1732 that also attempts
    to solve this problem). In the future we could do
    more advanced things like building the dependency tree
    of the pipelines to see what is build/payload (as
    Tomáš suggested) but currently we don't need this as
    all our build pipelines implement the build interface
    and the payload ones don't.
    
    I guess one downside might be if we ever build something
    more complicated where a build pipeline would double as
    a payload pipeline (i.e. something that implements both
    interfaces) but we can think about this if/when we
    do that (YAGNI and all that).

---
+++ pkg/manifest/pipeline_test.go
@@ -0,0 +1,34 @@
+package manifest_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/pkg/manifest"
+	"github.com/osbuild/images/pkg/runner"
+)
+
+func TestPipelineRoleBuild(t *testing.T) {
+	var mf manifest.Manifest
+	manifest.NewBuild(&mf, &runner.Linux{}, nil, nil)
+	assert.Equal(t, []string{"build"}, mf.BuildPipelines())
+	assert.Equal(t, 0, len(mf.PayloadPipelines()))
+}
+
+func TestPipelineRoleBuildFromContainer(t *testing.T) {
+	var mf manifest.Manifest
+	manifest.NewBuildFromContainer(&mf, &runner.Linux{}, nil, nil)
+	assert.Equal(t, []string{"build"}, mf.BuildPipelines())
+	assert.Equal(t, 0, len(mf.PayloadPipelines()))
+}
+
+func TestPipelineRolePayload(t *testing.T) {
+	var mf manifest.Manifest
+
+	bp := manifest.NewBuild(&mf, &runner.Linux{}, nil, nil)
+
+	manifest.NewXZ(bp, nil)
+	assert.Equal(t, []string{"xz"}, mf.PayloadPipelines())
+	assert.Equal(t, []string{"build"}, mf.BuildPipelines())
+}
