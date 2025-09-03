871b82eacd7ec74f3eccdaffa782029737f8a747
Michael Vogt <michael.vogt@gmail.com>
2025-09-03 14:41:50 +0200

    bootc: add component tests based on fake containers
    This commit introduces a fake bootc container that contains
    just enough content to simulate a bootc image. It is based
    on a "FROM scartch" container and does not require a network
    connection. It will build a static go binary to provide the
    "sleep" binary and the "bootc" binary that is expected by
    the bootc introspection code in images.
    
    The code to geneate fake bootc containers is put into
    pkg/distro/bootc/bootctest
    and can potentially be reused for manifest tests and
    for ibcli/bib.
    
    Thanks to Alex for suggesting the static go binary.

---
+++ pkg/distro/bootc/bootctest/bootctest.go
@@ -0,0 +1,118 @@
+package bootctest
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/internal/randutil"
+)
+
+func makeOsRelease(t *testing.T, buildDir string) {
+	osRelease := `
+NAME="bootc-fake-name"
+ID="bootc-fake"
+VERSION_ID="1"
+`
+
+	osReleasePath := filepath.Join(buildDir, "etc/os-release")
+	err := os.MkdirAll(filepath.Dir(osReleasePath), 0755)
+	require.NoError(t, err)
+	//nolint:gosec
+	err = os.WriteFile(osReleasePath, []byte(osRelease), 0644)
+	require.NoError(t, err)
+}
+
+func makeUsrBinInstall(t *testing.T, buildDir string) {
+	installToml := `
+[install]
+filesystem = [
+    { mountpoint = "/", type = "xfs", size = "10 GiB" },
+    { mountpoint = "/boot", type = "ext4", size = "1 GiB" },
+]
+`
+
+	installTomlPath := filepath.Join(buildDir, "usr/lib/bootc/install/99-fedora-install.toml")
+	err := os.MkdirAll(filepath.Dir(installTomlPath), 0755)
+	require.NoError(t, err)
+	//nolint:gosec
+	err = os.WriteFile(installTomlPath, []byte(installToml), 0644)
+	require.NoError(t, err)
+}
+
+func makeFakeBinaries(t *testing.T, buildDir string) {
+	_, currentFile, _, ok := runtime.Caller(0)
+	require.True(t, ok)
+	currentDir := filepath.Dir(currentFile)
+
+	fakeBootcPath := filepath.Join(buildDir, "usr/bin/bootc")
+	err := os.MkdirAll(filepath.Dir(fakeBootcPath), 0755)
+	require.NoError(t, err)
+	cmd := exec.Command(
+		"go", "build",
+		"-o", fakeBootcPath,
+		filepath.Join(currentDir, "./exe"),
+	)
+	cmd.Env = append(os.Environ(), "CGO_ENABLED=0")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		require.NoError(t, err, string(output))
+	}
+
+	fakeSleepPath := filepath.Join(buildDir, "usr/bin/sleep")
+	err = os.Symlink("bootc", fakeSleepPath)
+	require.NoError(t, err)
+}
+
+func makeContainerfile(t *testing.T, buildDir string) {
+	var fakeBootcCnt = `
+FROM scratch
+COPY etc /etc
+COPY usr/bin /usr/bin
+COPY usr/lib/bootc/install /usr/lib/bootc/install 
+`
+
+	cntFilePath := filepath.Join(buildDir, "Containerfile")
+	//nolint:gosec
+	err := os.WriteFile(cntFilePath, []byte(fakeBootcCnt), 0644)
+	require.NoError(t, err)
+}
+
+func makeFakeContainerImage(t *testing.T, buildDir, purpose string) string {
+	imgTag := fmt.Sprintf("image-builder-test-%s-%s", purpose, randutil.String(10, randutil.AsciiLower))
+	//nolint:gosec
+	output, err := exec.Command(
+		"podman", "build",
+		"-f", filepath.Join(buildDir, "Containerfile"),
+		"-t", imgTag,
+	).CombinedOutput()
+	require.NoError(t, err, string(output))
+	// add cleanup
+	t.Cleanup(func() {
+		output, err := exec.Command("podman", "image", "rm", imgTag).CombinedOutput()
+		assert.NoError(t, err, string(output))
+	})
+
+	return fmt.Sprintf("localhost/%s", imgTag)
+}
+
+func NewFakeContainer(t *testing.T, purpose string) string {
+	t.Helper()
+
+	buildDir := t.TempDir()
+
+	// XXX: allow adding test specific content
+	makeContainerfile(t, buildDir)
+	makeFakeBinaries(t, buildDir)
+	// XXX: make os-release content configurable
+	makeOsRelease(t, buildDir)
+	makeUsrBinInstall(t, buildDir)
+
+	return makeFakeContainerImage(t, buildDir, purpose)
+}
