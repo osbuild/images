f426f31366352da7faf3d42f46f1d8db810aff97
Michael Vogt <michael.vogt@gmail.com>
2025-09-04 10:11:05 +0200

    internal: add randutil.String() helper
    This commit adds a new `randutil.String()` helper
    that will generate a random lower/upper/digest/symbols
    string by default.
    
    The implements is inspired by pythons random.choice(),
    i.e. it is possible to pass a different set of strings
    to generate the a way to generate the random string
    from. This is useful to generate random container
    IDs for example.

---
+++ internal/randutil/randutil_test.go
@@ -0,0 +1,33 @@
+package randutil_test
+
+import (
+	"testing"
+	"unicode"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/images/internal/randutil"
+)
+
+func TestChoiceSimple(t *testing.T) {
+	n := 128
+
+	r1 := randutil.String(n)
+	r2 := randutil.String(n)
+	assert.NotEqual(t, r1, r2)
+	assert.Len(t, r1, n)
+	assert.Len(t, r2, n)
+	// by default we pick lower,upper,digit,symbols, ensure
+	// we do not stray from this range
+	defaultChars := randutil.AsciiLower + randutil.AsciiUpper + randutil.AsciiDigit + randutil.AsciiSymbol
+	for _, char := range r1 {
+		assert.Contains(t, []rune(defaultChars), char)
+	}
+}
+
+func TestChoiceHonorsSeq(t *testing.T) {
+	result := randutil.String(128, randutil.AsciiDigit)
+	for _, char := range result {
+		assert.True(t, unicode.IsDigit(char))
+	}
+}
