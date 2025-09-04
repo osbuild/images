1eb53a8f21ac158079fb2be9dd30f89909dc13d4
Brian C. Lane <bcl@redhat.com>
2025-09-03 17:28:49 -0700

    image: Add PXE tar image creation
    This will create a tar of a directory containing a kernel, initrd,
    compressed root filesystem, example grub2 config, grub2 EFI files and a
    README.

---
+++ pkg/image/pxetar_test.go
@@ -0,0 +1,97 @@
+package image_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/pkg/dnfjson"
+	"github.com/osbuild/images/pkg/image"
+	"github.com/osbuild/images/pkg/manifest"
+	"github.com/osbuild/images/pkg/rpmmd"
+	"github.com/osbuild/images/pkg/runner"
+)
+
+func TestPXETarNoCustomizations(t *testing.T) {
+	img := image.NewPXETar(testPlatform, "test-pxe.tar.xz")
+	require.NotNil(t, img)
+
+	img.Compression = "xz"
+	img.OSVersion = "42"
+
+	source := rand.NewSource(int64(0))
+	// math/rand is good enough in this case
+	/* #nosec G404 */
+	rng := rand.New(source)
+
+	mf := manifest.New()
+	_, err := img.InstantiateManifest(&mf, nil, &runner.Fedora{}, rng)
+	require.NoError(t, err)
+
+	// Fake packages to keep serialization happy
+	packages := map[string]dnfjson.DepsolveResult{
+		"build": {
+			Packages: []rpmmd.PackageSpec{
+				{
+					Name:     "coreutils",
+					Checksum: "sha256:cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+				},
+			},
+		},
+		"os": {
+			Packages: []rpmmd.PackageSpec{
+				{
+					Name:     "kernel",
+					Checksum: "sha256:eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+				},
+			},
+		},
+	}
+
+	osbm, err := mf.Serialize(packages, nil, nil, nil)
+	require.NoError(t, err)
+
+	// find the tar stage in the tar pipeline
+	pipeline := findPipelineFromOsbuildManifest(t, osbm, "tar")
+	assert.NotNil(t, pipeline)
+	stage := findStageFromOsbuildPipeline(t, pipeline, "org.osbuild.tar")
+	assert.NotNil(t, stage, "org.osbuild.tar stage not found")
+
+	// check inputs for name:pxe-tree
+	inputs := stage["inputs"].(map[string]any)
+	require.NotNil(t, inputs)
+	tree := inputs["tree"].(map[string]any)
+	require.NotNil(t, tree)
+	refstring := tree["references"].([]any)
+	require.NotNil(t, refstring)
+	require.Equal(t, 1, len(refstring))
+	assert.Equal(t, "name:pxe-tree", refstring[0])
+
+	// check filename for image.tar
+	stageOptions := stage["options"].(map[string]any)
+	require.NotNil(t, stageOptions)
+	assert.Equal(t, "image.tar", stageOptions["filename"].(string))
+
+	// find the xz stage in the xz pipeline
+	pipeline = findPipelineFromOsbuildManifest(t, osbm, "xz")
+	assert.NotNil(t, pipeline)
+	stage = findStageFromOsbuildPipeline(t, pipeline, "org.osbuild.xz")
+	assert.NotNil(t, stage, "org.osbuild.xz stage not found")
+
+	// check inputs for name:tar and file: image.tar
+	inputs = stage["inputs"].(map[string]any)
+	require.NotNil(t, inputs)
+	file := inputs["file"].(map[string]any)
+	require.NotNil(t, file)
+	refmap := file["references"].(map[string]any)
+	require.NotNil(t, refmap)
+	namepxe := refmap["name:tar"].(map[string]any)
+	assert.Equal(t, "image.tar", namepxe["file"].(string))
+
+	// check filename for pxe.tar.xz
+	stageOptions = stage["options"].(map[string]any)
+	require.NotNil(t, stageOptions)
+	assert.Equal(t, "test-pxe.tar.xz", stageOptions["filename"].(string))
+}
