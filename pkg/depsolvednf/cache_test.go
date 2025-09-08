53ecd32d5185e5aa461bd4f0384e3bbdc64e47e8
Tomáš Hozza <thozza@redhat.com>
2025-09-08 16:03:13 +0200

    pkg/depsolvednf: use rpmmd.PackageListNG for `dump` and `search`
    This is a step towards removing the original rpmmd.PackageList and
    rpmmd.Package structures.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- pkg/depsolvednf/cache_test.go
+++ pkg/depsolvednf/cache_test.go
@@ -307,8 +307,8 @@ func TestCacheCleanup(t *testing.T) {
 }
 
 // Mock package list to use in testing
-var PackageList = rpmmd.PackageList{
-	rpmmd.Package{
+var PackageList = rpmmd.PackageListNG{
+	rpmmd.PackageNG{
 		Name:        "package0",
 		Summary:     "package summary",
 		Description: "package description",
