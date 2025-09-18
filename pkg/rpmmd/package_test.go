9ddd4d9ff08dc7b84bc6a7321d1fab18f31ca451
Tomáš Hozza <thozza@redhat.com>
2025-09-18 10:09:12 +0200

    pkg/rpmmd: add Package() method for PackageList type
    This is needed in order to replace PackageSpec use by Package and the
    use of Getpackage() function for []PackageSpec lists.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- pkg/rpmmd/package_test.go
+++ pkg/rpmmd/package_test.go
@@ -2,6 +2,7 @@ package rpmmd_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/osbuild/images/pkg/rpmmd"
@@ -10,6 +11,63 @@ import (
 
 func TestChecksumString(t *testing.T) {
 	assert.Equal(t, "sha256:1234567890", (&rpmmd.Checksum{Type: "sha256", Value: "1234567890"}).String())
+}
+
+var packageList = rpmmd.PackageList{
+	{
+		Name:    "tmux",
+		Epoch:   0,
+		Version: "3.3a",
+		Release: "3.fc38",
+		Arch:    "x86_64",
+	},
+	{
+		Name:    "grub2",
+		Epoch:   1,
+		Version: "2.06",
+		Release: "94.fc38",
+		Arch:    "noarch",
+	},
+}
+
+func TestGetPackagePackageList(t *testing.T) {
+	testCases := []struct {
+		packages        rpmmd.PackageList
+		packageName     string
+		expectedPackage *rpmmd.Package
+		expectedError   error
+	}{
+		{
+			packages:        packageList,
+			packageName:     "grub2",
+			expectedPackage: &packageList[1],
+		},
+		{
+			packages:        packageList,
+			packageName:     "not-a-package",
+			expectedPackage: nil,
+			expectedError:   fmt.Errorf("package \"not-a-package\" not found in the Package list"),
+		},
+		{
+			packages:        rpmmd.PackageList{},
+			packageName:     "tmux",
+			expectedPackage: nil,
+			expectedError:   fmt.Errorf("package \"tmux\" not found in the Package list"),
+		},
+		{
+			packages:        nil,
+			packageName:     "tmux",
+			expectedPackage: nil,
+			expectedError:   fmt.Errorf("package \"tmux\" not found in the Package list"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.packageName, func(t *testing.T) {
+			pkg, err := tc.packages.Package(tc.packageName)
+			assert.Equal(t, tc.expectedError, err)
+			assert.Equal(t, tc.expectedPackage, pkg)
+		})
+	}
 }
 
 var specs = []rpmmd.PackageSpec{
