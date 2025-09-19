d806aa2747470fbb24e4ae9f6ec6212e3d3988d3
Brian C. Lane <bcl@redhat.com>
2025-09-19 09:32:31 -0700

    manifest,osbuild: Change lorax templates to a list with branding
    More than one lorax template can be run, and the runtime-cleanup.tmpl
    will need to know about the logo and release branding packages so
    support for that has been added to the lorax-script stage. This also
    changes to using a list of templates to run, and it gets the template package name
    from the yaml instead of using a bool to select between hardcoded
    options (that wasn't hooked up, so it always used the 99-generic one).

--- pkg/manifest/anaconda_installer.go
+++ pkg/manifest/anaconda_installer.go
@@ -137,13 +137,9 @@ func (p *AnacondaInstaller) getBuildPackages(Distro) []string {
 		"shadow-utils", // The pipeline always creates a root and installer user
 	)
 
-	if p.InstallerCustomizations.UseRHELLoraxTemplates {
+	if len(p.InstallerCustomizations.LoraxTemplatePackage) > 0 {
 		packages = append(packages,
-			"lorax-templates-rhel",
-		)
-	} else {
-		packages = append(packages,
-			"lorax-templates-generic",
+			p.InstallerCustomizations.LoraxTemplatePackage,
 		)
 	}
 
@@ -308,14 +304,16 @@ func (p *AnacondaInstaller) payloadStages() []*osbuild.Stage {
 		stages = append(stages, osbuild.NewAnacondaStage(anacondaStageOptions))
 	}
 
-	LoraxPath := "99-generic/runtime-postinstall.tmpl"
-	if p.InstallerCustomizations.UseRHELLoraxTemplates {
-		LoraxPath = "80-rhel/runtime-postinstall.tmpl"
+	for _, tmpl := range p.InstallerCustomizations.LoraxTemplates {
+		stages = append(stages, osbuild.NewLoraxScriptStage(&osbuild.LoraxScriptStageOptions{
+			Path: tmpl,
+			Branding: osbuild.Branding{
+				Release: p.InstallerCustomizations.LoraxReleasePackage,
+				Logos:   p.InstallerCustomizations.LoraxLogosPackage,
+			},
+			BaseArch: p.platform.GetArch().String(),
+		}))
 	}
-	stages = append(stages, osbuild.NewLoraxScriptStage(&osbuild.LoraxScriptStageOptions{
-		Path:     LoraxPath,
-		BaseArch: p.platform.GetArch().String(),
-	}))
 
 	// Create a generic initrd suitable for booting Anaconda and activating supported hardware
 	dracutOptions := p.dracutStageOptions()
