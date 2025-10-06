08f08d54f69f9080be80f0d87c381835109408e1
Michael Vogt <michael.vogt@gmail.com>
2025-10-06 16:29:33 +0200

    datasizes: add support for YAML unmarshal of datasizes
    Add support to unmarshal YAML based datasizes just like we
    support TOML and JSON. This is extremly simple and just
    reuses the existing `common.UnmarshalYAMLviaJSON()`.

--- pkg/datasizes/size_test.go
+++ pkg/datasizes/size_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 
 	"github.com/osbuild/images/pkg/datasizes"
 )
@@ -78,29 +79,67 @@ func TestSizeUnmarshalJSONUnhappy(t *testing.T) {
 	}
 }
 
+func TestSizeUnmarshalYAMLUnhappy(t *testing.T) {
+	cases := []struct {
+		name  string
+		input string
+		err   string
+	}{
+		{
+			name:  "misize nor string nor int",
+			input: `size: true`,
+			err:   `unmarshal yaml via json for true failed: error decoding size: failed to convert value "true" to number`,
+		},
+		{
+			name:  "wrong datatype/float",
+			input: `size: 3.14`,
+			err:   `unmarshal yaml via json for 3.14 failed: error decoding size: strconv.ParseInt: parsing "3.14": invalid syntax`,
+		},
+		{
+			name:  "misize not parseable",
+			input: `size: "20 KG"`,
+			err:   `unmarshal yaml via json for "20 KG" failed: error decoding size: unknown data size units in string: 20 KG`,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var v struct {
+				Size datasizes.Size `json:"size"`
+			}
+			err := yaml.Unmarshal([]byte(tc.input), &v)
+			assert.EqualError(t, err, tc.err, tc.input)
+		})
+	}
+}
+
 func TestSizeUnmarshalHappy(t *testing.T) {
 	cases := []struct {
 		name      string
 		inputJSON string
 		inputTOML string
+		inputYAML string
 		expected  datasizes.Size
 	}{
 		{
 			name:      "int",
 			inputJSON: `{"size": 1234}`,
 			inputTOML: `size = 1234`,
+			inputYAML: `size: 1234`,
 			expected:  1234,
 		},
 		{
 			name:      "str",
 			inputJSON: `{"size": "1234"}`,
 			inputTOML: `size = "1234"`,
+			inputYAML: `size: "1234"`,
 			expected:  1234,
 		},
 		{
 			name:      "str/with-unit",
 			inputJSON: `{"size": "1234 MiB"}`,
 			inputTOML: `size = "1234 MiB"`,
+			inputYAML: `size: "1234 MiB"`,
 			expected:  1234 * datasizes.MiB,
 		},
 	}
@@ -116,6 +155,10 @@ func TestSizeUnmarshalHappy(t *testing.T) {
 			err = json.Unmarshal([]byte(tc.inputJSON), &v)
 			assert.NoError(t, err)
 			assert.Equal(t, tc.expected, v.Size, tc.inputJSON)
+
+			err = yaml.Unmarshal([]byte(tc.inputYAML), &v)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, v.Size, tc.inputYAML)
 		})
 	}
 }
