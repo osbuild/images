d4933ac8039a9c76b99fb1a61f9cbf4d790d5d62
Tomáš Hozza <thozza@redhat.com>
2025-10-16 21:44:09 +0200

    pkg/remotefile: refactor and don't use internal/worker/clienterrors
    Rework the remotefile resolver to not embed the resolution error in the
    Spec definition, but instead return error from the resolver `Finish()`
    method. This is consistent with i.e. the container resolver.
    
    Additionally, don't use clienterrors package for errors.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- pkg/remotefile/spec.go
+++ pkg/remotefile/spec.go
@@ -1,9 +1,6 @@
 package remotefile
 
-import "github.com/osbuild/images/internal/worker/clienterrors"
-
 type Spec struct {
-	URL             string
-	Content         []byte
-	ResolutionError *clienterrors.Error
+	URL     string
+	Content []byte
 }
