a678908a89a636e8eecbf027d6ab5cf785b85c23
Simon de Vlieger <supakeen@redhat.com>
2025-11-06 11:30:58 +0100

    many: allow OCI archive options in image config
    OCI archive options were hard coded in the various pipelines, or part of
    the image types themselves. This commit moves them into a customization
    under the OSCustomizations.
    
    This moves visibility of these properties into the YAML and potentially
    in the future will allow setting (some of these) properties through
    blueprints.
    
    Note that there are still some dirty bits left. The options for the
    nginx config path and port are currently hardcoded in Go code as well.
    These should move into image config as well. I'll do that next.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ pkg/customizations/oci/oci.go
@@ -0,0 +1,16 @@
+package oci
+
+type OCIArchiveConfig struct {
+	Cmd          []string          `yaml:"cmd,omitempty"`
+	Env          []string          `yaml:"env,omitempty"`
+	ExposedPorts []string          `yaml:"exposed_ports,omitempty"`
+	User         string            `yaml:"user,omitempty"`
+	Labels       map[string]string `yaml:"labels,omitempty"`
+	StopSignal   string            `yaml:"stop_signal,omitempty"`
+	Volumes      []string          `yaml:"volumes,omitempty"`
+	WorkingDir   string            `yaml:"working_dir,omitempty"`
+}
+
+type OCI struct {
+	Archive *OCIArchiveConfig `yaml:"archive,omitempty"`
+}
--- pkg/distro/generic/images.go
+++ pkg/distro/generic/images.go
@@ -358,6 +358,14 @@ func osCustomizations(t *imageType, osPackageSet rpmmd.PackageSet, options distr
 	return osc, nil
 }
 
+func ociContainerCustomizations(t *imageType) manifest.OCIContainerCustomizations {
+	imageConfig := t.getDefaultImageConfig()
+
+	return manifest.OCIContainerCustomizations{
+		OCIArchiveConfig: osbuild.NewOCIArchiveConfig(imageConfig.OCI),
+	}
+}
+
 func installerCustomizations(t *imageType, c *blueprint.Customizations) (manifest.InstallerCustomizations, error) {
 	d := t.arch.distro
 	isoLabel, err := t.ISOLabel()
@@ -582,6 +590,8 @@ func containerImage(t *imageType,
 	img.OSCustomizations.PayloadRepos = payloadRepos
 	img.Environment = &t.ImageTypeYAML.Environment
 
+	img.OCIContainerCustomizations = ociContainerCustomizations(t)
+
 	return img, nil
 }
 
@@ -768,6 +778,8 @@ func iotContainerImage(t *imageType,
 	img.OSTreeParent = parentCommit
 	img.OSVersion = d.OsVersion()
 	img.ExtraContainerPackages = packageSets[containerPkgsKey]
+
+	img.OCIContainerCustomizations = ociContainerCustomizations(t)
 
 	return img, nil
 }
