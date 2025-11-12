c714ef5a33e08135acac191f36664b35b658e34c
Lukas Zapletal <lzap+git@redhat.com>
2025-11-12 15:13:04 +0100

    manifest: add treeinfo stage
    Generated ISO files are missing .treeinfo file and thus it cannot be
    used easily with virt-install/virt-manager/cockpit to install VMs. This
    commit adds a new TreeinfoStage to osbuild that generates the .treeinfo
    file based on the parameters passed from the anaconda installer ISO
    manifest.
    
    All fields supported by the new osbuild stage are defined, but one is
    currently skipped: the ShortName because we do not have it in
    distrodefs.

---
+++ pkg/osbuild/treeinfo_stage.go
@@ -0,0 +1,134 @@
+package osbuild
+
+import (
+	"time"
+
+	"github.com/osbuild/images/pkg/arch"
+)
+
+// TreeinfoStageOptions represents the options of the treeinfo stage.
+// All paths are relative to .treeinfo location.
+type TreeinfoStageOptions struct {
+	Path     string       `json:"path"`     // path to the treeinfo file, must be set to ".treeinfo"
+	Treeinfo TreeinfoData `json:"treeinfo"` // treeinfo data
+}
+
+// TreeinfoData represents the data stored in the .treeinfo file.
+type TreeinfoData struct {
+	Release   *TreeinfoReleaseStageOptions `json:"release,omitempty"`   // release information
+	Tree      *TreeinfoTreeStageOptions    `json:"tree,omitempty"`      // tree information
+	Checksums []string                     `json:"checksums,omitempty"` // filenames of files in a tree to calculate shasum for
+	Stage2    *TreeinfoStage2StageOptions  `json:"stage2,omitempty"`    // stage2 information (only for Anaconda)
+
+	Images   map[string]map[string]string           `json:"images,omitempty"`   // images compatible with particular $platform - serialized as "images-<arch>"
+	Variants map[string]TreeinfoVariantStageOptions `json:"variants,omitempty"` // serialized as "variant-<uid>"
+
+	General *TreeinfoGeneralStageOptions `json:"general,omitempty"` // generated automatically for backwards compatibility
+}
+
+// TreeinfoGeneralStageOptions is a generated section for backwards compatibility.
+type TreeinfoGeneralStageOptions struct {
+	Arch       string   `json:"arch,omitempty"`
+	Family     string   `json:"family,omitempty"`
+	Name       string   `json:"name,omitempty"`
+	Packagedir string   `json:"packagedir,omitempty"`
+	Platforms  []string `json:"platforms,omitempty"`
+	Repository string   `json:"repository,omitempty"`
+	Timestamp  int64    `json:"timestamp,omitempty"`
+	Variant    string   `json:"variant,omitempty"`
+	Version    string   `json:"version,omitempty"`
+}
+
+type TreeinfoReleaseStageOptions struct {
+	Name    string `json:"name,omitempty"`    // release name, for example: "Fedora", "Red Hat Enterprise Linux", "Spacewalk"
+	Short   string `json:"short,omitempty"`   // release short name, for example: "F", "RHEL", "Spacewalk"
+	Version string `json:"version,omitempty"` // release version, for example: "21", "7.0", "2.1"
+}
+
+type TreeinfoTreeStageOptions struct {
+	Arch           string   `json:"arch,omitempty"`            // tree architecture, for example x86_64
+	BuildTimestamp int64    `json:"build_timestamp,omitempty"` // tree build time timestamp; format: unix time
+	Platforms      []string `json:"platforms,omitempty"`       // supported platforms; for example x86_64,xen
+	Variants       []string `json:"variants,omitempty"`        // UIDs of available variants, for example "Server,Workstation"
+}
+
+type TreeinfoStage2StageOptions struct {
+	Mainimage string `json:"mainimage,omitempty"` // Anaconda stage2 main image file path
+}
+
+type TreeinfoVariantStageOptions struct {
+	ID         string `json:"id,omitempty"`
+	Name       string `json:"name,omitempty"`
+	Packages   string `json:"packages,omitempty"`
+	Repository string `json:"repository,omitempty"`
+	Type       string `json:"type,omitempty"`
+	UID        string `json:"uid,omitempty"`
+}
+
+func (TreeinfoStageOptions) isStageOptions() {}
+
+func NewTreeinfoStageFromOptions(options *TreeinfoStageOptions) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.treeinfo",
+		Options: options,
+	}
+}
+
+func NewTreeinfoStage(name, version string, architecture arch.Arch, variant string, kernel, initrd, stage2 string) *Stage {
+	images := make(map[string]map[string]string)
+	images["images-"+architecture.String()] = map[string]string{
+		"kernel": kernel,
+		"initrd": initrd,
+	}
+
+	checksums := []string{
+		kernel,
+		initrd,
+		stage2,
+	}
+
+	treeinfo := &TreeinfoStageOptions{
+		Path: ".treeinfo",
+		Treeinfo: TreeinfoData{
+			// XXX: also set ShortName (RHEL/F) but it needs to be added to distrodef first
+			Release: &TreeinfoReleaseStageOptions{
+				Name:    name,
+				Version: version,
+			},
+			Tree: &TreeinfoTreeStageOptions{
+				Arch:           architecture.String(),
+				Platforms:      []string{architecture.String()},
+				BuildTimestamp: time.Now().Unix(),
+			},
+			Stage2: &TreeinfoStage2StageOptions{
+				Mainimage: stage2,
+			},
+			Checksums: checksums,
+			Images:    images,
+		},
+	}
+
+	treeinfo.Treeinfo.General = &TreeinfoGeneralStageOptions{
+		Arch:      treeinfo.Treeinfo.Tree.Arch,
+		Family:    treeinfo.Treeinfo.Release.Name,
+		Name:      treeinfo.Treeinfo.Release.Name + " " + treeinfo.Treeinfo.Release.Version,
+		Platforms: treeinfo.Treeinfo.Tree.Platforms,
+		Timestamp: treeinfo.Treeinfo.Tree.BuildTimestamp,
+		Version:   treeinfo.Treeinfo.Release.Version,
+	}
+
+	if variant != "" {
+		treeinfo.Treeinfo.Tree.Variants = []string{variant}
+		treeinfo.Treeinfo.Variants = map[string]TreeinfoVariantStageOptions{
+			"variant-" + variant: {
+				Type: "variant",
+				ID:   variant,
+				UID:  variant,
+				Name: variant,
+			},
+		}
+		treeinfo.Treeinfo.General.Variant = variant
+	}
+
+	return NewTreeinfoStageFromOptions(treeinfo)
+}
