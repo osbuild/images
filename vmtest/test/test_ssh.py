90a59addbf2041ab43f5fd4c2bb74985911dec23
Michael Vogt <michael.vogt@gmail.com>
2025-11-24 11:22:02 +0100

    vmtest: make `VM.run()` more like subprocess.run()
    The current vmtest.vm.VM.run() is a bit clunky and now that use
    this in more place lets make it nicer. This commit tweaks it
    so that it behaves more like subprocess.run() and raises an
    exception on error and returns a subprocess.CalledProgress object.

---
+++ vmtest/test/test_ssh.py
@@ -0,0 +1,60 @@
+import os
+import subprocess
+import textwrap
+import time
+from unittest.mock import call, patch
+
+import pytest
+
+import vmtest.vm
+from vmtest.vm import VM
+
+
+class MockVM(VM):
+    def start(self):
+        pass
+    def force_stop(self):
+        pass
+    def running(self):
+        return True
+
+
+def make_fake_ssh(fake_bin_path, extra_script=""):
+    mock_ssh = fake_bin_path / "ssh"
+    mock_ssh.write_text(textwrap.dedent(f"""\
+    #!/bin/bash -e
+    echo "calling $0 with: $@"
+    {extra_script}
+    """))
+    mock_ssh.chmod(0o755)
+    return mock_ssh
+
+
+def test_ssh_calls_cmd_happy(tmp_path, monkeypatch):
+    monkeypatch.setenv("PATH", os.fspath(tmp_path), prepend=os.pathsep)
+    make_fake_ssh(tmp_path)
+    vm = MockVM()
+    res = vm.run("cmd1 arg1 arg2", user="user1", keyfile="keyfile1")
+    assert res.returncode == 0
+    assert res.stdout.endswith("cmd1 arg1 arg2\n")
+
+
+def test_ssh_calls_cmd_sad(tmp_path, monkeypatch):
+    monkeypatch.setenv("PATH", os.fspath(tmp_path), prepend=os.pathsep)
+    make_fake_ssh(tmp_path, """ echo bad-output ; if [ "${@: -1}" = "bad-cmd" ]; then exit 42; fi """)
+    vm = MockVM()
+    with pytest.raises(subprocess.CalledProcessError) as e:
+        vm.run("bad-cmd", user="user1", keyfile="keyfile1")
+    assert e.value.returncode == 42
+    assert e.value.stdout.endswith("bad-output\n")
+
+
+@patch("time.sleep")
+def test_ssh_calls_retries(mocked_sleep, tmp_path, monkeypatch):
+    monkeypatch.setenv("PATH", os.fspath(tmp_path), prepend=os.pathsep)
+    make_fake_ssh(tmp_path, "echo ssh-very-sad; exit 1")
+    vm = MockVM()
+    with pytest.raises(RuntimeError) as e:
+        res = vm.run("cmd1 arg1 arg2", user="user1", keyfile="keyfile1")
+    assert str(e.value) == "no ssh after 3 retries of 10s"
+    assert mocked_sleep.call_args_list == [ call(10), call(10), call(10) ]
