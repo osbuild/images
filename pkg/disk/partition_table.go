87a376d33c27eb486993cc310b11c4f33eb029bf
Simon de Vlieger <supakeen@redhat.com>
2025-12-11 10:45:33 +0100

    many: check if mountable is on plain
    We need to do things slightly differently for our BLS and grub configs
    when the mountable for `/boot` is not on an plain partition.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- pkg/disk/partition_table.go
+++ pkg/disk/partition_table.go
@@ -648,6 +648,28 @@ func (pt *PartitionTable) FindMountable(mountpoint string) Mountable {
 	if len(path) == 0 {
 		return nil
 	}
+
+	// first path element is guaranteed to be Mountable
+	return path[0].(Mountable)
+}
+
+// FindMountableOnPlain returns the Mountable entity with the given mountpoint in
+// PartitionTable if it is directly located on a plain partition. Returns nil if no
+// Entity on a plain partition has the target as a Mountpoint.
+func (pt *PartitionTable) FindMountableOnPlain(mountpoint string) Mountable {
+	path := entityPath(pt, mountpoint)
+
+	// Make sure that the entity actually has a parent
+	if len(path) < 2 {
+		return nil
+	}
+
+	parent := path[1]
+
+	if _, ok := parent.(*Partition); !ok {
+		return nil
+	}
+
 	// first path element is guaranteed to be Mountable
 	return path[0].(Mountable)
 }
