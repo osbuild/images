304b0224d8553dc8e4a1f6d2fc7935f0a8c5c67a
Simon de Vlieger <supakeen@redhat.com>
2025-12-20 08:27:05 +0100

    manifest/iso: pass customizations instead
    Instead of passing separate fields into the xorrisofs stage option
    function pass all the customizations so they're more easily expanded.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- pkg/manifest/iso_test.go
+++ pkg/manifest/iso_test.go
@@ -8,18 +8,18 @@ import (
 )
 
 func TestISOBoot(t *testing.T) {
-	options := xorrisofsStageOptions("boot.iso", "test-iso-1", Grub2UEFIOnlyISOBoot)
+	options := xorrisofsStageOptions("boot.iso", ISOCustomizations{Label: "test-iso-1", BootType: Grub2UEFIOnlyISOBoot})
 	assert.Nil(t, options.Boot)
 	assert.Equal(t, "", options.IsohybridMBR)
 	assert.Equal(t, "", options.Grub2MBR)
 
-	options = xorrisofsStageOptions("boot.iso", "test-iso-1", SyslinuxISOBoot)
+	options = xorrisofsStageOptions("boot.iso", ISOCustomizations{Label: "test-iso-1", BootType: SyslinuxISOBoot})
 	require.NotNil(t, options.Boot)
 	assert.Equal(t, "isolinux/isolinux.bin", options.Boot.Image)
 	assert.Equal(t, "/usr/share/syslinux/isohdpfx.bin", options.IsohybridMBR)
 	assert.Equal(t, "", options.Grub2MBR)
 
-	options = xorrisofsStageOptions("boot.iso", "test-iso-1", Grub2ISOBoot)
+	options = xorrisofsStageOptions("boot.iso", ISOCustomizations{Label: "test-iso-1", BootType: Grub2ISOBoot})
 	require.NotNil(t, options.Boot)
 	assert.Equal(t, "images/eltorito.img", options.Boot.Image)
 	assert.Equal(t, "/usr/lib/grub/i386-pc/boot_hybrid.img", options.Grub2MBR)
