1d313183399795314b054d85de13c9d43a71a6d0
Simon de Vlieger <supakeen@redhat.com>
2026-01-08 07:53:37 +0100

    wip!
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- test/scripts/check-host-config.sh
+++ test/scripts/check-host-config.sh
@@ -341,7 +341,7 @@ if (( $# > 0 )); then
 
     config="$1"
 
-    if [[ -e "${config}" ]]; then
+    if [[ ! -e "${config}" ]]; then
         echo "Error: config file does not exist"
         ls -larth /tmp
         exit 1
