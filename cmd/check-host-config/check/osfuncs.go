0c5c22eb537035c40efaec90ffbeab37a06813f8
Lukas Zapletal <lzap+git@redhat.com>
2026-01-21 15:46:38 +0100

    checks: provide new utility functions
    These will be used in the next commits from the series.
    
    Also simplify the function signatures, thanks thozza for noticing.

--- cmd/check-host-config/check/osfuncs.go
+++ cmd/check-host-config/check/osfuncs.go
@@ -9,10 +9,10 @@ import (
 )
 
 // ExecCommand is mockable version of os/exec.Command
-var ExecCommand func(name string, arg ...string) *exec.Cmd = exec.Command
+var ExecCommand = exec.Command
 
 // Exec is mockable version of os/exec.Command.Run
-var Exec func(name string, arg ...string) ([]byte, []byte, int, error) = func(name string, arg ...string) ([]byte, []byte, int, error) {
+var Exec = func(name string, arg ...string) ([]byte, []byte, int, error) {
 	cmdStr := name
 	if len(arg) > 0 {
 		cmdStr += " " + strings.Join(arg, " ")
@@ -45,7 +45,7 @@ func ExecString(name string, arg ...string) (string, string, int, error) {
 }
 
 // Exists is mockable version of os.Stat
-var Exists func(name string) bool = func(name string) bool {
+var Exists = func(name string) bool {
 	log.Printf("Exists: %s\n", name)
 	_, err := os.Stat(name)
 	exists := !os.IsNotExist(err)
@@ -55,8 +55,27 @@ var Exists func(name string) bool = func(name string) bool {
 	return exists
 }
 
+// ExistsDir is mockable version that checks if a path exists and is a directory
+var ExistsDir = func(name string) bool {
+	log.Printf("ExistsDir: %s\n", name)
+	info, err := os.Stat(name)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Printf("ExistsDir failed: %s (error: %v)\n", name, err)
+		}
+		return false
+	}
+	return info.IsDir()
+}
+
+// Stat is mockable version of os.Stat
+var Stat = func(name string) (os.FileInfo, error) {
+	log.Printf("Stat: %s\n", name)
+	return os.Stat(name)
+}
+
 // Grep is mockable version of os.ReadFile with grep capabilities
-var Grep func(pattern, filename string) (bool, error) = func(pattern, filename string) (bool, error) {
+var Grep = func(pattern, filename string) (bool, error) {
 	log.Printf("Grep: %s %s\n", pattern, filename)
 	content, err := os.ReadFile(filename)
 	if err != nil {
@@ -67,7 +86,7 @@ var Grep func(pattern, filename string) (bool, error) = func(pattern, filename s
 }
 
 // ReadFile is mockable version of os.ReadFile
-var ReadFile func(filename string) ([]byte, error) = func(filename string) ([]byte, error) {
+var ReadFile = func(filename string) ([]byte, error) {
 	log.Printf("ReadFile: %s\n", filename)
 	data, err := os.ReadFile(filename)
 	if err != nil {
