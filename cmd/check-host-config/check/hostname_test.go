c0ec7092541cdf367587d2bbddab6762089b985d
Lukas Zapletal <lzap+git@redhat.com>
2026-01-30 16:48:37 +0100

    check-host-config: convert tests to tabular format
    Pretty much all tests except OpenSCAP checks look much cleaner in the
    tabular testing format. In order to achieve maximum readability, I
    introduced a helper types and functions in mock_helpers_test.go that
    allows to reuse the following testing pattern:
    
    	tests := []struct {
    		name         string
    		config       *blueprint.KernelCustomization
    		mockExec     map[string]ExecResult
    		mockReadFile map[string]ReadFileResult
    		wantErr      error
    	}
    
    Each test case has a name, configuration and returns either nil or
    error. It also has one or more mocks which are represented as maps with
    input (typically a string but a struct for functions with multiple
    arguments) and output (struct). These mock maps can be nicely formatted:
    
        {
            name: "fail when append does not match",
            config: &blueprint.KernelCustomization{
                Append: "debug",
            },
            mockReadFile: map[string]ReadFileResult{
                "/proc/cmdline": {Data: []byte("root=UUID=1234-5678 ro")},
            },
            wantErr: check.ErrCheckFailed,
        },
    
    The code that performs installation of these mocks is contained in the
    helper functions:
    
        installMockExec(t, tt.mockExec)
        installMockReadFile(t, tt.mockReadFile)

--- cmd/check-host-config/check/hostname_test.go
+++ cmd/check-host-config/check/hostname_test.go
@@ -1,42 +1,62 @@
 package check_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/osbuild/blueprint/pkg/blueprint"
 	check "github.com/osbuild/images/cmd/check-host-config/check"
 	"github.com/osbuild/images/internal/common"
-	"github.com/osbuild/images/internal/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHostnameCheck(t *testing.T) {
-	test.MockGlobal(t, &check.Exec, func(name string, arg ...string) ([]byte, []byte, int, error) {
-		return []byte("test-hostname\n"), nil, 0, nil
-	})
+	tests := []struct {
+		name     string
+		config   *string // hostname customization
+		mockExec map[string]ExecResult
+		wantErr  error
+	}{
+		{
+			name:   "pass when hostname matches",
+			config: common.ToPtr("test-hostname"),
+			mockExec: map[string]ExecResult{
+				"hostname ": {Stdout: []byte("test-hostname\n")},
+			},
+		},
+		{
+			name:   "warning when hostname does not match",
+			config: common.ToPtr("test-hostname"),
+			mockExec: map[string]ExecResult{
+				"hostname ": {Stdout: []byte("changed-by-cloud-init\n")},
+			},
+			wantErr: check.ErrCheckWarning,
+		},
+		{
+			name:    "skip when no hostname customization",
+			config:  nil,
+			wantErr: check.ErrCheckSkipped,
+		},
+	}
 
-	chk, found := check.FindCheckByName("hostname")
-	require.True(t, found, "Hostname Check not found")
-	config := buildConfig(&blueprint.Customizations{
-		Hostname: common.ToPtr("test-hostname"),
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			installMockExec(t, tt.mockExec)
 
-	require.NoError(t, chk.Func(chk.Meta, config))
-}
+			chk, found := check.FindCheckByName("hostname")
+			require.True(t, found, "hostname check not found")
+			config := buildConfig(&blueprint.Customizations{
+				Hostname: tt.config,
+			})
 
-func TestHostnameCheckWarning(t *testing.T) {
-	test.MockGlobal(t, &check.Exec, func(name string, arg ...string) ([]byte, []byte, int, error) {
-		return []byte("changed-by-cloud-init\n"), nil, 0, nil
-	})
-
-	chk, found := check.FindCheckByName("hostname")
-	require.True(t, found, "Hostname Check not found")
-	config := buildConfig(&blueprint.Customizations{
-		Hostname: common.ToPtr("test-hostname"),
-	})
-
-	err := chk.Func(chk.Meta, config)
-	require.Error(t, err)
-	assert.True(t, check.IsWarning(err))
+			err := chk.Func(chk.Meta, config)
+			if tt.wantErr != nil {
+				require.Error(t, err)
+				assert.True(t, errors.Is(err, tt.wantErr))
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
