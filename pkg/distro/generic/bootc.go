62999ebf7e89a5762a41e3d17213a0d5c1d705e1
Achilleas Koutsou <achilleas@koutsou.net>
2026-02-03 12:04:13 +0100

    distro/generic: add bootc distro initialiser to generic distro package
    Merge (copy) the bootc distro types and initialiser into the generic
    distro package.  The distro/bootc package will be removed later.
    
    The goal is to have a single distro package for managing all types of
    distributions, having different entrypoints and internal Distro
    interface implementations for initialising and managing the different
    distribution variants rather than separating them into different
    packages that behave differently.
    
    One important change in the new bootc distro initialiser is it now
    requires that the information from the container is resolved before
    initialising the distro object.  This has two advantages:
    1. It does not implicitly run or introspect the container when
       initialising distribution, architecture, and image type objects.
       Instead, it gives the caller control over when and where this
       information resolution task runs.
    2. It makes it very straightforward to initialise a bootc-based
       distribution with mock data.  Instead of overriding internal
       functions to avoid running container info resolution, a caller can
       populate a bootc distro with any data, real of mocked, to generate
       manifests for production or testing accordingly.
    
    This is a bit more consistent with the generic, package-based
    distribution types, where initialising a distro depends only on the
    internal static information, and any external info and resources
    (packages, containers, ostree commits) are resolved and provided by the
    caller during manifest generation.  However, due to the nature of the
    bootc distro, the container info must be resolved before the
    distribution object is initialised, not between manifest initialisation
    and serialisation.

---
+++ pkg/distro/generic/bootc.go
@@ -0,0 +1,172 @@
+package generic
+
+import (
+	"errors"
+	"fmt"
+	"maps"
+	"slices"
+	"strings"
+
+	"github.com/osbuild/images/pkg/arch"
+	"github.com/osbuild/images/pkg/bib/osinfo"
+	"github.com/osbuild/images/pkg/bootc"
+	"github.com/osbuild/images/pkg/disk"
+	"github.com/osbuild/images/pkg/distro"
+	"github.com/osbuild/images/pkg/distro/defs"
+)
+
+type BootcDistro struct {
+	imgref          string
+	imageID         string
+	buildImgref     string
+	sourceInfo      *osinfo.Info
+	buildSourceInfo *osinfo.Info
+
+	id            distro.ID
+	defaultFs     string
+	releasever    string
+	rootfsMinSize uint64
+
+	arches map[string]distro.Arch
+}
+
+const (
+	// As a baseline heuristic we double the size of the input container to
+	// support in-place updates. We plan to make this configurable in the
+	// future.
+	containerSizeToDiskSizeMultiplier = 2
+)
+
+// NewBootc returns a distro initialised with the provided information. All
+// required information must be defined. There are no restrictions or
+// requirements for the name of the distro and it is only used to identify this
+// particular instance of the distribution. The name bootc is commonly used,
+// unless multiple instances are created.
+// To generate the [github.com/osbuild/images/pkg/bootc.Info] from a container
+// ref, use the [github.com/osbuild/images/pkg/bootc.Container] type and its
+// methods.
+func NewBootc(name string, cinfo *bootc.Info) (distro.Distro, error) {
+	if cinfo == nil {
+		return nil, errors.New("failed to initialize bootc distro: container info is empty")
+	}
+
+	// verify required information
+	var missing []string
+	if cinfo.Imgref == "" {
+		missing = append(missing, "Imgref")
+	}
+	if cinfo.ImageID == "" {
+		missing = append(missing, "ImageID")
+	}
+	if cinfo.Arch == "" {
+		missing = append(missing, "Arch")
+	}
+	if cinfo.DefaultRootFs == "" {
+		missing = append(missing, "DefaultRootFs")
+	}
+	if cinfo.Size == 0 {
+		missing = append(missing, "Size")
+	}
+
+	if len(missing) > 0 {
+		return nil, fmt.Errorf("failed to initialize bootc distro: missing required info: %s", strings.Join(missing, ", "))
+	}
+
+	osInfo := cinfo.OSInfo
+	if osInfo == nil {
+		// TODO: required?
+		osInfo = &osinfo.Info{}
+	}
+
+	d := &BootcDistro{
+		// the ID is technically not allowed by the ID parser, as it doesn't
+		// contain a version, but we will relax this requirement later
+		id:              distro.ID{Name: name},
+		imgref:          cinfo.Imgref,
+		imageID:         cinfo.ImageID,
+		buildImgref:     cinfo.Imgref, // default to using the same image for build for now
+		sourceInfo:      osInfo,
+		buildSourceInfo: osInfo,
+		defaultFs:       cinfo.DefaultRootFs,
+		releasever:      osInfo.OSRelease.VersionID,
+		rootfsMinSize:   cinfo.Size * containerSizeToDiskSizeMultiplier,
+	}
+
+	// load image types from bootc-generic-1
+	distroYAML, err := defs.LoadDistroWithoutImageTypes("bootc-generic-1")
+	if err != nil {
+		return nil, fmt.Errorf("failed to load bootc image types: %w", err)
+	}
+	defaultFs, err := disk.NewFSType(cinfo.DefaultRootFs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set default rootfs for bootc distro: %w", err)
+	}
+	distroYAML.DefaultFSType = defaultFs
+	if err := distroYAML.LoadImageTypes(); err != nil {
+		return nil, fmt.Errorf("failed to load bootc distro image types: %w", err)
+	}
+
+	// initialise a single architecture to match the architecture of the bootc
+	// container
+	archi, err := arch.FromString(cinfo.Arch)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set bootc distro architecture: %w", err)
+	}
+
+	ba := &architecture{
+		arch:       archi,
+		imageTypes: map[string]distro.ImageType{},
+	}
+	for _, imgTypeYaml := range distroYAML.ImageTypes() {
+		err := ba.addImageType(nil, imageType{ // NOTE: platform does not apply to bootc image types
+			ImageTypeYAML: imgTypeYaml,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to add image type to bootc distro: %w", err)
+		}
+	}
+	d.arches = map[string]distro.Arch{ba.Name(): ba}
+
+	return d, nil
+}
+
+func (d *BootcDistro) Name() string {
+	return d.id.String()
+}
+
+func (d *BootcDistro) Codename() string {
+	return ""
+}
+
+func (d *BootcDistro) Releasever() string {
+	return d.releasever
+}
+
+func (d *BootcDistro) OsVersion() string {
+	return d.releasever
+}
+
+func (d *BootcDistro) Product() string {
+	return d.id.String()
+}
+
+func (d *BootcDistro) ModulePlatformID() string {
+	return ""
+}
+
+func (d *BootcDistro) ListArches() []string {
+	return slices.Sorted(maps.Keys(d.arches))
+}
+
+func (d *BootcDistro) GetArch(arch string) (distro.Arch, error) {
+	a, exists := d.arches[arch]
+	if !exists {
+		return nil, fmt.Errorf("requested bootc arch %q does not match available arches %v", arch, slices.Collect(maps.Keys(d.arches)))
+	}
+	return a, nil
+}
+
+func (d *BootcDistro) GetTweaks() *distro.Tweaks {
+	// The bootc distro does not require or support tweaks (yet)
+	return nil
+}
---
+++ pkg/distro/generic/bootc_test.go
@@ -0,0 +1,138 @@
+package generic
+
+import (
+	"testing"
+
+	"github.com/osbuild/images/pkg/arch"
+	"github.com/osbuild/images/pkg/bib/osinfo"
+	"github.com/osbuild/images/pkg/bootc"
+	"github.com/osbuild/images/pkg/datasizes"
+	"github.com/osbuild/images/pkg/disk"
+	"github.com/osbuild/images/pkg/distro"
+	"github.com/osbuild/images/pkg/distro/defs"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewBootc(t *testing.T) {
+	type testCase struct {
+		info           *bootc.Info
+		expectedDistro *BootcDistro
+		expectedError  string
+	}
+
+	testCases := map[string]testCase{
+		"empty": {
+			expectedError: "failed to initialize bootc distro: container info is empty",
+		},
+
+		"ok": {
+			info: &bootc.Info{
+				Imgref:        "example.com/containers/distro-bootc:version12",
+				ImageID:       "acf88e518194fac963a1b2e2e4110e38a4ce5fb3fceddd624fae8997d4566930",
+				Arch:          "arm64",
+				DefaultRootFs: "xfs",
+				Size:          100 * datasizes.MiB,
+			},
+			expectedDistro: &BootcDistro{
+				imgref:          "example.com/containers/distro-bootc:version12",
+				imageID:         "acf88e518194fac963a1b2e2e4110e38a4ce5fb3fceddd624fae8997d4566930",
+				buildImgref:     "example.com/containers/distro-bootc:version12",
+				sourceInfo:      &osinfo.Info{},
+				buildSourceInfo: &osinfo.Info{},
+				id: distro.ID{
+					Name: "bootc",
+				},
+
+				defaultFs:     "xfs",
+				rootfsMinSize: 200 * datasizes.MiB,
+				arches: map[string]distro.Arch{
+					"aarch64": &architecture{
+						arch: arch.ARCH_AARCH64,
+					},
+				},
+			},
+		},
+
+		"noimgref": {
+			info: &bootc.Info{
+				ImageID:       "acf88e518194fac963a1b2e2e4110e38a4ce5fb3fceddd624fae8997d4566930",
+				Arch:          "aarch64",
+				DefaultRootFs: "xfs",
+				Size:          100 * datasizes.MiB,
+			},
+			expectedError: "failed to initialize bootc distro: missing required info: Imgref",
+		},
+
+		"noimageid": {
+			info: &bootc.Info{
+				Imgref:        "example.com/containers/distro-bootc:version12",
+				Arch:          "amd64",
+				DefaultRootFs: "xfs",
+				Size:          100 * datasizes.MiB,
+			},
+			expectedError: "failed to initialize bootc distro: missing required info: ImageID",
+		},
+
+		"missing-multiple": {
+			info: &bootc.Info{
+				Imgref: "example.com/containers/distro-bootc:version12",
+			},
+			expectedError: "failed to initialize bootc distro: missing required info: ImageID, Arch, DefaultRootFs, Size",
+		},
+
+		"unknown-arch": {
+			info: &bootc.Info{
+				Imgref:        "example.com/containers/distro-bootc:version12",
+				ImageID:       "acf88e518194fac963a1b2e2e4110e38a4ce5fb3fceddd624fae8997d4566930",
+				Arch:          "not-an-arch",
+				DefaultRootFs: "xfs",
+				Size:          100 * datasizes.MiB,
+			},
+			expectedError: "failed to set bootc distro architecture: unsupported architecture \"not-an-arch\"",
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			require := require.New(t)
+
+			d, err := NewBootc("bootc", tc.info)
+
+			if tc.expectedError != "" {
+				require.EqualError(err, tc.expectedError)
+				return
+			}
+
+			require.NotNil(d)
+			loadImageTypes(t, tc.expectedDistro)
+			require.Equal(tc.expectedDistro, d)
+		})
+	}
+}
+
+// Helper function for loading static bootc image type definitions onto the
+// expected distro object.
+func loadImageTypes(t *testing.T, d *BootcDistro) {
+	t.Helper()
+
+	require := require.New(t)
+
+	distroYAML, err := defs.LoadDistroWithoutImageTypes("bootc-generic-1")
+	require.NoError(err)
+
+	fs, err := disk.NewFSType(d.defaultFs)
+	require.NoError(err)
+
+	distroYAML.DefaultFSType = fs // It's very weird that this is required here
+	require.NoError(distroYAML.LoadImageTypes())
+
+	for archName, arch := range d.arches {
+		darch := arch.(*architecture)
+		darch.imageTypes = map[string]distro.ImageType{}
+		require.NotNil(darch)
+		for _, imgTypeYaml := range distroYAML.ImageTypes() {
+			require.NoError(darch.addImageType(nil, imageType{ImageTypeYAML: imgTypeYaml}))
+		}
+		d.arches[archName] = darch
+	}
+}
