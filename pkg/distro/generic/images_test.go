417388477b4b9cd7c57c2b4be970d1f8af93ce4c
Tomáš Hozza <thozza@redhat.com>
2026-02-05 11:42:54 +0100

    distro/generic: propagate kernel options to kickstart for installers
    The blueprint's customizations.kernel.append setting should configure
    kernel options for the installed system's bootloader, not just the
    installer ISO's boot menu. This was working for bootc installers but
    was missing from the generic distro's installer image functions.
    
    Without this fix, users setting kernel.append in their blueprint would
    only see the options applied to the installer's boot menu, but after
    installation the system's bootloader would not have these options
    configured.
    
    Add the missing KernelOptionsAppend propagation to the kickstart
    configuration in imageInstallerImage (minimal-installer),
    iotInstallerImage (edge-installer), and networkInstallerImage
    (boot-iso) functions.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- pkg/distro/generic/images_test.go
+++ pkg/distro/generic/images_test.go
@@ -1,6 +1,7 @@
 package generic
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +11,9 @@ import (
 	"github.com/osbuild/images/internal/common"
 	"github.com/osbuild/images/pkg/distro"
 	"github.com/osbuild/images/pkg/distro/defs"
+	"github.com/osbuild/images/pkg/image"
+	"github.com/osbuild/images/pkg/ostree"
+	"github.com/osbuild/images/pkg/rpmmd"
 )
 
 func isoTestImageType() *imageType {
@@ -103,5 +107,85 @@ func TestInstallerCustomizationsOverridePreview(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, tc.expected, isc.Preview)
 	}
+}
 
+// TestKickstartKernelOptionsAppend tests that the kernel.append blueprint
+// customization is properly propagated to the kickstart configuration for the
+// installed system's bootloader in all installer image types.
+func TestKickstartKernelOptionsAppend(t *testing.T) {
+	// math/rand is good enough for testing
+	/* #nosec G404 */
+	rng := rand.New(rand.NewSource(0))
+
+	kernelAppend := "debug console=ttyS0"
+
+	bp := &blueprint.Blueprint{
+		Customizations: &blueprint.Customizations{
+			Kernel: &blueprint.KernelCustomization{
+				Append: kernelAppend,
+			},
+		},
+	}
+
+	packageSets := map[string]rpmmd.PackageSet{
+		osPkgsKey:        {},
+		installerPkgsKey: {},
+	}
+
+	testCases := []struct {
+		name     string
+		imageFn  imageFunc
+		isOSTree bool
+	}{
+		{
+			name:    "imageInstallerImage",
+			imageFn: imageInstallerImage,
+		},
+		{
+			name:     "iotInstallerImage",
+			imageFn:  iotInstallerImage,
+			isOSTree: true,
+		},
+		{
+			name:    "networkInstallerImage",
+			imageFn: networkInstallerImage,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			it := isoTestImageType()
+			io := distro.ImageOptions{}
+			if tc.isOSTree {
+				it.ImageTypeYAML.OSTree.Name = "fedora"
+				it.ImageTypeYAML.OSTree.RemoteName = "fedora"
+				io.OSTree = &ostree.ImageOptions{
+					ImageRef: "test/ref",
+					URL:      "http://example.com/repo",
+				}
+			}
+
+			img, err := tc.imageFn(it, bp, io, packageSets, nil, nil, rng)
+			require.NoError(t, err)
+
+			// All installer image types embed AnacondaInstallerBase which has Kickstart
+			var kickstartOpts []string
+			switch v := img.(type) {
+			case *image.AnacondaTarInstaller:
+				require.NotNil(t, v.Kickstart)
+				kickstartOpts = v.Kickstart.KernelOptionsAppend
+			case *image.AnacondaOSTreeInstaller:
+				require.NotNil(t, v.Kickstart)
+				kickstartOpts = v.Kickstart.KernelOptionsAppend
+			case *image.AnacondaNetInstaller:
+				require.NotNil(t, v.Kickstart)
+				kickstartOpts = v.Kickstart.KernelOptionsAppend
+			default:
+				t.Fatalf("unexpected image type: %T", img)
+			}
+
+			assert.Contains(t, kickstartOpts, kernelAppend,
+				"Kickstart.KernelOptionsAppend should contain the kernel.append from blueprint")
+		})
+	}
 }
