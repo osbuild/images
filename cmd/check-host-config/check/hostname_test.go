c8a6207fcfe8015293471826790b25e833ee49d9
Lukas Zapletal <lzap+git@redhat.com>
2026-02-09 14:56:34 +0100

    check-host-config: fallback to /etc/hostname
    For container tests, both hostnamectl and hostname are not available, so we
    fallback to reading the hostname from /etc/hostname.
    
    Also fix extra space in hostname test.

--- cmd/check-host-config/check/hostname_test.go
+++ cmd/check-host-config/check/hostname_test.go
@@ -13,23 +13,42 @@ import (
 
 func TestHostnameCheck(t *testing.T) {
 	tests := []struct {
-		name     string
-		config   *string // hostname customization
-		mockExec map[string]ExecResult
-		wantErr  error
+		name         string
+		config       *string // hostname customization
+		mockExec     map[string]ExecResult
+		mockReadFile map[string]ReadFileResult
+		wantErr      error
 	}{
 		{
-			name:   "pass when hostname matches",
+			name:   "pass when hostname matches (via hostnamectl)",
 			config: common.ToPtr("test-hostname"),
 			mockExec: map[string]ExecResult{
-				"hostname ": {Stdout: []byte("test-hostname\n")},
+				"hostnamectl hostname": {Stdout: []byte("test-hostname\n")},
+			},
+		},
+		{
+			name:   "pass when hostname matches (via hostname)",
+			config: common.ToPtr("test-hostname"),
+			mockExec: map[string]ExecResult{
+				"hostname": {Stdout: []byte("test-hostname\n")},
+			},
+		},
+		{
+			name:   "pass when hostname matches (via /etc/hostname)",
+			config: common.ToPtr("myhost"),
+			mockExec: map[string]ExecResult{
+				"hostnamectl hostname": {Err: errors.New("not found")},
+				"hostname":             {Err: errors.New("not found")},
+			},
+			mockReadFile: map[string]ReadFileResult{
+				"/etc/hostname": {Data: []byte("myhost\n")},
 			},
 		},
 		{
 			name:   "warning when hostname does not match",
 			config: common.ToPtr("test-hostname"),
 			mockExec: map[string]ExecResult{
-				"hostname ": {Stdout: []byte("changed-by-cloud-init\n")},
+				"hostnamectl hostname": {Stdout: []byte("changed-by-cloud-init\n")},
 			},
 			wantErr: check.ErrCheckWarning,
 		},
@@ -38,11 +57,24 @@ func TestHostnameCheck(t *testing.T) {
 			config:  nil,
 			wantErr: check.ErrCheckSkipped,
 		},
+		{
+			name:   "error when no source available",
+			config: common.ToPtr("expected"),
+			mockExec: map[string]ExecResult{
+				"hostnamectl hostname": {Err: errors.New("hostnamectl not found")},
+				"hostname":             {Err: errors.New("hostname not found")},
+			},
+			mockReadFile: map[string]ReadFileResult{
+				"/etc/hostname": {Err: errors.New("no such file")},
+			},
+			wantErr: check.ErrHostname,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			installMockExec(t, tt.mockExec)
+			installMockReadFile(t, tt.mockReadFile)
 
 			chk, found := check.FindCheckByName("hostname")
 			require.True(t, found, "hostname check not found")
