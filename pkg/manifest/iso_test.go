ba3e19255b6aee8859ee9cb83e21dd4ba24b5ceb
Brian C. Lane <bcl@redhat.com>
2026-02-25 11:12:25 -0800

    manifest: Add Grub2PPCISOBoot to xorrisofsStageOptions
    This sets up the xorrisofs stage for booting on ppc64le, includes a
    test.
    
    Related: HMS-10110

--- pkg/manifest/iso_test.go
+++ pkg/manifest/iso_test.go
@@ -12,18 +12,21 @@ func TestISOBoot(t *testing.T) {
 	assert.Nil(t, options.Boot)
 	assert.Equal(t, "", options.IsohybridMBR)
 	assert.Equal(t, "", options.Grub2MBR)
+	assert.Equal(t, "images/efiboot.img", options.EFI)
 
 	options = xorrisofsStageOptions("boot.iso", ISOCustomizations{Label: "test-iso-1", BootType: SyslinuxISOBoot})
 	require.NotNil(t, options.Boot)
 	assert.Equal(t, "isolinux/isolinux.bin", options.Boot.Image)
 	assert.Equal(t, "/usr/share/syslinux/isohdpfx.bin", options.IsohybridMBR)
 	assert.Equal(t, "", options.Grub2MBR)
+	assert.Equal(t, "images/efiboot.img", options.EFI)
 
 	options = xorrisofsStageOptions("boot.iso", ISOCustomizations{Label: "test-iso-1", BootType: Grub2ISOBoot})
 	require.NotNil(t, options.Boot)
 	assert.Equal(t, "images/eltorito.img", options.Boot.Image)
 	assert.Equal(t, "/usr/lib/grub/i386-pc/boot_hybrid.img", options.Grub2MBR)
 	assert.Equal(t, "", options.IsohybridMBR)
+	assert.Equal(t, "images/efiboot.img", options.EFI)
 
 	options = xorrisofsStageOptions("boot.iso", ISOCustomizations{Label: "test-iso-1", BootType: Grub2ISOBoot, Preparer: "Test", Publisher: "Tester"})
 	require.NotNil(t, options.Boot)
@@ -32,4 +35,15 @@ func TestISOBoot(t *testing.T) {
 	assert.Equal(t, "", options.IsohybridMBR)
 	assert.Equal(t, "Test", options.Prep)
 	assert.Equal(t, "Tester", options.Pub)
+	assert.Equal(t, "images/efiboot.img", options.EFI)
+
+	options = xorrisofsStageOptions("boot.iso", ISOCustomizations{Label: "test-iso-1", BootType: Grub2PPCISOBoot, Preparer: "Test", Publisher: "Tester"})
+	require.Nil(t, options.Boot)
+	assert.Equal(t, "", options.Grub2MBR)
+	assert.Equal(t, "", options.IsohybridMBR)
+	assert.Equal(t, "Test", options.Prep)
+	assert.Equal(t, "Tester", options.Pub)
+	assert.Equal(t, true, options.CHRPBoot)
+	assert.Equal(t, "test-iso-1", options.VolSet)
+	assert.Equal(t, "", options.EFI)
 }
