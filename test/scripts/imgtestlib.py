b7284b6f8876989bc68c0ed2bdbb1bec021f4b63
Achilleas Koutsou <achilleas@koutsou.net>
2026-03-03 18:33:31 +0100

    test: collapsible log sections for each build
    Print a log section start and end line when building an image.  If
    running in GitLab CI, use their custom collapsible sections escape
    sequences [1] to create collapsible build logs for each image.
    
    [1] https://docs.gitlab.com/ci/jobs/job_logs/#custom-collapsible-sections

--- test/scripts/imgtestlib.py
+++ test/scripts/imgtestlib.py
@@ -720,3 +720,38 @@ def touch_s3(distro, arch, manifest_id, osbuild_ref=None, runner_distro=None):
     print(f"⌚ Updating timestamps for {s3url} ({now})")
     cmd = ["aws", "s3", "cp", "--recursive", "--metadata", f"touched={now}", s3url, s3url]
     runcmd_nc(cmd)
+
+
+def running_in_gitlab():
+    """
+    Returns true if running in GitLab CI.
+    """
+    return os.environ.get("GITLAB_CI")
+
+
+def print_section_start(name: str, msg: str = ""):
+    """
+    Prints a section header with a timestamp for logging output during tests.
+    If running in GitLab CI, it also creates a collapsible section.
+
+    https://docs.gitlab.com/ci/jobs/job_logs/#custom-collapsible-sections
+    """
+    now = datetime.now()
+    if running_in_gitlab():
+        print(f"\033[0Ksection_start:{int(now.timestamp())}:{name}\r\033[0K{msg}")
+        return
+
+    # custom line for non CI runs
+    isonow = now.isoformat()
+    print(f":: [{isonow}] {msg} ({name})")
+
+
+def print_section_end(name: str):
+    now = datetime.now()
+    if running_in_gitlab():
+        print(f"\033[0Ksection_end:{int(now.timestamp())}:{name}\r\033[0K")
+        return
+
+    # custom line for non CI runs
+    isonow = now.isoformat()
+    print(f":: [{isonow}] Done ({name})")
